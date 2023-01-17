2227a1d79db6019fedd7aadf655bd92874290198
Runeword <cha.cascio@gmail.com>
2023-01-18 00:06:57 +0100

    Update flake/flake.nix

--- flake/flake.nix
+++ flake/flake.nix
@@ -2,23 +2,28 @@
   inputs.home-manager.url = "github:nix-community/home-manager";
   inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-  inputs.neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+  # inputs.neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+
+  inputs.neovim-nightly-overlay = {
+    url = "github:nix-community/neovim-nightly-overlay";
+    inputs.nixpkgs.url = "github:nixos/nixpkgs?rev=fad51abd42ca17a60fc1d4cb9382e2d79ae31836";
+  };
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
-  outputs = { self, ... }@inputs: {
+  outputs = { self, ... }@inputs:
+  let pkgs = import inputs.nixpkgs {
+    system = "x86_64-linux";
+    config.allowUnfree = true;
+    overlays = [
+      inputs.neovim-nightly-overlay.overlay
+    ];
+  };
+  in {
     homeConfigurations.charles = inputs.home-manager.lib.homeManagerConfiguration {
-
-      pkgs = import inputs.nixpkgs {
-        system = "x86_64-linux";
-        config.allowUnfree = true;
-        overlays = [
-          inputs.neovim-nightly-overlay.overlay
-        ];
-      };
-
+      inherit pkgs;
       modules = [
-        ./home.nix
+        modules/home.nix
       ];
     };
   };

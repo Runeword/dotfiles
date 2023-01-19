f0f422605274466e201de7116da01574fc96d611
Runeword <cha.cascio@gmail.com>
2023-01-19 21:14:31 +0100

    Update flake/flake.nix

--- flake/flake.nix
+++ flake/flake.nix
@@ -2,12 +2,8 @@
   inputs.home-manager.url = "github:nix-community/home-manager";
   inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-  # inputs.neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
-
-  inputs.neovim-nightly-overlay = {
-    url = "github:nix-community/neovim-nightly-overlay";
-    inputs.nixpkgs.url = "github:nixos/nixpkgs?rev=fad51abd42ca17a60fc1d4cb9382e2d79ae31836";
-  };
+  inputs.neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+  inputs.neovim-nightly-overlay.inputs.nixpkgs.url = "github:nixos/nixpkgs?rev=fad51abd42ca17a60fc1d4cb9382e2d79ae31836";
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 

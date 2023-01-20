d4af559004199843558abe795c824a74cddc1cdb
Runeword <cha.cascio@gmail.com>
2023-01-20 22:06:20 +0100

    Update flake/modules/home.nix

--- flake/modules/home.nix
+++ flake/modules/home.nix
@@ -1,53 +1,65 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }: {
+  #let
+  #  fromGitHub = ref: repo: pkgs.vimUtils.buildVimPluginFrom2Nix {
+  #    pname = "${lib.strings.sanitizeDerivationName repo}";
+  #    version = ref;
+  #    src = builtins.fetchGit {
+  #      url = "https://github.com/${repo}.git";
+  #      ref = ref;
+  #    };
+  #  };
+  #in {
 
-{
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
-  home.username = "charles";
-  home.homeDirectory = "/home/charles";
-
-  # This value determines the Home Manager release that your
-  # configuration is compatible with. This helps avoid breakage
-  # when a new Home Manager release introduces backwards
-  # incompatible changes.
-  #
-  # You can update Home Manager without changing this value. See
-  # the Home Manager release notes for a list of state version
-  # changes in each release.
-  home.stateVersion = "22.11";
-
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-
-  fonts.fontconfig.enable = true;
-
-  home.packages = with pkgs; [
-    git
-    htop
-    wget
-    ripgrep
-    fd
-    vifm
-    xclip
-    chezmoi
-    bat
-    gcc
-    python311
-    nodejs-19_x
-    alacritty
-    bitwarden-cli
-    google-chrome
-    neovim-nightly
-    (nerdfonts.override { fonts = [ "Hack" ]; })
-  ];
-
-  # home.shellAliases = import ./shellAliases.nix
-
-  programs.fzf.enable = true;
-  programs.fzf.enableBashIntegration = true;
-  programs.fzf.fileWidgetCommand = "fd --hidden --follow --no-ignore --max-depth 1 --exclude .git --exclude node_modules";
-  programs.fzf.defaultOptions = [ "--no-separator" ];
-
-  programs.bash.enable = true;
-  programs.bash.enableCompletion = true;
-}
+    fonts.fontconfig.enable = true;
+  
+    # home.shellAliases = import ./shellAliases.nix
+    home.username = "charles";
+    home.homeDirectory = "/home/charles";
+    home.stateVersion = "22.11";
+    home.packages = with pkgs; [
+      git
+      htop
+      wget
+      ripgrep
+      fd
+      vifm
+      xclip
+      chezmoi
+      bat
+      gcc
+      python311
+      nodejs-19_x
+      alacritty
+      neovim-nightly
+      bitwarden-cli
+      google-chrome
+      (nerdfonts.override { fonts = [ "Hack" ]; })
+    ];
+  
+    programs.home-manager.enable = true;
+  
+    programs.fzf.enable = true;
+    programs.fzf.enableBashIntegration = true;
+    programs.fzf.fileWidgetCommand = "fd --hidden --follow --no-ignore --max-depth 1 --exclude .git --exclude node_modules";
+    programs.fzf.defaultOptions = [ "--no-separator" ];
+  
+    programs.bash.enable = true;
+    programs.bash.enableCompletion = true;
+  
+    # programs.neovim.enable = true;
+    # programs.neovim.defaultEditor = true;
+    # programs.neovim.viAlias = true;
+    # programs.neovim.package = pkgs.neovim-nightly;
+    # programs.neovim.extraConfig = "luafile ~/flake/config/nvim/init.lua";
+  
+    # programs.neovim.plugins = with pkgs.vimPlugins; [
+    #   nvim-lspconfig
+    #   nvim-treesitter
+    #   plenary-nvim
+    #   (fromGitHub "HEAD" "itchyny/vim-cursorword")
+    #   (fromGitHub "HEAD" "windwp/nvim-ts-autotag")
+    #   (fromGitHub "HEAD" "tommcdo/vim-exchange")
+    #   (fromGitHub "HEAD" "kyazdani42/nvim-web-devicons")
+    #   (fromGitHub "HEAD" "kana/vim-arpeggio")
+    # ];
+  }

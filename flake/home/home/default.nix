20a27ef0208c3abb7d09b94860a90a583adce76b
Runeword <cha.cascio@gmail.com>
2023-01-21 12:10:17 +0100

    Add flake/home/home/default.nix

---
+++ flake/home/home/default.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, lib, ... }: {
+  imports = [ ./shellAliases.nix ];
+  home.username = "charles";
+  home.homeDirectory = "/home/charles";
+  home.stateVersion = "22.11";
+  home.packages = with pkgs; [
+    git
+    htop
+    wget
+    ripgrep
+    fd
+    vifm
+    xclip
+    chezmoi
+    bat
+    gcc
+    python311
+    nodejs-19_x
+    alacritty
+    neovim-nightly
+    bitwarden-cli
+    google-chrome
+    (nerdfonts.override { fonts = [ "Hack" ]; })
+  ];
+}

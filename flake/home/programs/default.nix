f00f245d77e70bcd6289aaf8ddcc97f56860b5b5
Runeword <cha.cascio@gmail.com>
2023-01-24 18:13:00 +0100

    Update flake/home/programs/default.nix

--- flake/home/programs/default.nix
+++ flake/home/programs/default.nix
@@ -12,6 +12,7 @@
 
   imports = [
     ./readline.nix
+    ./alacritty.nix
   ];
 
   programs.home-manager.enable = true;
@@ -21,6 +22,8 @@
   programs.fzf.fileWidgetCommand = "fd --hidden --follow --no-ignore --max-depth 1 --exclude .git --exclude node_modules";
   programs.fzf.defaultOptions = [ "--no-separator" ];
 
+  programs.rofi.enable = true;
+
   programs.bash.enable = true;
   programs.bash.enableCompletion = true;
   programs.bash.bashrcExtra = ''
@@ -28,38 +31,11 @@
     stty -ixon # unbind ctrl-s and ctrl-q (terminal scroll lock)
   '';
 
-  programs.alacritty.enable = true;
-  programs.alacritty.settings = {
-    font = {
-      normal.family = "Hack Nerd Font";
-      normal.style = "Regular";
-      bold.family = "Hack Nerd Font";
-      bold.style = "Bold";
-      italic.family = "Hack Nerd Font";
-      italic.style = "Italic";
-      bold_italic.family = "Hack Nerd Font";
-      bold_italic.style = "Bold Italic";
-      size = 10.0;
-      offset.y = 5;
-    };
-    selection = {
-      semantic_escape_chars = ",│`|:\"' ()[]{}<>\t";
-      save_to_clipboard = false;
-    };
-    cursor = {
-      style.shape = "Beam";
-      style.blinking = "Always";
-      blink_interval = 500;
-      thickness = 0.1;
-    };
-    window = {
-      dynamic_padding = true;
-      decorations = "none";
-      startup_mode = "Maximized";
-      opacity = 0.70;
-    };
-  };
-
+  programs.tmux.enable = true;
+  programs.tmux.baseIndex = 1;
+  programs.tmux.disableConfirmationPrompt = true;
+  programs.tmux.escapeTime = 0;
+  # programs.tmux.extraConfig;
 
   # programs.neovim.enable = true;
   # programs.neovim.defaultEditor = true;

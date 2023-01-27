969caca3c366095e0d7981aa5b3849b0ed03504d
Runeword <cha.cascio@gmail.com>
2023-01-27 11:03:42 +0100

    Add flake/charles/programs/readline.nix

---
+++ flake/charles/programs/readline.nix
@@ -0,0 +1,16 @@
+{
+  programs.readline.enable = true;
+  programs.readline.includeSystemConfig = false;
+  programs.readline.variables.bind-tty-special-chars = false; # override default readline bindings
+
+  programs.readline.bindings = {
+    "\\C-u" = "kill-whole-line"; # ctrl + u
+    "\\C-\\b" = "backward-kill-word"; # ctrl + backspace
+    "\\e[A" = "history-search-backward"; # arrow up
+    "\\e[B" = "history-search-forward"; # arrow down
+    "\\e[6~" = ""; # page down
+    "\\e[5~" = ""; # page up
+    # "\e[1;5B"= "history-search-forward"; # ctrl + arrow down
+    # "\e[1;5A"= "history-search-backward"; # ctrl + arrow up
+  };
+}

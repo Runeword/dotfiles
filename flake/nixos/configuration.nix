42559b7f2256e37f4d1958070382f08e0b4f5656
Runeword <cha.cascio@gmail.com>
2023-01-31 11:27:35 +0100

    Update flake/nixos/configuration.nix

--- flake/nixos/configuration.nix
+++ flake/nixos/configuration.nix
@@ -1,20 +1,13 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix      
-    ];
+  imports = [
+    ./hardware-configuration.nix
+  ];
 
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
-  
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.kernelParams = [
     "mem_sleep_default=deep"

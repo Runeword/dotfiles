2769cd23f41e97c357eae6e989e2c2a6a2dabec7
Runeword <cha.cascio@gmail.com>
2023-02-07 21:52:11 +0100

    Add flake/home-manager/charles/programs/alacritty.nix

---
+++ flake/home-manager/charles/programs/alacritty.nix
@@ -0,0 +1,64 @@
+{
+  programs.alacritty.enable = true;
+
+  programs.alacritty.settings.font = {
+    normal.family = "Hack Nerd Font";
+    normal.style = "Regular";
+    bold.family = "Hack Nerd Font";
+    bold.style = "Bold";
+    italic.family = "Hack Nerd Font";
+    italic.style = "Italic";
+    bold_italic.family = "Hack Nerd Font";
+    bold_italic.style = "Bold Italic";
+    size = 10.0;
+    offset.y = 5;
+  };
+
+  programs.alacritty.settings.selection = {
+    semantic_escape_chars = ",│`|:\"' ()[]{}<>\t";
+    save_to_clipboard = false;
+  };
+
+  programs.alacritty.settings.cursor = {
+    style.shape = "Beam";
+    style.blinking = "Always";
+    blink_interval = 500;
+    thickness = 0.1;
+  };
+
+  programs.alacritty.settings.window = {
+    dynamic_padding = true;
+    decorations = "none";
+    startup_mode = "Maximized";
+    opacity = 0.70;
+  };
+
+  programs.alacritty.settings.colors = {
+    primary = {
+      background = "#000000";
+      foreground = "#ffffff";
+    };
+
+    normal = {
+      black = "#000000";
+      red = "#cd0000";
+      green = "#00cd00";
+      yellow = "#cdcd00";
+      blue = "#0000ee";
+      magenta = "#cd00cd";
+      cyan = "#00cdcd";
+      white = "#e5e5e5";
+    };
+
+    bright = {
+      black = "#7f7f7f";
+      red = "#ff0000";
+      green = "#00ff00";
+      yellow = "#ffff00";
+      blue = "#5c5cff";
+      magenta = "#ff00ff";
+      cyan = "#00ffff";
+      white = "#ffffff";
+    };
+  };
+}

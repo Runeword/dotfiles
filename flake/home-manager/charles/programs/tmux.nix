0a0eba72121efe0c6e1f48402cc4380fe61fb900
Runeword <cha.cascio@gmail.com>
2023-02-07 21:53:20 +0100

    Add flake/home-manager/charles/programs/tmux.nix

---
+++ flake/home-manager/charles/programs/tmux.nix
@@ -0,0 +1,29 @@
+{ config, pkgs, lib, ... }: {
+  programs.tmux.enable = true;
+
+  programs.tmux.terminal = "tmux-256color";
+  programs.tmux.baseIndex = 1;
+  programs.tmux.disableConfirmationPrompt = true;
+  programs.tmux.escapeTime = 0;
+
+  programs.tmux.extraConfig = ''
+  # _________________________________ Extra options
+  set -g mouse on
+  set -g status-interval 1 # Update the status line every 1 seconds
+  set -g renumber-windows on # Re-number windows when one is closed
+
+  # _________________________________ Status line
+  set -g status-style bg=default
+  set -g status-position top
+  set -g status-left ""
+  set -g status-right "#[fg=#ffffff]#S "
+  set -g status-justify centre
+  set -g window-status-current-format "#[fg=#ffffff]#W"
+  set -g window-status-format "#[fg=#7a7c9e]#W"
+  set -g window-status-separator "  "
+  '';
+
+  programs.tmux.plugins = [
+    { plugin = pkgs.tmuxPlugins.resurrect; }
+  ];
+}

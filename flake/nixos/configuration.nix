6f45632c82d8ed70368385626c6b58576697435a
Runeword <cha.cascio@gmail.com>
2023-02-11 20:33:54 +0100

    Update flake/nixos/configuration.nix

--- flake/nixos/configuration.nix
+++ flake/nixos/configuration.nix
@@ -39,6 +39,8 @@
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
+  programs.ssh.startAgent = true;
+
   time.timeZone = "Europe/Paris";
   i18n.defaultLocale = "en_US.UTF-8";
   i18n.extraLocaleSettings = {

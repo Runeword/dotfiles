d39971e3597a5a8149e784a19d47a23c1a274c48
Runeword <cha.cascio@gmail.com>
2023-02-17 18:16:35 +0100

    Add flake/home-manager/charles/programs/neovim.nix

---
+++ flake/home-manager/charles/programs/neovim.nix
@@ -0,0 +1,44 @@
+{ config, pkgs, lib, ... }: {
+  programs.neovim.enable = true;
+  programs.neovim.defaultEditor = true;
+  programs.neovim.viAlias = false;
+  programs.neovim.withNodeJs = true;
+  programs.neovim.withPython3 = true;
+  programs.neovim.package = pkgs.neovim-nightly;
+
+  # install language servers
+  programs.neovim.extraPackages = with pkgs; [
+  nodePackages.vls
+  nodePackages.typescript-language-server
+  nodePackages.eslint
+  nodePackages.vscode-langservers-extracted
+  nodePackages.bash-language-server
+  nodePackages.yaml-language-server
+  sumneko-lua-language-server
+  marksman
+  nil
+  ];
+
+  # programs.neovim.extraConfig = "luafile ~/flake/config/nvim/init.lua";
+
+  # let
+  #   fromGitHub = ref: repo: pkgs.vimUtils.buildVimPluginFrom2Nix {
+  #     pname = "${lib.strings.sanitizeDerivationName repo}";
+  #     version = ref;
+  #     src = builtins.fetchGit {
+  #       url = "https://github.com/${repo}.git";
+  #       ref = ref;
+  #     };
+  #   };
+  # in {
+  # programs.neovim.plugins = with pkgs.vimPlugins; [
+  #   nvim-lspconfig
+  #   nvim-treesitter
+  #   plenary-nvim
+  #   (fromGitHub "HEAD" "itchyny/vim-cursorword")
+  #   (fromGitHub "HEAD" "windwp/nvim-ts-autotag")
+  #   (fromGitHub "HEAD" "tommcdo/vim-exchange")
+  #   (fromGitHub "HEAD" "kyazdani42/nvim-web-devicons")
+  #   (fromGitHub "HEAD" "kana/vim-arpeggio")
+  # ];
+}

e69323bb15e7d88ec6a1c35c0d9cc5374705b31b
Runeword <cha.cascio@gmail.com>
2023-02-17 19:26:50 +0100

    Update flake/flake.nix

--- flake/flake.nix
+++ flake/flake.nix
@@ -1,12 +1,12 @@
 {
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+
   inputs.home-manager.url = "github:nix-community/home-manager";
   inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
   inputs.neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
   # inputs.neovim-nightly-overlay.inputs.nixpkgs.url = "github:nixos/nixpkgs?rev=fad51abd42ca17a60fc1d4cb9382e2d79ae31836";
-
-  inputs.nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
   outputs = { self, ... }@inputs:
   let pkgs = import inputs.nixpkgs {

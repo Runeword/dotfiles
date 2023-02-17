469bcca7263c396a8f1a263510ee9c41b4e7e3c7
Runeword <cha.cascio@gmail.com>
2023-02-17 19:48:57 +0100

    Update flake/home-manager/charles/programs/default.nix

--- flake/home-manager/charles/programs/default.nix
+++ flake/home-manager/charles/programs/default.nix
@@ -20,21 +20,5 @@
 
   programs.bash.enable = true;
   programs.bash.enableCompletion = true;
-  programs.bash.bashrcExtra = ''
-    # bind ctrl-n to neovim
-    bind -x '"\C-n":"nvim"'
-
-    # unbind alt-number
-    for i in "-" {0..9}; do bind -r "\e$i"; done
-
-    # unbind ctrl-s and ctrl-q (terminal scroll lock)
-    stty -ixon
-
-    if ! pgrep -u "$USER" ssh-agent > /dev/null; then
-        ssh-agent -t 2h > "$XDG_RUNTIME_DIR/ssh-agent.env"
-    fi
-    if [[ ! "$SSH_AUTH_SOCK" ]]; then
-        source "$XDG_RUNTIME_DIR/ssh-agent.env" >/dev/null
-    fi
-  '';
+  programs.bash.bashrcExtra = builtins.readFile ../extra/.bashrc;
 }

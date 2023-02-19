b424bc470d0041eedc8cb2074ab016846e3d00fe
Runeword <cha.cascio@gmail.com>
2023-02-19 19:16:56 +0100

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -141,29 +141,9 @@ require("lazy").setup({
     lazy = false,
     priority = 1000,
     init = function()
-      -- autocommand will have no effect on previously sourced colorschemes
-      -- so it must be added before any colorscheme is sourced
-      require('autocmd').bufferline()
-      require('autocmd').sj()
-      require('autocmd').matchup()
-
-      vim.g.starry_italic_comments = false
-      vim.g.starry_italic_string = false
-      vim.g.starry_italic_keywords = true
-      vim.g.starry_italic_functions = false
-      vim.g.starry_italic_variables = false
-      vim.g.starry_contrast = false
-      vim.g.starry_borders = false
-      vim.g.starry_disable_background = true
-      vim.g.starry_style_fix = true
-      vim.g.starry_darker_contrast = false
-      vim.g.starry_deep_black = false
-      vim.g.starry_set_hl = false
-      vim.g.starry_daylight_switch = false
-    end,
-    config = function()
+      require("setup").starry()
       vim.cmd('colorscheme moonlight')
-    end
+    end,
   },
 
   {
@@ -443,6 +423,7 @@ require("lazy").setup({
     end,
   }
 },
+
   {
     defaults = { lazy = false },
     install = { colorscheme = { "tokyonight", "habamax" } },

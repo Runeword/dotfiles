5de24e1c6bf31d604215246229522cb89ac9cf05
Runeword <cha.cascio@gmail.com>
2023-02-21 16:10:15 +0100

    Update .config/qmk/qmk_firmware/keyboards/ferris/keymaps/Runeword/keymap.c

--- dot_config/qmk/qmk_firmware/keyboards/ferris/keymaps/Runeword/keymap.c
+++ dot_config/qmk/qmk_firmware/keyboards/ferris/keymaps/Runeword/keymap.c
@@ -1,7 +1,7 @@
 #include QMK_KEYBOARD_H
 
-// US layout altgr-intl variant with french special characters
 enum custom_keycodes {
+    // US layout altgr-intl variant with french special characters
     GRAVEA = SAFE_RANGE,
     GRAVEE,
     GRAVEU,
@@ -13,60 +13,84 @@ enum custom_keycodes {
     TREMAE,
     TREMAU,
     TREMAI,
+    L4LCTL
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
+    case L4LCTL :
+        if (record->event.pressed) {
+          layer_on(4);
+          register_code(KC_LCTL);
+          set_oneshot_mods(MOD_BIT(KC_LCTL));
+          return false;
+        } else {
+          layer_off(4);
+          unregister_code(KC_LCTL);
+          return true;
+        }
+        break;
+
     case GRAVEA:
         if (record->event.pressed) {
         SEND_STRING(SS_RALT("`")"a");
         }
         break;
+
     case GRAVEE:
         if (record->event.pressed) {
         SEND_STRING(SS_RALT("`")"e");
         }
         break;
+
     case GRAVEU:
         if (record->event.pressed) {
         SEND_STRING(SS_RALT("`")"u");
         }
         break;
+
     case CIRCUMFLEXA:
         if (record->event.pressed) {
         SEND_STRING(SS_RALT("6")"a");
         }
         break;
+
     case CIRCUMFLEXO:
         if (record->event.pressed) {
         SEND_STRING(SS_RALT("6")"o");
         }
         break;
+
     case CIRCUMFLEXE:
         if (record->event.pressed) {
         SEND_STRING(SS_RALT("6")"e");
         }
         break;
+
     case CIRCUMFLEXU:
         if (record->event.pressed) {
         SEND_STRING(SS_RALT("6")"u");
         }
         break;
+
     case CIRCUMFLEXI:
         if (record->event.pressed) {
         SEND_STRING(SS_RALT("6")"i");
         }
         break;
+
     case TREMAE:
         if (record->event.pressed) {
           SEND_STRING(SS_RALT(SS_RSFT("\""))"e");
         }
         break;
+
     case TREMAU:
         if (record->event.pressed) {
           SEND_STRING(SS_RALT(SS_RSFT("\""))"u");
         }
         break;
+
     case TREMAI:
         if (record->event.pressed) {
           SEND_STRING(SS_RALT(SS_RSFT("\""))"i");
@@ -145,7 +169,8 @@ combo_t key_combos[COMBO_COUNT] = {
   [L1SFT] = COMBO(l1sft, OSM(MOD_LSFT)),
   [L2ALT] = COMBO(l2alt, OSM(MOD_RALT)),
   [L2CS] = COMBO(l2cs, OSM(MOD_RCTL|MOD_RSFT)),
-  [L2CTL] = COMBO(l2ctl, OSM(MOD_RCTL)),
+  // [L2CTL] = COMBO(l2ctl, OSM(MOD_RCTL)),
+  [L2CTL] = COMBO(l2ctl, L4LCTL),
   [L2GS] = COMBO(l2gs, OSM(MOD_RGUI|MOD_RSFT)),
   [L2GUI] = COMBO(l2gui, OSM(MOD_RGUI)),
   [L2SFT] = COMBO(l2sft, OSM(MOD_RSFT)),
@@ -180,12 +205,13 @@ combo_t key_combos[COMBO_COUNT] = {
 // 	[3] = LAYOUT_split_3x5_2(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_9, KC_5, KC_6, KC_7, KC_8, KC_LALT, KC_LGUI, KC_LSFT, KC_LCTL, KC_NO, KC_0, KC_1, KC_2, KC_3, KC_4, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PEQL, KC_PPLS, KC_PMNS, KC_PSLS, KC_PAST, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
 // };
 
- const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[0] = LAYOUT_split_3x5_2(KC_QUOT, KC_COMM, KC_DOT, KC_P, KC_Y, KC_F, KC_G, KC_C, KC_R, KC_L, KC_A, KC_O, KC_E, KC_U, KC_I, KC_D, KC_H, KC_T, KC_N, KC_S, KC_SCLN, KC_Q, KC_J, KC_K, KC_X, KC_B, KC_M, KC_W, KC_V, KC_Z, LT(3,KC_ESC), LT(1,KC_TAB), LT(2,KC_ENT), KC_SPC),
-	[1] = LAYOUT_split_3x5_2(KC_GRV, KC_MINS, KC_EQL, KC_PLUS, KC_NO, KC_AT, KC_LBRC, KC_RBRC, KC_SLSH, KC_BSLS, KC_EXLM, KC_QUES, KC_HASH, KC_ASTR, KC_NO, KC_TILD, KC_LPRN, KC_RPRN, KC_CIRC, KC_DLR, KC_AMPR, KC_PIPE, KC_UNDS, KC_PERC, KC_NO, KC_NO, KC_LCBR, KC_RCBR, KC_NO, KC_NO, KC_TRNS, KC_TRNS, LT(3,KC_ENT), KC_TRNS),
-	[2] = LAYOUT_split_3x5_2(KC_HOME, KC_END, KC_PGDN, KC_PGUP, KC_INS, KC_NO, KC_MPRV, KC_MNXT, KC_MPLY, KC_MUTE, KC_LEFT, KC_RGHT, KC_DOWN, KC_UP, KC_CAPS, KC_NO, KC_RCTL, KC_RSFT, KC_RGUI, KC_RALT, KC_WH_L, KC_WH_R, KC_WH_D, KC_WH_U, KC_NO, KC_NO, KC_BRIU, KC_BRID, KC_VOLU, KC_VOLD, KC_DEL, LT(3,KC_BSPC), KC_TRNS, KC_TRNS),
-	[3] = LAYOUT_split_3x5_2(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_9, KC_5, KC_6, KC_7, KC_8, KC_LALT, KC_LGUI, KC_LSFT, KC_LCTL, KC_NO, KC_0, KC_1, KC_2, KC_3, KC_4, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PEQL, KC_PPLS, KC_PMNS, KC_PSLS, KC_PAST, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
-	[4] = LAYOUT_split_3x5_2(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PPLS, KC_EQL, KC_MINS, KC_GRV, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PAST, KC_HASH, KC_QUES, KC_EXLM, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PERC, KC_UNDS, KC_PIPE, KC_AMPR, KC_NO, KC_NO, KC_NO, KC_NO)
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT_split_3x5_2(KC_QUOT, KC_COMM, KC_DOT, KC_P, KC_Y, KC_F, KC_G, KC_C, KC_R, KC_L, KC_A, KC_O, KC_E, KC_U, KC_I, KC_D, KC_H, KC_T, KC_N, KC_S, KC_SCLN, KC_Q, KC_J, KC_K, KC_X, KC_B, KC_M, KC_W, KC_V, KC_Z, LT(3,KC_ESC), LT(1,KC_TAB), LT(2,KC_ENT), KC_SPC),
+    [1] = LAYOUT_split_3x5_2(KC_GRV, KC_MINS, KC_EQL, KC_PLUS, KC_NO, KC_AT, KC_LBRC, KC_RBRC, KC_SLSH, KC_BSLS, KC_EXLM, KC_QUES, KC_HASH, KC_ASTR, KC_NO, KC_TILD, KC_LPRN, KC_RPRN, KC_CIRC, KC_DLR, KC_AMPR, KC_PIPE, KC_UNDS, KC_PERC, KC_NO, KC_NO, KC_LCBR, KC_RCBR, KC_NO, KC_NO, KC_TRNS, KC_TRNS, LT(3,KC_ENT), KC_TRNS),
+    [2] = LAYOUT_split_3x5_2(KC_HOME, KC_END, KC_PGDN, KC_PGUP, KC_INS, KC_NO, KC_MPRV, KC_MNXT, KC_MPLY, KC_MUTE, KC_LEFT, KC_RGHT, KC_DOWN, KC_UP, KC_CAPS, KC_NO, KC_RCTL, KC_RSFT, KC_RGUI, KC_RALT, KC_WH_L, KC_WH_R, KC_WH_D, KC_WH_U, KC_NO, KC_NO, KC_BRIU, KC_BRID, KC_VOLU, KC_VOLD, KC_DEL, LT(3,KC_BSPC), KC_TRNS, KC_TRNS),
+    [3] = LAYOUT_split_3x5_2(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_9, KC_5, KC_6, KC_7, KC_8, KC_LALT, KC_LGUI, KC_LSFT, KC_LCTL, KC_NO, KC_0, KC_1, KC_2, KC_3, KC_4, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_PEQL, KC_PPLS, KC_PMNS, KC_PSLS, KC_PAST, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
+    [4] = LAYOUT_split_3x5_2(KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_LCTL, KC_LSFT, KC_LGUI, KC_LALT, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_NO, KC_NO),
+    [5] = LAYOUT_split_3x5_2(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LALT, KC_LGUI, KC_LSFT, KC_LCTL, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO, KC_NO, KC_TRNS, KC_TRNS)
 };
 
 // layer_state_t layer_state_set_user(layer_state_t state) {

e3ab358cbd3925dc8c3ace19859d421f9a1cc4eb
Runeword <cha.cascio@gmail.com>
2023-03-04 18:33:24 +0100

    Update .config/Thunar/accels.scm

--- dot_config/private_Thunar/accels.scm
+++ dot_config/private_Thunar/accels.scm
@@ -2,13 +2,13 @@
 ; this file is an automated accelerator map dump
 ;
 (gtk_accel_path "<Actions>/ThunarActionManager/move-to-trash" "Delete")
-(gtk_accel_path "<Actions>/ThunarWindow/open-parent" "h")
-(gtk_accel_path "<Actions>/ThunarStandardView/rename" "<Primary>r")
+(gtk_accel_path "<Actions>/ThunarWindow/zoom-reset" "<Primary>KP_Equal")
+(gtk_accel_path "<Actions>/ThunarWindow/switch-next-tab" "<Primary>Tab")
 (gtk_accel_path "<Actions>/ThunarActionManager/open" "l")
 (gtk_accel_path "<Actions>/ThunarWindow/switch-previous-tab" "<Primary><Shift>ISO_Left_Tab")
 (gtk_accel_path "<Actions>/ThunarStandardView/duplicate" "<Primary>d")
 (gtk_accel_path "<Actions>/ThunarActionManager/execute" "Return")
 (gtk_accel_path "<Actions>/ThunarWindow/reload" "<Primary>l")
-(gtk_accel_path "<Actions>/ThunarWindow/switch-next-tab" "<Primary>Tab")
-(gtk_accel_path "<Actions>/ThunarWindow/zoom-reset" "<Primary>KP_Equal")
+(gtk_accel_path "<Actions>/ThunarStandardView/rename" "<Primary>r")
+(gtk_accel_path "<Actions>/ThunarWindow/open-parent" "h")
 (gtk_accel_path "<Actions>/ThunarWindow/show-hidden" "period")

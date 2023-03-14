f5143fa63e86719af3717e32f289c455788e9190
Runeword <cha.cascio@gmail.com>
2023-03-14 22:34:17 +0100

    Update flake/nixos/hardware-configuration.nix

--- flake/nixos/hardware-configuration.nix
+++ flake/nixos/hardware-configuration.nix
@@ -4,30 +4,24 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports =
-    [ (modulesPath + "/installer/scan/not-detected.nix")
-    ];
+  imports = [
+  (modulesPath + "/installer/scan/not-detected.nix")
+  ];
 
   boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/febf3a5b-22df-4c5b-9a8f-2a52133e40dc";
-      fsType = "ext4";
-    };
+  fileSystems."/".device = "/dev/disk/by-uuid/febf3a5b-22df-4c5b-9a8f-2a52133e40dc";
+  fileSystems."/".fsType = "ext4";
 
   boot.initrd.luks.devices."luks-e7b38457-61ac-441d-ab46-db469f456336".device = "/dev/disk/by-uuid/e7b38457-61ac-441d-ab46-db469f456336";
 
-  fileSystems."/boot/efi" =
-    { device = "/dev/disk/by-uuid/A7CB-42E9";
-      fsType = "vfat";
-    };
+  fileSystems."/boot/efi".device = "/dev/disk/by-uuid/A7CB-42E9";
+  fileSystems."/boot/efi".fsType = "vfat";
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/35362b83-6c2d-4749-90e2-116c3f06f450"; }
-    ];
+  swapDevices = [ { device = "/dev/disk/by-uuid/35362b83-6c2d-4749-90e2-116c3f06f450"; } ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's

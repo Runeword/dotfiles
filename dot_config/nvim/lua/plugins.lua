cdf6ecdc854eed552ecee4c730914a869fc73b0b
Runeword <cha.cascio@gmail.com>
2023-03-17 18:58:06 +0100

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,16 +1,12 @@
 return {
-  {
-    enabled = true,
-    'woosaaahh/sj.nvim',
+  { 'woosaaahh/sj.nvim',
     config = function()
       require('setup').sj()
       require('mappings').sj()
     end,
   },
 
-  {
-    enabled = true,
-    'andymass/vim-matchup',
+  { 'andymass/vim-matchup',
     init = function()
       require('options').matchup()
     end,
@@ -19,24 +15,11 @@ return {
     end,
   },
 
-  {
-    enabled = true,
-    'nvim-lua/plenary.nvim',
-  },
+  { 'nvim-lua/plenary.nvim', },
+  { 'kyazdani42/nvim-web-devicons', },
+  { 'kana/vim-arpeggio', },
 
-  {
-    enabled = true,
-    'kyazdani42/nvim-web-devicons',
-  },
-
-  {
-    enabled = true,
-    'kana/vim-arpeggio',
-  },
-
-  {
-    enabled = true,
-    'dhruvasagar/vim-table-mode',
+  { 'dhruvasagar/vim-table-mode',
     init = function()
       require('setup').tablemode()
     end,
@@ -45,15 +28,11 @@ return {
     end,
   },
 
-  {
-    enabled = true,
-    'mattn/vim-gist',
+  { 'mattn/vim-gist',
     dependencies = { 'mattn/webapi-vim' },
   },
 
-  {
-    enabled = true,
-    'willothy/flatten.nvim',
+  { 'willothy/flatten.nvim',
     config = true,
     lazy = false,
     priority = 1001,
@@ -68,9 +47,7 @@ return {
   --   end,
   -- },
 
-  {
-    enabled = true,
-    'michaelb/sniprun',
+  { 'michaelb/sniprun',
     build = 'bash ./install.sh',
     config = function()
       require('setup').sniprun()
@@ -78,52 +55,41 @@ return {
     end,
   },
 
-  {
-    enabled = true,
-    'rareitems/printer.nvim',
+  { 'rareitems/printer.nvim',
     config = function()
       require('setup').printer()
       require('mappings').printer()
     end,
   },
 
-  {
-    enabled = true,
-    'mfussenegger/nvim-dap',
+  { 'mfussenegger/nvim-dap',
     config = function()
       require('mappings').dap()
       require('setup.dap')
     end,
   },
 
-  {
-    enabled = true,
-    'akinsho/bufferline.nvim',
+  { 'akinsho/bufferline.nvim',
     config = function()
       require('mappings').bufferline()
       require('setup').bufferline()
     end,
   },
 
-  {
-    enabled = true,
-    'inside/vim-search-pulse',
+  { 'inside/vim-search-pulse',
     config = function()
       require('mappings').pulse()
     end,
   },
 
-  {
-    enabled = true,
-    'chrisgrieser/nvim-various-textobjs',
+  { 'chrisgrieser/nvim-various-textobjs',
     config = function()
       require('setup').varioustextobjs()
       require('mappings').varioustextobjs()
     end,
   },
 
-  {
-    'ray-x/starry.nvim',
+  { 'ray-x/starry.nvim',
     lazy = false,
     priority = 1000,
     init = function()
@@ -131,41 +97,31 @@ return {
     end,
   },
 
-  {
-    enabled = true,
-    'nvim-treesitter/nvim-treesitter',
+  { 'nvim-treesitter/nvim-treesitter',
     config = function()
       require('setup').treesitter()
     end,
     build = ':TSUpdate',
   },
 
-  {
-    enabled = true,
-    'neovim/nvim-lspconfig',
+  { 'neovim/nvim-lspconfig',
     config = require('setup.lsp'),
   },
 
-  {
-    enabled = true,
-    'anuvyklack/hydra.nvim',
+  { 'anuvyklack/hydra.nvim',
     dependencies = 'anuvyklack/keymap-layer.nvim',
     config = function()
       require('mappings').hydra()
     end,
   },
 
-  {
-    enabled = true,
-    'smjonas/live-command.nvim',
+  { 'smjonas/live-command.nvim',
     config = function()
       require('setup').livecommand()
     end,
   },
 
-  {
-    enabled = true,
-    'ms-jpq/coq_nvim',
+  { 'ms-jpq/coq_nvim',
     branch = 'coq',
     init = function()
       require('setup').coq()
@@ -176,42 +132,32 @@ return {
     end,
   },
 
-  {
-    enabled = true,
-    'is0n/fm-nvim',
+  { 'is0n/fm-nvim',
     config = function()
       require('mappings').fm()
       require('setup').fm()
     end,
   },
 
-  {
-    enabled = true,
-    'lewis6991/gitsigns.nvim',
+  { 'lewis6991/gitsigns.nvim',
     config = function()
       require('setup').gitsigns()
     end,
   },
 
-  {
-    enabled = true,
-    'gbprod/stay-in-place.nvim',
+  { 'gbprod/stay-in-place.nvim',
     config = function()
       require('setup').stayinplace()
     end,
   },
 
-  {
-    enabled = true,
-    'norcalli/nvim-colorizer.lua',
+  { 'norcalli/nvim-colorizer.lua',
     config = function()
       require('setup').colorizer()
     end,
   },
 
-  {
-    enabled = true,
-    'weilbith/nvim-code-action-menu',
+  { 'weilbith/nvim-code-action-menu',
     config = function()
       require('mappings').codeactionmenu()
       require('setup').codeactionmenu()
@@ -219,17 +165,13 @@ return {
     cmd = 'CodeActionMenu',
   },
 
-  {
-    enabled = true,
-    'nacro90/numb.nvim',
+  { 'nacro90/numb.nvim',
     config = function()
       require('setup').numb()
     end,
   },
 
-  {
-    enabled = true,
-    'echasnovski/mini.nvim',
+  { 'echasnovski/mini.nvim',
     config = function()
       require('autocmd').indentscope()
       require('setup').indentscope()
@@ -237,125 +179,81 @@ return {
     end,
   },
 
-  {
-    enabled = true,
-    'nvim-treesitter/nvim-treesitter-textobjects',
-  },
+  { 'nvim-treesitter/nvim-treesitter-textobjects', },
+  { 'windwp/nvim-ts-autotag', },
+  { 'itchyny/vim-cursorword', },
+  { 'tommcdo/vim-exchange', },
 
-  {
-    enabled = true,
-    'windwp/nvim-ts-autotag',
-  },
-
-  {
-    enabled = true,
-    'itchyny/vim-cursorword',
-  },
-
-  {
-    enabled = true,
-    'tommcdo/vim-exchange',
-  },
-
-  {
-    enabled = true,
-    'numToStr/Comment.nvim',
+  { 'numToStr/Comment.nvim',
     config = function()
       require('setup').comment()
       require('mappings').comment()
     end,
   },
 
-  {
-    enabled = true,
-    'smjonas/inc-rename.nvim',
+  { 'smjonas/inc-rename.nvim',
     config = function()
       require('inc_rename').setup()
     end,
   },
 
-  {
-    enabled = true,
-    'AndrewRadev/splitjoin.vim',
+  { 'AndrewRadev/splitjoin.vim',
     config = function()
       require('mappings').splitjoin()
     end,
   },
 
-  {
-    enabled = true,
-    'windwp/nvim-autopairs',
+  { 'windwp/nvim-autopairs',
     config = function()
       require('setup').autopairs()
     end,
   },
 
-  {
-    enabled = true,
-    'ahmedkhalf/project.nvim',
+  { 'ahmedkhalf/project.nvim',
     config = function()
       require('setup').project()
     end,
   },
 
-  {
-    enabled = true,
-    'kosayoda/nvim-lightbulb',
+  { 'kosayoda/nvim-lightbulb',
     config = function()
       require('setup').lightbulb()
       require('autocmd').lightbulb()
     end,
   },
 
-  {
-    enabled = true,
-    'nvim-lualine/lualine.nvim',
-    -- event = "VeryLazy",
-    -- lazy = true,
+  { 'nvim-lualine/lualine.nvim',
     dependencies = { 'kyazdani42/nvim-web-devicons' },
     config = require('setup.lualine'),
   },
 
-  {
-    enabled = true,
-    'ibhagwan/fzf-lua',
+  { 'ibhagwan/fzf-lua',
     config = function()
       require('mappings').fzf()
       require('setup').fzf()
     end,
   },
 
-  {
-    enabled = true,
-    'monaqa/dial.nvim',
+  { 'monaqa/dial.nvim',
     config = function()
       require('mappings').dial()
       require('setup').dial()
     end,
   },
 
-  {
-    enabled = true,
-    'svban/YankAssassin.vim',
-  },
+  { 'svban/YankAssassin.vim', },
 
-  {
-    enabled = true,
-    'machakann/vim-highlightedyank',
+  { 'machakann/vim-highlightedyank',
     config = function()
       require('setup').highlightedyank()
     end,
   },
 
-  {
-    enabled = true,
-    'glts/vim-textobj-comment',
+  { 'glts/vim-textobj-comment',
     dependencies = 'kana/vim-textobj-user',
   },
 
-  {
-    enabled = true,
-    'D4KU/vim-textobj-chainmember',
+  { 'D4KU/vim-textobj-chainmember',
     dependencies = 'kana/vim-textobj-user',
     init = function()
       require('setup').textobjchainmember()
@@ -365,15 +263,11 @@ return {
     end,
   },
 
-  {
-    enabled = true,
-    'kana/vim-textobj-line',
+  { 'kana/vim-textobj-line',
     dependencies = 'kana/vim-textobj-user',
   },
 
-  {
-    enabled = true,
-    'AndrewRadev/sideways.vim',
+  { 'AndrewRadev/sideways.vim',
     config = function()
       require('mappings').sideways()
     end,
@@ -386,22 +280,15 @@ return {
   --   end,
   -- },
 
-  {
-    enabled = true,
-    'kylechui/nvim-surround',
+  { 'kylechui/nvim-surround',
     config = function()
       require('setup').surround()
     end,
   },
 
-  {
-    enabled = true,
-    'tpope/vim-abolish',
-  },
+  { 'tpope/vim-abolish', },
 
-  {
-    enabled = true,
-    'Runeword/putter.nvim',
+  { 'Runeword/putter.nvim',
     -- "/home/charles/Documents/dev/plugins/putter.nvim",
     config = function()
       require('mappings').putter()

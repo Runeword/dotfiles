7edf39ed3f5b4ba8881aec448968d65df07b7aaa
Runeword <cha.cascio@gmail.com>
2023-03-22 18:22:28 +0100

    Update flake/home-manager/charles/home/default.nix

--- flake/home-manager/charles/home/default.nix
+++ flake/home-manager/charles/home/default.nix
@@ -10,10 +10,12 @@
     xarchiver
     wget
     ripgrep
+    deno
     shopify-cli
     progress
     nodePackages.pnpm
     fd
+    # supabase-cli
     thefuck
     inkscape
     clifm
@@ -34,4 +36,40 @@
     # nodejs-19_x
     # python311
   ];
+
+  nixpkgs.overlays = [ (self: super:
+{
+# supabase-cli = super.supabase-cli.override (old: {
+#     buildGoModule = args: pkgs.buildGoModule (args // {
+#         version = "1.45.1";
+#         src = super.fetchFromGitHub {
+#         owner = "supabase";
+#         repo = "cli";
+#         rev = "v1.45.1";
+#         sha256 = "sha256-fj8kMvHSs1ZOnMgyaWAyIAwzK6LJz0gxhJ+Dpf5hwis=";
+#         };
+#         ldflags = [ "-s" "-w" "-X" "github.com/supabase/cli/cmd.version=1.45.1" ];
+#         vendorSha256 = "sha256-bxqfg77f6BuUy+9KrcToWJRzfwshOdqEQkAq4IFaxS8=";
+#         postInstall = ''
+#             rm $out/bin/{codegen,listdep}
+#             mv $out/bin/{cli,supabase}
+#             installShellCompletion --cmd supabase \
+#               --bash <($out/bin/supabase completion bash) \
+#               --fish <($out/bin/supabase completion fish) \
+#               --zsh <($out/bin/supabase completion zsh)
+#           '';
+#         });
+# });
+
+ # supabase-cli = super.supabase-cli.overrideAttrs (old: {
+ #   src = super.fetchFromGitHub {
+ #     owner = "supabase";
+ #     repo = "cli";
+ #     rev = "v1.45.1";
+ #     sha256 = "sha256-fj8kMvHSs1ZOnMgyaWAyIAwzK6LJz0gxhJ+Dpf5hwis=";
+ #   };
+ #   ldflags = [ "-s" "-w" "-X" "github.com/supabase/cli/cmd.version=1.45.1" ];
+# });
+
+}) ];
 }

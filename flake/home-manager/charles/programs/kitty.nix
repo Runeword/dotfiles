5705ee30f0c98793ea3ff71983e1c12a1c68190c
Runeword <cha.cascio@gmail.com>
2023-03-28 21:22:45 +0200

    Update flake/home-manager/charles/programs/kitty.nix

--- flake/home-manager/charles/programs/kitty.nix
+++ flake/home-manager/charles/programs/kitty.nix
@@ -1,19 +1,42 @@
 {
   programs.kitty.enable = true;
 
+  programs.kitty.font = {
+    name = "Hack Nerd Font";
+    size = 10;
+  };
+
   programs.kitty.settings = {
     background_opacity = "0.7";
-    font_size = 20;
     cursor = "#ffffff";
     cursor_shape = "beam";
     cursor_beam_thickness = "0.1";
     cursor_blink_interval = "0.5";
     modify_font = "cell_height +5px";
+    clear_all_shortcuts = "yes";
   };
 
   programs.kitty.keybindings = {
     "ctrl+kp_plus" = "change_font_size all +2.0";
     "ctrl+kp_minus" = "change_font_size all -2.0";
     "ctrl+kp_equal" = "change_font_size all 0";
+    "ctrl+shift+c" = "copy_to_clipboard";
+    "ctrl+shift+v" = "paste_from_clipboard";
+    "ctrl+tab" = "launch --type background tmux select-window -t+1";
+    "ctrl+shift+tab" = "launch --type background tmux select-window -t-1";
+    "ctrl+page_down" = "launch --type background tmux select-window -t+1";
+    "ctrl+page_up" = "launch --type background tmux select-window -t-1";
+    "ctrl+shift+page_up" = "launch --type background tmux swap-window -t-1 ; select-window -t-1";
+    "ctrl+shift+page_down" = "launch --type background tmux swap-window -t+1 ; select-window -t+1";
+    "ctrl+9" = "launch --type background tmux switch-client -p";
+    "ctrl+0" = "launch --type background tmux switch-client -n";
+    "ctrl+1" = "launch --type background tmux select-window -t1";
+    "ctrl+2" = "launch --type background tmux select-window -t2";
+    "ctrl+3" = "launch --type background tmux select-window -t3";
+    "ctrl+4" = "launch --type background tmux select-window -t4";
+    "ctrl+5" = "launch --type background tmux select-window -t5";
+    "ctrl+6" = "launch --type background tmux select-window -t6";
+    "ctrl+7" = "launch --type background tmux select-window -t7";
+    "ctrl+8" = "launch --type background tmux select-window -t8";
   };
 }

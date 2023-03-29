8073a0a8e28deb57069e6cb275459686b7c47518
Runeword <cha.cascio@gmail.com>
2023-03-29 21:56:24 +0200

    Update flake/home-manager/charles/default.nix

--- flake/home-manager/charles/default.nix
+++ flake/home-manager/charles/default.nix
@@ -2,6 +2,9 @@
   imports = [ ./programs ./home ./services ];
 
   fonts.fontconfig.enable = true;
+
+  xresources.properties."Xft.dpi" = 200; 
+
   # xsession.enable = true;
   # xsession.profileExtra = ''
   # xset r rate 180 40 &

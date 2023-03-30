d5704eccfe5b9a9678fad0fd5da7a5d4ad6ba5f8
Runeword <cha.cascio@gmail.com>
2023-03-30 19:09:16 +0200

    Update flake/home-manager/charles/programs/ssh.nix

--- flake/home-manager/charles/programs/ssh.nix
+++ flake/home-manager/charles/programs/ssh.nix
@@ -21,4 +21,12 @@
     extraOptions.UpdateHostKeys = "no";
     extraOptions.PreferredAuthentications = "publickey";
   };
+
+  programs.ssh.matchBlocks."gitlab.0" = {
+    host = "gitlab.0.com";
+    hostname = "gitlab.com";
+    identityFile = "~/.ssh/id_ed25519_0";
+    extraOptions.UpdateHostKeys = "no";
+    extraOptions.PreferredAuthentications = "publickey";
+  };
 }

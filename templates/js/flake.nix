9f5b0199f4436d55e7703f260de679a169bf9e52
Runeword <cha.cascio@gmail.com>
2023-03-31 19:50:00 +0200

    Update templates/js/flake.nix

--- templates/js/flake.nix
+++ templates/js/flake.nix
@@ -2,14 +2,13 @@
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   inputs.nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
-  outputs = { self, ... }@inputs:
-    let pkgs = import inputs.nixpkgs {
+  outputs = {self, ...} @ inputs: let
+    pkgs = import inputs.nixpkgs {
       system = "x86_64-linux";
       config.allowUnfree = true;
       overlays = [
       ];
     };
-
   in {
     devShells.x86_64-linux = {
       default = pkgs.mkShell {
@@ -21,16 +20,17 @@
       js = pkgs.mkShell {
         packages = [
           pkgs.nodePackages.typescript
-            pkgs.nodejs-19_x
-            pkgs.deno
-            pkgs.nodePackages.pnpm
-            pkgs.nodePackages.npm
+          pkgs.nodejs-19_x
+          pkgs.deno
+          pkgs.nodePackages.pnpm
+          pkgs.nodePackages.npm
         ];
+
         shellHook = ''
-          nl="npm ls --depth=0";
-        nlg="npm ls -g --depth=0";
-        nd="npm run dev";
-        ni="npm i";
+            nl="npm ls --depth=0";
+          nlg="npm ls -g --depth=0";
+          nd="npm run dev";
+          ni="npm i";
         '';
       };
     };

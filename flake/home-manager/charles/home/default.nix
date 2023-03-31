f855e8a003483869dbbba01a3505294f2ab7cdc3
Runeword <cha.cascio@gmail.com>
2023-03-31 19:48:18 +0200

    Update flake/home-manager/charles/home/default.nix

--- flake/home-manager/charles/home/default.nix
+++ flake/home-manager/charles/home/default.nix
@@ -1,5 +1,10 @@
-{ config, pkgs, lib, ... }: {
-  imports = [ ./shellAliases.nix ];
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  imports = [./shellAliases.nix];
   home.username = "charles";
   home.homeDirectory = "/home/charles";
   home.stateVersion = "22.11";
@@ -9,6 +14,7 @@
     qmk
     xarchiver
     wget
+    doppler
     ripgrep
     deno
     shopify-cli
@@ -23,63 +29,65 @@
     rofi
     peek
     gcc
+    colorpicker
     realesrgan-ncnn-vulkan
     slack
     bitwarden-cli
+    nodePackages.firebase-tools
+    # supabase-cli
     google-chrome
     firefox
-    (nerdfonts.override { fonts = [ "Hack" "DroidSansMono" ]; })
+    (nerdfonts.override {fonts = ["Hack" "DroidSansMono"];})
     neovim-nightly
     # python311
   ];
 
-home.sessionVariables = {
-  # FZF_DEFAULT_COMMAND="fd --strip-cwd-prefix --hidden --follow --exclude .git";
-  # FZF_CTRL_T_COMMAND="${config.home.sessionVariables.FZF_DEFAULT_COMMAND}";
-  _ZO_FZF_OPTS = "--reverse --height 40% --no-separator --border none"; # zoxide fzf options
-};
+  home.sessionVariables = {
+    # FZF_DEFAULT_COMMAND="fd --strip-cwd-prefix --hidden --follow --exclude .git";
+    # FZF_CTRL_T_COMMAND="${config.home.sessionVariables.FZF_DEFAULT_COMMAND}";
+    _ZO_FZF_OPTS = "--reverse --height 40% --no-separator --border none"; # zoxide fzf options
+  };
 
-# home.sessionVariables = {
-#   FOO = "Hello";
-#   BAR = "${config.home.sessionVariables.FOO} World!";
-# };
+  # home.sessionVariables = {
+  #   FOO = "Hello";
+  #   BAR = "${config.home.sessionVariables.FOO} World!";
+  # };
 
-nixpkgs.overlays = [
-  # (self: super: {
+  nixpkgs.overlays = [
+    # (self: super: {
 
-# supabase-cli = super.supabase-cli.override (old: {
-#     buildGoModule = args: pkgs.buildGoModule (args // {
-#         version = "1.45.1";
-#         src = super.fetchFromGitHub {
-#         owner = "supabase";
-#         repo = "cli";
-#         rev = "v1.45.1";
-#         sha256 = "sha256-fj8kMvHSs1ZOnMgyaWAyIAwzK6LJz0gxhJ+Dpf5hwis=";
-#         };
-#         ldflags = [ "-s" "-w" "-X" "github.com/supabase/cli/cmd.version=1.45.1" ];
-#         vendorSha256 = "sha256-bxqfg77f6BuUy+9KrcToWJRzfwshOdqEQkAq4IFaxS8=";
-#         postInstall = ''
-#             rm $out/bin/{codegen,listdep}
-#             mv $out/bin/{cli,supabase}
-#             installShellCompletion --cmd supabase \
-#               --bash <($out/bin/supabase completion bash) \
-#               --fish <($out/bin/supabase completion fish) \
-#               --zsh <($out/bin/supabase completion zsh)
-#           '';
-#         });
-# });
+    # supabase-cli = super.supabase-cli.override (old: {
+    #     buildGoModule = args: pkgs.buildGoModule (args // {
+    #         version = "1.45.1";
+    #         src = super.fetchFromGitHub {
+    #         owner = "supabase";
+    #         repo = "cli";
+    #         rev = "v1.45.1";
+    #         sha256 = "sha256-fj8kMvHSs1ZOnMgyaWAyIAwzK6LJz0gxhJ+Dpf5hwis=";
+    #         };
+    #         ldflags = [ "-s" "-w" "-X" "github.com/supabase/cli/cmd.version=1.45.1" ];
+    #         vendorSha256 = "sha256-bxqfg77f6BuUy+9KrcToWJRzfwshOdqEQkAq4IFaxS8=";
+    #         postInstall = ''
+    #             rm $out/bin/{codegen,listdep}
+    #             mv $out/bin/{cli,supabase}
+    #             installShellCompletion --cmd supabase \
+    #               --bash <($out/bin/supabase completion bash) \
+    #               --fish <($out/bin/supabase completion fish) \
+    #               --zsh <($out/bin/supabase completion zsh)
+    #           '';
+    #         });
+    # });
 
- # supabase-cli = super.supabase-cli.overrideAttrs (old: {
- #   src = super.fetchFromGitHub {
- #     owner = "supabase";
- #     repo = "cli";
- #     rev = "v1.45.1";
- #     sha256 = "sha256-fj8kMvHSs1ZOnMgyaWAyIAwzK6LJz0gxhJ+Dpf5hwis=";
- #   };
- #   ldflags = [ "-s" "-w" "-X" "github.com/supabase/cli/cmd.version=1.45.1" ];
-# });
+    # supabase-cli = super.supabase-cli.overrideAttrs (old: {
+    #   src = super.fetchFromGitHub {
+    #     owner = "supabase";
+    #     repo = "cli";
+    #     rev = "v1.45.1";
+    #     sha256 = "sha256-fj8kMvHSs1ZOnMgyaWAyIAwzK6LJz0gxhJ+Dpf5hwis=";
+    #   };
+    #   ldflags = [ "-s" "-w" "-X" "github.com/supabase/cli/cmd.version=1.45.1" ];
+    # });
 
-# })
-];
-
+    # })
+  ];
 }

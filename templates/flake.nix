ff56a0b2af1d1011bc65efcf6e9be6ac61adc13d
Runeword <60324746+Runeword@users.noreply.github.com>
2023-04-12 16:02:13 +0200

    Update templates/flake.nix

--- templates/flake.nix
+++ templates/flake.nix
@@ -2,5 +2,9 @@
   outputs = { self }: {
     templates.js.path = ./js;
     templates.js.welcomeText = "js";
+    templates.proto.path = ./proto;
+    templates.proto.welcomeText = "proto";
+    templates.firebase.path = ./firebase;
+    templates.firebase.welcomeText = "firebase";
   };
 }

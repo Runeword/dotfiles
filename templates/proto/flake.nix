3fe74f301fae88a6c1cecdc2b7abcc8661b90c30
Runeword <60324746+Runeword@users.noreply.github.com>
2023-04-12 16:27:46 +0200

    Update templates/proto/flake.nix

--- templates/proto/flake.nix
+++ templates/proto/flake.nix
@@ -4,19 +4,45 @@
   outputs = {self, ...} @ inputs: let
     pkgs = import inputs.nixpkgs {
       system = "x86_64-linux";
+      overlays = [
+        (self: super: {
+          supabase-cli = super.supabase-cli.override (old: {
+            buildGoModule = args:
+              pkgs.buildGoModule (args
+                // {
+                  version = "1.45.1";
+                  src = super.fetchFromGitHub {
+                    owner = "supabase";
+                    repo = "cli";
+                    rev = "v1.45.1";
+                    sha256 = "sha256-fj8kMvHSs1ZOnMgyaWAyIAwzK6LJz0gxhJ+Dpf5hwis=";
+                  };
+                  ldflags = ["-s" "-w" "-X" "github.com/supabase/cli/cmd.version=1.45.1"];
+                  vendorSha256 = "sha256-bxqfg77f6BuUy+9KrcToWJRzfwshOdqEQkAq4IFaxS8=";
+                  postInstall = ''
+                    rm $out/bin/{codegen,listdep}
+                    mv $out/bin/{cli,supabase}
+                    installShellCompletion --cmd supabase \
+                      --bash <($out/bin/supabase completion bash) \
+                      --fish <($out/bin/supabase completion fish) \
+                      --zsh <($out/bin/supabase completion zsh)
+                  '';
+                });
+          });
+        })
+      ];
     };
   in {
-    devShells.x86_64-linux = {
-      default = pkgs.mkShell {
-        packages = [
-          pkgs.nodejs-19_x
-          pkgs.deno
-          pkgs.nodePackages.typescript
-          pkgs.nodePackages.pnpm
-          pkgs.nodePackages.npm
-          pkgs.shopify-cli
-        ];
-      };
+    devShells.x86_64-linux.default = pkgs.mkShell {
+      packages = [
+        pkgs.nodejs-19_x
+        pkgs.deno
+        pkgs.nodePackages.typescript
+        pkgs.nodePackages.pnpm
+        pkgs.nodePackages.npm
+        pkgs.supabase-cli
+        pkgs.shopify-cli
+      ];
     };
   };
 }

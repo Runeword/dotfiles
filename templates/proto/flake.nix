52b09a1a159e5db1a3c903d1392df8c9d54699be
Runeword <60324746+Runeword@users.noreply.github.com>
2023-04-12 15:25:52 +0200

    Add templates/proto/flake.nix

---
+++ templates/proto/flake.nix
@@ -0,0 +1,22 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs = {self, ...} @ inputs: let
+    pkgs = import inputs.nixpkgs {
+      system = "x86_64-linux";
+    };
+  in {
+    devShells.x86_64-linux = {
+      default = pkgs.mkShell {
+        packages = [
+          pkgs.nodejs-19_x
+          pkgs.deno
+          pkgs.nodePackages.typescript
+          pkgs.nodePackages.pnpm
+          pkgs.nodePackages.npm
+          pkgs.shopify-cli
+        ];
+      };
+    };
+  };
+}

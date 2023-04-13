2d3fe4e9096eb9892b11034fde3ebc1fd41aff4b
Runeword <60324746+Runeword@users.noreply.github.com>
2023-04-13 23:31:07 +0200

    Update flake/home-manager/charles/home/shellAliases.nix

--- flake/home-manager/charles/home/shellAliases.nix
+++ flake/home-manager/charles/home/shellAliases.nix
@@ -63,16 +63,32 @@
     blc = "bluetoothctl devices Connected";
     blt = "bluetoothctl devices Trusted";
     bl = ''
-    bluetoothctl devices | fzf \
+    choice1=$(bluetoothctl devices | fzf \
     --preview 'bluetoothctl info {2} | head -$FZF_PREVIEW_LINES' \
     --preview-window right,65%,noborder \
     --no-scrollbar \
-    --header-first \
-    --header='C-p pair  C-c connect  C-t trust' \
-    --bind='ctrl-p:preview:bluetoothctl info {2} | grep "Paired: yes" -q && bluetoothctl remove {2} || bluetoothctl pair {2}' \
-    --bind='ctrl-c:preview:bluetoothctl info {2} | grep "Connected: yes" -q && bluetoothctl disconnect {2} || bluetoothctl connect {2}' \
-    --bind='ctrl-t:preview:bluetoothctl info {2} | grep "Trusted: yes" -q && bluetoothctl untrust {2} || bluetoothctl trust {2}'
+    --bind='enter:execute(echo {2})+abort'
+    ); \
+    [ -n "$choice1" ] || exit 0
+    choice2=$(echo -e "connect\ndisconnect\npair\nunpair\ntrust\nuntrust" | fzf \
+    --preview "bluetoothctl info $choice1 | head -$FZF_PREVIEW_LINES" \
+    --preview-window right,65%,noborder \
+    --no-scrollbar \
+    --bind='enter:execute(echo {1})+abort'
+    ); \
+    [ -n "$choice2" ] && [ -n "$choice1" ] && bluetoothctl $choice2 $choice1
     '';
+    # bl = ''
+    # bluetoothctl devices | fzf \
+    # --preview 'bluetoothctl info {2} | head -$FZF_PREVIEW_LINES' \
+    # --preview-window right,65%,noborder \
+    # --no-scrollbar \
+    # --header-first \
+    # --header='C-p pair  C-c connect  C-t trust' \
+    # --bind='ctrl-p:preview:bluetoothctl info {2} | grep "Paired: yes" -q && bluetoothctl remove {2} || bluetoothctl pair {2}' \
+    # --bind='ctrl-c:preview:bluetoothctl info {2} | grep "Connected: yes" -q && bluetoothctl disconnect {2} || bluetoothctl connect {2}' \
+    # --bind='ctrl-t:preview:bluetoothctl info {2} | grep "Trusted: yes" -q && bluetoothctl untrust {2} || bluetoothctl trust {2}'
+    # ''
 
     # ______________________________________ NETWORK
     wo= "nmcli radio wifi on";

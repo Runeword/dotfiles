43d08d724e886b7eea6e9ce81f723c5f000769a7
Runeword <60324746+Runeword@users.noreply.github.com>
2023-05-11 17:41:47 +0200

    Update flake/home-manager/charles/extra/shellFunctions/chezmoi.sh

--- flake/home-manager/charles/extra/shellFunctions/chezmoi.sh
+++ flake/home-manager/charles/extra/shellFunctions/chezmoi.sh
@@ -1,4 +1,23 @@
+select_files() {
+  echo "$1" | fzf \
+    --multi --inline-info --cycle --height 70% \
+    --preview 'chezmoi diff --reverse --color=true ~/{}' \
+    --preview-window bottom,80%,noborder
+}
+
 cha() {
+  files=$(chezmoi status | awk '{print $2}')
+  [ -z "$files" ] && return 1
+
+  selected_files=($select_files $files)
+  [ -z "$selected_files" ] && return 1
+
+  for i in $selected_files; do
+    chezmoi add $i
+  done
+}
+
+chy() {
   files=$(chezmoi status | awk '{print $2}')
   [ -z "$files" ] && return 1
 
@@ -10,6 +29,22 @@ cha() {
   [ -z "$selected_files" ] && return 1
 
   for i in $selected_files; do
-    chezmoi add $i
+    chezmoi apply $i
+  done
+}
+
+chf() {
+  files=$(chezmoi managed)
+  [ -z "$files" ] && return 1
+
+  selected_files=$(echo "$files" | fzf \
+    --multi --inline-info --cycle --height 70% \
+    --preview 'chezmoi diff --reverse --color=true ~/{}' \
+    --preview-window bottom,80%,noborder
+  )
+  [ -z "$selected_files" ] && return 1
+
+  for i in $selected_files; do
+    chezmoi forget $i
   done
 }

5cc3a6d2183bd4a9b009710dd5267b27f9fe986f
Runeword <60324746+Runeword@users.noreply.github.com>
2023-05-13 22:46:10 +0200

    Update flake/home-manager/charles/home/shellAliases.nix

--- flake/home-manager/charles/home/shellAliases.nix
+++ flake/home-manager/charles/home/shellAliases.nix
@@ -21,7 +21,8 @@
     c = "cd $(fd --type directory --hidden --follow --no-ignore --exclude .git --exclude node_modules | fzf --inline-info --cycle --preview 'ls -AxF {} | head -$FZF_PREVIEW_LINES' --preview-window right,50%,noborder --no-scrollbar)";
     ca = "cd $(fd --type directory --hidden --follow --no-ignore | fzf --cycle)";
     o = "xdg-open $(fd --type file --hidden --follow --no-ignore --exclude .git --exclude node_modules | fzf)";
-    s = "src search";
+    s = "source";
+    ss = "src search";
 
     # ______________________________________ NIX
     hs = "home-manager switch --flake $HOME/flake";

fedded5d08f3059866c310664866a83a7d674a54
Runeword <60324746+Runeword@users.noreply.github.com>
2023-05-14 14:24:00 +0200

    Update flake/home-manager/charles/programs/readline.nix

--- flake/home-manager/charles/programs/readline.nix
+++ flake/home-manager/charles/programs/readline.nix
@@ -2,11 +2,12 @@
   programs.readline.enable = true;
   programs.readline.includeSystemConfig = false;
   programs.readline.variables.bind-tty-special-chars = false; # override default readline bindings
-  programs.readline.variables.show-all-if-ambiguous = true;
+  programs.readline.variables.show-all-if-ambiguous = true; # show all tab-completion options on first tab
   programs.readline.variables.completion-ignore-case = true;
 
   programs.readline.bindings = {
     "\\C-i" = "menu-complete"; # tab
+    "\\e[Z" = "menu-complete-backward"; # shift + tab
     "\\C-u" = "kill-whole-line"; # ctrl + u
     "\\C-\\b" = "backward-kill-word"; # ctrl + backspace
     "\\e[A" = "history-search-backward"; # arrow up

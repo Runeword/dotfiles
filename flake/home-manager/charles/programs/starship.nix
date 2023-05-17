5f010defc78797d8de9b3c969dcba02fd51a7511
Runeword <60324746+Runeword@users.noreply.github.com>
2023-05-17 23:15:39 +0200

    Update flake/home-manager/charles/programs/starship.nix

--- flake/home-manager/charles/programs/starship.nix
+++ flake/home-manager/charles/programs/starship.nix
@@ -4,6 +4,7 @@
   programs.starship.settings = {
     line_break.disabled = true;
     character.disabled = true;
+    cmd_duration.disabled = true;
     cmd_duration.min_time = 10000;
     cmd_duration.format = "[$duration]($style) ";
     git_status.disabled = true;

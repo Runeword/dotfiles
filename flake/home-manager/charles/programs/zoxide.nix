c2e7ce64275943feb51ae38b5b5936c73f39fd42
Runeword <60324746+Runeword@users.noreply.github.com>
2023-05-23 20:45:44 +0200

    Add flake/home-manager/charles/programs/zoxide.nix

---
+++ flake/home-manager/charles/programs/zoxide.nix
@@ -0,0 +1,7 @@
+{
+  programs.zoxide.enable = true;
+  programs.zoxide.enableBashIntegration = true;
+  programs.zoxide.enableFishIntegration = true;
+  programs.zoxide.options = ["--no-cmd"];
+  home.sessionVariables._ZO_FZF_OPTS = "--reverse --height 40% --no-separator --border none"; # fzf options
+}

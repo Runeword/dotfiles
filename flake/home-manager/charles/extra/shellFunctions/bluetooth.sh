207389bdeca545422333bf8a7b11b2ec65f880f6
Runeword <60324746+Runeword@users.noreply.github.com>
2023-05-24 22:06:09 +0200

    Update flake/home-manager/charles/extra/shellFunctions/bluetooth.sh

--- flake/home-manager/charles/extra/shellFunctions/bluetooth.sh
+++ flake/home-manager/charles/extra/shellFunctions/bluetooth.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
 
-bt () {
+__bluetooth () {
 bluetoothctl devices | fzf \
 --preview 'bluetoothctl info {2} | head -$FZF_PREVIEW_LINES' \
 --preview-window right,65%,noborder \
 --no-scrollbar \
 --header-first \
 --header='
-ACTION            RELOAD/FILTER
-C-s  scan         A-a  all
-C-p  pair         A-p  paired
-C-c  connect      A-c  connected
-C-t  trust        A-t  trusted
-C-d  disconnect
+C-s  scan devices 
+C-w  bluetoothctl power on/off
+C-b  bluetooth on/off
+
+< DEVICE >         < LIST >
+C-p  pair          A-p  paired
+C-c  connect       A-c  connected
+C-t  trust         A-t  trusted
+C-d  disconnect    A-a  all
 C-r  remove
 C-u  untrust
-C-w  bluetoothctl power on/off
-C-o  bluetooth on/off
 ' \
 --bind='enter:execute(echo {2})+abort' \
 --bind='ctrl-s:execute-silent(bluetoothctl scan on&)' \
@@ -27,7 +28,7 @@ C-o  bluetooth on/off
 --bind='ctrl-c:preview:bluetoothctl connect {2}' \
 --bind='ctrl-d:preview:bluetoothctl disconnect {2}' \
 --bind='ctrl-w:preview:bluetoothctl show | grep -q "Powered: yes" && bluetoothctl power off || bluetoothctl power on' \
---bind='ctrl-o:preview:bluetooth | grep -q "bluetooth = on" && bluetooth off || bluetooth on' \
+--bind='ctrl-b:preview:bluetooth | grep -q "bluetooth = on" && bluetooth off || bluetooth on' \
 --bind='alt-a:reload-sync(bluetoothctl devices)' \
 --bind='alt-p:reload-sync(bluetoothctl devices Paired)' \
 --bind='alt-c:reload-sync(bluetoothctl devices Connected)' \

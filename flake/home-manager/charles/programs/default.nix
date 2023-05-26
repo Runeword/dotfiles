b3cd1fa363eb1700d5acf642b8eafee9dcaefd66
Runeword <60324746+Runeword@users.noreply.github.com>
2023-05-26 20:54:52 +0200

    Update flake/home-manager/charles/programs/default.nix

--- flake/home-manager/charles/programs/default.nix
+++ flake/home-manager/charles/programs/default.nix
@@ -18,6 +18,7 @@
     ./zoxide.nix
     ./fzf.nix
     ./dircolors.nix
+    ./fish
     # ./xmonad.nix
     # ./wezterm.nix
   ];
@@ -28,12 +29,6 @@
     bash.enable = true;
     bash.enableCompletion = true;
     bash.bashrcExtra = builtins.readFile ../extra/.bashrc;
-
-    fish.enable = true;
-    fish.shellAbbrs = config.home.shellAliases;
-    fish.interactiveShellInit = "echo 'interactiveShellInit'";
-    fish.loginShellInit = "echo 'loginShellInit'";
-    fish.shellInit = builtins.readFile ../extra/config.fish;
 
     git.enable = true;
     git.userEmail = "60324746+Runeword@users.noreply.github.com";

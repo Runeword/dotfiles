9aac29f3d889ccb4fbb885e2c64cc1bbcf1185e7
Runeword <60324746+Runeword@users.noreply.github.com>
2023-05-26 18:54:47 +0200

    Update flake/home-manager/charles/programs/readline.nix

--- flake/home-manager/charles/programs/readline.nix
+++ flake/home-manager/charles/programs/readline.nix
@@ -8,15 +8,13 @@
 
   programs.readline.bindings = {
     "\\C-i" = "menu-complete"; # tab
-    "\\e[Z" = "menu-complete-backward"; # shift + tab
+    "\\e[Z" = "menu-complete-backward"; # shift tab
     "\\C-u" = "kill-whole-line";
     "\\C-j" = "unix-line-discard";
-    "\\C-\\b" = "backward-kill-word"; # ctrl + backspace
-    "\\e[A" = "history-search-backward"; # arrow up
-    "\\e[B" = "history-search-forward"; # arrow down
+    "\\C-\\b" = "backward-kill-word"; # ctrl backspace
+    "\\e[A" = "history-search-backward"; # up
+    "\\e[B" = "history-search-forward"; # down
     "\\e[6~" = ""; # page down
     "\\e[5~" = ""; # page up
-    # "\e[1;5B"= "history-search-forward"; # ctrl + arrow down
-    # "\e[1;5A"= "history-search-backward"; # ctrl + arrow up
   };
 }

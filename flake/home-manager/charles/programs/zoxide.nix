66ebc57c988ff9f42ab052e215df5f6da307a7bf
Runeword <60324746+Runeword@users.noreply.github.com>
2023-05-27 20:32:10 +0200

    Remove flake/home-manager/charles/programs/alacritty.nix Remove flake/home-manager/charles/programs/bash/bashrc Remove flake/home-manager/charles/programs/bash/default.nix Remove flake/home-manager/charles/programs/broot.nix Remove flake/home-manager/charles/programs/default.nix Remove flake/home-manager/charles/programs/feh.nix Remove flake/home-manager/charles/programs/fish/config.fish Remove flake/home-manager/charles/programs/fish/default.nix Remove flake/home-manager/charles/programs/fzf.nix Remove flake/home-manager/charles/programs/i3.nix Remove flake/home-manager/charles/programs/kitty.nix Remove flake/home-manager/charles/programs/neovim.nix Remove flake/home-manager/charles/programs/readline.nix Remove flake/home-manager/charles/programs/ssh.nix Remove flake/home-manager/charles/programs/starship.nix Remove flake/home-manager/charles/programs/tmux.nix Remove flake/home-manager/charles/programs/wezterm.nix Remove flake/home-manager/charles/programs/zoxide.nix

--- flake/home-manager/charles/programs/zoxide.nix
+++
@@ -1,9 +0,0 @@
-{
-  programs.zoxide.enable = true;
-  programs.zoxide.enableBashIntegration = true;
-  programs.zoxide.enableFishIntegration = true;
-  programs.zoxide.options = ["--no-cmd"];
-  home.sessionVariables._ZO_FZF_OPTS = "--reverse --height 40% --no-separator --border none"; # fzf options
-  home.shellAliases.cd = "__zoxide_z";
-  home.shellAliases.ch = "__zoxide_zi";
-}

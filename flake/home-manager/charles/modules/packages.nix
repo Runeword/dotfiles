ff0ca556f1785ac36f292e96be1d33e915f62964
Runeword <60324746+Runeword@users.noreply.github.com>
2023-05-27 21:56:31 +0200

    Add flake/home-manager/charles/modules/packages.nix

---
+++ flake/home-manager/charles/modules/packages.nix
@@ -0,0 +1,45 @@
+{
+  pkgs,
+  inputs,
+  ...
+}: {
+  home.packages = with pkgs; [
+    htop
+    unzip
+    nurl
+    nix-init
+    qmk
+    gh
+    xarchiver
+    xdragon
+    wget
+    doppler
+    ripgrep
+    progress
+    neofetch
+    onefetch
+    fd
+    inkscape
+    vifm
+    xclip
+    chezmoi
+    trashy
+    rofi
+    peek
+    gcc
+    colorpicker
+    realesrgan-ncnn-vulkan
+    slack
+    bitwarden-cli
+    google-chrome
+    firefox
+    neovim-nightly
+    python311
+
+    (nerdfonts.override {fonts = ["Hack" "DroidSansMono" "SourceCodePro"];})
+
+    inputs.src-cli.packages.x86_64-linux.default
+    inputs.nixified-ai.packages.x86_64-linux.invokeai-nvidia
+    # inputs.nixified-ai.packages.x86_64-linux.koboldai-nvidia
+  ];
+}

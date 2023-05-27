cd2a13b88ee353c0684ba7c6231d1dd69c924fac
Runeword <60324746+Runeword@users.noreply.github.com>
2023-05-27 20:09:49 +0200

    Update flake/home-manager/charles/home/shellAliases.nix

--- flake/home-manager/charles/home/shellAliases.nix
+++ flake/home-manager/charles/home/shellAliases.nix
@@ -14,7 +14,7 @@
     b = "br -c :open_preview";
     bb = "br -c ':toggle_hidden;:toggle_perm;:toggle_dates'";
     bt = "__bluetooth";
-    l = "command ls -A --color";
+    l = "command ls -A --color --width 90";
     ll = "command ls -hAltF --color";
     oi = "kitty +kitten icat";
     n = "nvim";

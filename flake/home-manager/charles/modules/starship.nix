487eeaf6e2726d986027f31c965981b89d723d5c
Runeword <60324746+Runeword@users.noreply.github.com>
2023-05-29 22:56:47 +0200

    Update flake/home-manager/charles/modules/starship.nix

--- flake/home-manager/charles/modules/starship.nix
+++ flake/home-manager/charles/modules/starship.nix
@@ -5,18 +5,40 @@
   programs.starship.enableNushellIntegration = true;
 
   programs.starship.settings = {
+    format = builtins.concatStringsSep "" [
+      "$shell"
+      "$directory"
+      "$nix_shell"
+      "$all"
+      "$git_branch"
+    ];
+
+    shell.disabled = false;
+    shell.fish_indicator = "🐟";
+    shell.bash_indicator = "";
+
+    directory.style = "white";
+
     line_break.disabled = true;
+
     character.disabled = true;
+
     cmd_duration.disabled = true;
     cmd_duration.min_time = 10000;
     cmd_duration.format = "[$duration]($style) ";
+
     git_status.disabled = true;
-    git_branch.format = "[$symbol$branch(:$remote_branch)]($style) ";
-    git_branch.symbol = "󰊢 ";
+    git_branch.format = "[$symbol $branch(:$remote_branch)]($style) ";
+    git_branch.symbol = "🌱";
+    git_branch.style = "white";
+
     nodejs.format = "[$symbol($version)]($style) ";
-    nodejs.symbol = "󰎙 ";
+    nodejs.symbol = "node ";
+    nodejs.style = "white";
     nodejs.version_format = "$\{raw\}";
+
     nix_shell.format = "[$symbol($name)]($style) ";
-    nix_shell.symbol = "󱄅 ";
+    nix_shell.symbol = "❄️ ";
+    nix_shell.style = "white";
   };
 }

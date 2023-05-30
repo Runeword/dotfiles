08b7cbc944345fd38ee50deeee8036e1ed2ee8ef
Runeword <60324746+Runeword@users.noreply.github.com>
2023-05-30 20:06:49 +0200

    Update flake/home-manager/charles/modules/fish/default.nix

--- flake/home-manager/charles/modules/fish/default.nix
+++ flake/home-manager/charles/modules/fish/default.nix
@@ -5,14 +5,16 @@
   ...
 }: {
   programs.fish.enable = true;
+
   programs.fish.shellAbbrs = config.home.shellAliases;
   programs.fish.interactiveShellInit = "echo 'interactiveShellInit'";
   programs.fish.loginShellInit = "echo 'loginShellInit'";
   programs.fish.shellInit = builtins.readFile ./config.fish;
-  programs.fish.plugins = [
-    {
-      name = "bass";
-      src = pkgs.fishPlugins.bass.src;
-    }
-  ];
+
+  # programs.fish.plugins = [
+  #   {
+  #     name = "bass";
+  #     src = pkgs.fishPlugins.bass.src;
+  #   }
+  # ];
 }

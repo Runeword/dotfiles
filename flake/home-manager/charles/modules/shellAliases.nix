3ff08ef45f4c0811858e2c0bfc247ac88844cfce
Runeword <60324746+Runeword@users.noreply.github.com>
2023-05-31 20:42:40 +0200

    Update flake/home-manager/charles/modules/shellAliases.nix

--- flake/home-manager/charles/modules/shellAliases.nix
+++ flake/home-manager/charles/modules/shellAliases.nix
@@ -19,9 +19,9 @@
     oi = "kitty +kitten icat";
     n = "nvim";
     r = "trash";
-    c = "cd $(fd --type directory --hidden --follow --no-ignore --exclude .git --exclude node_modules | fzf --inline-info --cycle --preview 'ls -AxF {} | head -$FZF_PREVIEW_LINES' --preview-window right,50%,noborder --no-scrollbar)";
-    ca = "cd $(fd --type directory --hidden --follow --no-ignore | fzf --cycle)";
-    o = "xdg-open $(fd --type file --hidden --follow --no-ignore --exclude .git --exclude node_modules | fzf)";
+    # c = "cd $(fd --type directory --hidden --follow --no-ignore --exclude .git --exclude node_modules | fzf --inline-info --cycle --preview 'ls -AxF {} | head -$FZF_PREVIEW_LINES' --preview-window right,50%,noborder --no-scrollbar)";
+    # ca = "cd $(fd --type directory --hidden --follow --no-ignore | fzf --cycle)";
+    # o = "xdg-open $(fd --type file --hidden --follow --no-ignore --exclude .git --exclude node_modules | fzf)";
     s = "source";
     ss = "src search";
     xc = "xclip -selection c";

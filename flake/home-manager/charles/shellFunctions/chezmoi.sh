ac6eb6de58c3a8c5bb54e7cf1b669a6aa0d71171
Runeword <60324746+Runeword@users.noreply.github.com>
2023-06-03 21:20:57 +0200

    Update flake/home-manager/charles/shellFunctions/chezmoi.sh

--- flake/home-manager/charles/shellFunctions/chezmoi.sh
+++ flake/home-manager/charles/shellFunctions/chezmoi.sh
@@ -1,60 +1,57 @@
 __select_files() {
-  echo "$1" | fzf \
-    --multi --inline-info --cycle --height 70% \
-    --preview 'chezmoi diff --reverse --color=true ~/{}' \
-    --preview-window bottom,70%,noborder
+	echo "$1" | fzf \
+		--multi --inline-info --cycle --height 70% \
+		--preview 'chezmoi diff --reverse --color=true ~/{}' \
+		--preview-window bottom,70%,noborder
 }
 
 cha() {
-  if [ $# -gt 0 ]
-  then
-    selected_files=$@
-  else
-    files=$(chezmoi status | awk '{print $2}')
-    [ -z "$files" ] && return 1
+	if [ $# -gt 0 ]; then
+		selected_files=$@
+	else
+		files=$(chezmoi status | awk '{print $2}')
+		[ -z "$files" ] && return 1
 
-    selected_files=$(__select_files "$files")
-    [ -z "$selected_files" ] && return 1
-  fi
+		selected_files=$(__select_files "$files")
+		[ -z "$selected_files" ] && return 1
+	fi
 
-  for i in $selected_files; do
-    chezmoi add $i
-  done
+	for i in $selected_files; do
+		chezmoi add $i
+	done
 }
 
 chy() {
-  if [ $# -gt 0 ]
-  then
-    selected_files=$@
-  else
-    files=$(chezmoi status | awk '{print $2}')
-    [ -z "$files" ] && return 1
+	if [ $# -gt 0 ]; then
+		selected_files=$@
+	else
+		files=$(chezmoi status | awk '{print $2}')
+		[ -z "$files" ] && return 1
 
-    selected_files=$(__select_files "$files")
-    [ -z "$selected_files" ] && return 1
-  fi
+		selected_files=$(__select_files "$files")
+		[ -z "$selected_files" ] && return 1
+	fi
 
-  for i in $selected_files; do
-    chezmoi apply $i
-  done
+	for i in $selected_files; do
+		chezmoi apply $i
+	done
 }
 
 chf() {
-  if [ $# -gt 0 ]
-  then
-    selected_files=$@
-  else
-    files=$(chezmoi managed)
-    [ -z "$files" ] && return 1
+	if [ $# -gt 0 ]; then
+		selected_files=$@
+	else
+		files=$(chezmoi managed)
+		[ -z "$files" ] && return 1
 
-    selected_files=$(echo "$files" | fzf \
-      --multi --inline-info --cycle --height 70% \
-      --preview '[ -f {} ] && bat --style=plain --color=always {}' \
-      --preview-window right,70%,noborder)
-          [ -z "$selected_files" ] && return 1
-  fi
+		selected_files=$(echo "$files" | fzf \
+			--multi --inline-info --cycle --height 70% \
+			--preview '[ -f {} ] && bat --style=plain --color=always {}' \
+			--preview-window right,70%,noborder)
+		[ -z "$selected_files" ] && return 1
+	fi
 
-  for i in $selected_files; do
-    chezmoi forget $i
-  done
+	for i in $selected_files; do
+		chezmoi forget $i
+	done
 }

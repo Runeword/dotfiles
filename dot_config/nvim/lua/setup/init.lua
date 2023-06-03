55564737daa7bbcf4cad2ec16beae842bcef7be9
Runeword <60324746+Runeword@users.noreply.github.com>
2023-06-03 21:14:46 +0200

    Update .config/nvim/lua/setup/init.lua

--- dot_config/nvim/lua/setup/init.lua
+++ dot_config/nvim/lua/setup/init.lua
@@ -13,7 +13,7 @@ local api = vim.api
 
 -------------------- norcalli/nvim-colorizer.lua
 local function colorizer()
-  require("colorizer").setup()
+  require('colorizer').setup()
 end
 
 -------------------- machakann/vim-highlightedyank
@@ -23,34 +23,34 @@ end
 
 -------------------- ahmedkhalf/project.nvim
 local function project()
-  require("project_nvim").setup()
+  require('project_nvim').setup()
 end
 
 -------------------- nacro90/numb.nvim
 local function numb()
-  require("numb").setup()
+  require('numb').setup()
 end
 
 -------------------- chrisgrieser/nvim-various-textobjs
 local function varioustextobjs()
-  require("various-textobjs").setup({ useDefaultKeymaps = false })
+  require('various-textobjs').setup({ useDefaultKeymaps = false, })
 end
 
 -------------------- gbprod/stay-in-place.nvim
 local function stayinplace()
-  require("stay-in-place").setup()
+  require('stay-in-place').setup()
 end
 
 -------------------- cshuaimin/ssr.nvim
 local function ssr()
-  require("ssr").setup {
+  require('ssr').setup {
     min_width = 50,
     min_height = 5,
     keymaps = {
-      close = "q",
-      next_match = "n",
-      prev_match = "N",
-      replace_all = "<C-CR>",
+      close = 'q',
+      next_match = 'n',
+      prev_match = 'N',
+      replace_all = '<C-CR>',
     },
   }
 end
@@ -60,6 +60,19 @@ local function tablemode()
   g.table_mode_disable_mappings = 1
   g.table_mode_disable_tableize_mappings = 1
   g.table_mode_syntax = 0
+end
+
+-------------------- jose-elias-alvarez/null-ls.nvim
+local function nullls()
+  local null_ls = require('null-ls')
+
+  null_ls.setup({
+    sources = {
+      null_ls.builtins.formatting.shfmt,
+      -- null_ls.builtins.diagnostics.eslint,
+      -- null_ls.builtins.completion.spell,
+    },
+  })
 end
 
 -------------------- rareitems/printer.nvim
@@ -72,14 +85,14 @@ local function printer()
       end,
     },
     add_to_inside = function(text)
-      return string.format('(%s) %s', fn.line("."), text)
+      return string.format('(%s) %s', fn.line('.'), text)
     end,
   })
 end
 
 -------------------- kylechui/nvim-surround
 local function surround()
-  require("nvim-surround").setup({
+  require('nvim-surround').setup({
     keymaps = {
       insert = false,
       insert_line = false,
@@ -87,17 +100,17 @@ local function surround()
       normal_cur_line = false,
       visual_line = false,
       normal_cur = 'ySS',
-      normal = "yS",
-      delete = "dS",
-      change = "cS",
-      visual = "S",
+      normal = 'yS',
+      delete = 'dS',
+      change = 'cS',
+      visual = 'S',
     },
   })
 end
 
 -------------------- numToStr/Comment.nvim
 local function comment()
-  require("Comment").setup()
+  require('Comment').setup()
 end
 
 -------------------- gelguy/wilder.nvim
@@ -105,7 +118,7 @@ local function wilder()
   local wilder = require('wilder')
 
   wilder.setup({
-    modes = { ':' },
+    modes = { ':', },
     -- modes = { ':', '/', '?' },
     next_key = '<Tab>',
     previous_key = '<S-Tab>',
@@ -127,14 +140,15 @@ local function wilder()
 
   wilder.set_option('renderer', wilder.popupmenu_renderer({
     pumblend = 25,
-    left = { ' ', wilder.popupmenu_devicons() },
-    right = { ' ', wilder.popupmenu_scrollbar() },
+    left = { ' ', wilder.popupmenu_devicons(), },
+    right = { ' ', wilder.popupmenu_scrollbar(), },
     highlighter = {
       wilder.lua_pcre2_highlighter(),
       wilder.lua_fzy_highlighter(),
     },
     highlights = {
-      accent = wilder.make_hl('WilderAccent', 'Pmenu', { { a = 1 }, { a = 1 }, { foreground = '#ff4d97' } }),
+      accent = wilder.make_hl('WilderAccent', 'Pmenu',
+        { { a = 1, }, { a = 1, }, { foreground = '#ff4d97', }, }),
     },
   }))
 end
@@ -173,61 +187,61 @@ end
 
 -------------------- woosaaahh/sj.nvim
 local function sj()
-  require("sj").setup({
+  require('sj').setup({
     auto_jump = false,
     forward_search = true,
     highlights_timeout = 0,
     max_pattern_length = 0,
-    pattern_type = "vim",
+    pattern_type = 'vim',
     preserve_highlights = true,
-    prompt_prefix = "",
+    prompt_prefix = '',
     relative_labels = false,
-    search_scope = "visible_lines",
-    separator = "",
+    search_scope = 'visible_lines',
+    separator = '',
     update_search_register = false,
     use_last_pattern = false,
     use_overlay = false,
     wrap_jumps = o.wrapscan,
     labels = {
-      "'", ",", "p", "y", "a", "o", "e", "u", "i", "d", "h", "t",
-      "n", "s", "f", "g", "c", "r", "l", ";", "q", "j", "k", "x", "b",
-      "m", "w", "v", "z",
+      "'", ',', 'p', 'y', 'a', 'o', 'e', 'u', 'i', 'd', 'h', 't',
+      'n', 's', 'f', 'g', 'c', 'r', 'l', ';', 'q', 'j', 'k', 'x', 'b',
+      'm', 'w', 'v', 'z',
     },
     keymaps = {
-      cancel = "<Esc>",
-      validate = "<Tab>",
-      prev_match = "<S-Enter>",
-      next_match = "<Enter>",
-      prev_pattern = "<C-p>",
-      next_pattern = "<C-n>",
-      delete_prev_char = "<BS>",
-      delete_prev_word = "<C-w>",
-      delete_pattern = "<C-u>",
-      restore_pattern = "<A-BS>",
-      send_to_qflist = "<A-q>",
+      cancel = '<Esc>',
+      validate = '<Tab>',
+      prev_match = '<S-Enter>',
+      next_match = '<Enter>',
+      prev_pattern = '<C-p>',
+      next_pattern = '<C-n>',
+      delete_prev_char = '<BS>',
+      delete_prev_word = '<C-w>',
+      delete_pattern = '<C-u>',
+      restore_pattern = '<A-BS>',
+      send_to_qflist = '<A-q>',
     },
   })
 end
 
 -------------------- kosayoda/nvim-lightbulb
 local function lightbulb()
-  fn.sign_define('LightBulbSign', { text = "⚡" })
+  fn.sign_define('LightBulbSign', { text = '⚡', })
 end
 
 -------------------- ibhagwan/fzf-lua
 local function fzf()
-  local actions = require("fzf-lua.actions")
+  local actions = require('fzf-lua.actions')
 
-  require("fzf-lua").setup({
+  require('fzf-lua').setup({
     winopts = {
       fullscreen = true,
-      border = "none",
+      border = 'none',
       preview = {
-        layout = "horizontal",
-        horizontal = "up:70%",
+        layout = 'horizontal',
+        horizontal = 'up:70%',
         title = false,
         delay = 0,
-        scrollchars = { "▎", "" },
+        scrollchars = { '▎', '', },
       },
     },
     keymap = {
@@ -235,16 +249,16 @@ local function fzf()
       fzf = {
         -- 	-- ["tab"] = "down",
         -- 	-- ["btab"] = "up",
-        ["ctrl-e"] = "preview-page-down",
-        ["ctrl-u"] = "preview-page-up",
+        ['ctrl-e'] = 'preview-page-down',
+        ['ctrl-u'] = 'preview-page-up',
       },
     },
     actions = {
       files = {
-        ["default"] = actions.file_edit,
+        ['default'] = actions.file_edit,
       },
       buffers = {
-        ["default"] = actions.buf_edit,
+        ['default'] = actions.buf_edit,
       },
     },
   })
@@ -254,8 +268,8 @@ end
 local function sniprun()
   require('sniprun').setup({
     snipruncolors = {
-      SniprunVirtualTextOk  = { bg = "black", fg = "white" },
-      SniprunVirtualTextErr = { bg = "black", fg = "white" },
+      SniprunVirtualTextOk  = { bg = 'black', fg = 'white', },
+      SniprunVirtualTextErr = { bg = 'black', fg = 'white', },
     },
     live_mode_toggle = 'enable',
   })
@@ -263,25 +277,25 @@ end
 
 -------------------- is0n/fm-nvim
 local function fm()
-  require("fm-nvim").setup({
-    edit_cmd = "edit",
+  require('fm-nvim').setup({
+    edit_cmd = 'edit',
     ui = {
-      default = "float",
+      default = 'float',
       float = {
-        border = "none",
-        float_hl = "Normal",
-        border_hl = "FloatBorder",
+        border = 'none',
+        float_hl = 'Normal',
+        border_hl = 'FloatBorder',
         blend = 0,
         height = 999,
         width = 999,
       },
       split = {
-        direction = "topleft",
+        direction = 'topleft',
         size = 24,
       },
     },
     cmds = {
-      vifm_cmd = "vifm",
+      vifm_cmd = 'vifm',
     },
     mappings = {
       -- vert_split = "<C-v>",
@@ -295,41 +309,41 @@ end
 
 -------------------- lewis6991/gitsigns.nvim
 local function gitsigns()
-  require("gitsigns").setup({
+  require('gitsigns').setup({
     current_line_blame_opts = {
       delay = 0,
     },
-    on_attach = require("mappings").gitsigns,
+    on_attach = require('mappings').gitsigns,
     signs = {
       add = {
-        hl = "GitSignsAdd",
-        text = "+",
-        numhl = "GitSignsAddNr",
-        linehl = "GitSignsAddLn",
+        hl = 'GitSignsAdd',
+        text = '+',
+        numhl = 'GitSignsAddNr',
+        linehl = 'GitSignsAddLn',
       },
       change = {
-        hl = "GitSignsChange",
-        text = "~",
-        numhl = "GitSignsChangeNr",
-        linehl = "GitSignsChangeLn",
+        hl = 'GitSignsChange',
+        text = '~',
+        numhl = 'GitSignsChangeNr',
+        linehl = 'GitSignsChangeLn',
       },
       delete = {
-        hl = "GitSignsDelete",
-        text = "_",
-        numhl = "GitSignsDeleteNr",
-        linehl = "GitSignsDeleteLn",
+        hl = 'GitSignsDelete',
+        text = '_',
+        numhl = 'GitSignsDeleteNr',
+        linehl = 'GitSignsDeleteLn',
       },
       topdelete = {
-        hl = "GitSignsDelete",
-        text = "‾",
-        numhl = "GitSignsDeleteNr",
-        linehl = "GitSignsDeleteLn",
+        hl = 'GitSignsDelete',
+        text = '‾',
+        numhl = 'GitSignsDeleteNr',
+        linehl = 'GitSignsDeleteLn',
       },
       changedelete = {
-        hl = "GitSignsChange",
-        text = "~",
-        numhl = "GitSignsChangeNr",
-        linehl = "GitSignsChangeLn",
+        hl = 'GitSignsChange',
+        text = '~',
+        numhl = 'GitSignsChangeNr',
+        linehl = 'GitSignsChangeLn',
       },
     },
   })
@@ -337,26 +351,26 @@ end
 
 -------------------- echasnovski/mini.nvim#miniindentscope
 local function indentscope()
-  local scope = require("mini.indentscope")
+  local scope = require('mini.indentscope')
   scope.setup({
     draw = {
       delay = 0,
-      animation = scope.gen_animation.none()
+      animation = scope.gen_animation.none(),
     },
     mappings = {
-      object_scope = "ii",
-      object_scope_with_border = "ai",
-      goto_top = "",
-      goto_bottom = "",
+      object_scope = 'ii',
+      object_scope_with_border = 'ai',
+      goto_top = '',
+      goto_bottom = '',
       -- goto_top = "<S-CR>",
       -- goto_bottom = "<CR>",
     },
     options = {
-      border = "both",
+      border = 'both',
       indent_at_cursor = true,
       try_as_border = true,
     },
-    symbol = "▏",
+    symbol = '▏',
   })
 end
 
@@ -366,7 +380,7 @@ local function ai()
   require('mini.ai').setup({
     custom_textobjects = {
       f = false,
-      a = gen_spec.argument({ brackets = { '%b()', '%b{}', '%b[]' } }),
+      a = gen_spec.argument({ brackets = { '%b()', '%b{}', '%b[]', }, }),
       -- a = gen_spec.argument({ brackets = { '%b()' } }),
       -- o = gen_spec.argument({ brackets = { '%b{}' } }),
       -- e = gen_spec.argument({ brackets = { '%b[]' } }),
@@ -393,36 +407,39 @@ end
 
 -------------------- nvim-treesitter/nvim-treesitter
 local function treesitter()
-  require("nvim-treesitter.configs").setup({
-    ensure_installed = "all",
+  require('nvim-treesitter.configs').setup({
+    ensure_installed = 'all',
     highlight = { enable = true, },
     indent = { enable = true, },
     autopairs = { enable = true, }, -- windwp/nvim-autopairs
-    autotag = { enable = true, }, -- windwp/nvim-ts-autotag
-    matchup = { -- andymass/vim-matchup
+    autotag = { enable = true, },   -- windwp/nvim-ts-autotag
+    matchup = {
+                                    -- andymass/vim-matchup
       enable = true,
       disable_virtual_text = true,
     },
-    rainbow = { -- p00f/nvim-ts-rainbow
+    rainbow = {
+                -- p00f/nvim-ts-rainbow
       enable = false,
       max_file_lines = nil,
       colors = {
-        "#FF0048",
-        "#B800FF",
-        "#FAFF00"
+        '#FF0048',
+        '#B800FF',
+        '#FAFF00',
       },
     },
-    textobjects = { -- nvim-treesitter/nvim-treesitter-textobjects
+    textobjects = {
+                    -- nvim-treesitter/nvim-treesitter-textobjects
       select = {
         enable = true,
         lookahead = true,
         keymaps = {
-          ["f"] = "@function.outer",
-          ["af"] = "@function.inner",
-          ["if"] = "@function.inner",
-          ["F"] = "@call.outer",
-          ["aF"] = "@call.outer",
-          ["iF"] = "@call.inner",
+          ['f'] = '@function.outer',
+          ['af'] = '@function.inner',
+          ['if'] = '@function.inner',
+          ['F'] = '@call.outer',
+          ['aF'] = '@call.outer',
+          ['iF'] = '@call.inner',
         },
       },
       move = {
@@ -466,8 +483,8 @@ local function bufferline()
       numbers = function(opts)
         return string.format('%s', opts.raise(opts.ordinal))
       end,
-      indicator = { style = 'none' },
-      separator_style = { '', '' },
+      indicator = { style = 'none', },
+      separator_style = { '', '', },
       tab_size = 0,
       buffer_close_icon = '',
       modified_icon = '',
@@ -493,34 +510,34 @@ end
 
 -------------------- gbprod/cutlass.nvim
 local function cutlass()
-  require("cutlass").setup({
-    cut_key = "m",
+  require('cutlass').setup({
+    cut_key = 'm',
     override_del = true,
-    exclude = { "ns", "nS" },
+    exclude = { 'ns', 'nS', },
   })
 end
 
 -------------------- jonatan-branting/nvim-better-n
 local function bettern()
-  require("better-n").setup {
+  require('better-n').setup {
     callbacks = {},
     mappings = {
-      ["F"] = { previous = "n", next = "<s-n>" },
-      ["T"] = { previous = "n", next = "<s-n>" },
-    }
+      ['F'] = { previous = 'n', next = '<s-n>', },
+      ['T'] = { previous = 'n', next = '<s-n>', },
+    },
   }
 end
 
 -------------------- windwp/nvim-autopairs
 local function autopairs()
   local remap = vim.api.nvim_set_keymap
-  local npairs = require("nvim-autopairs")
+  local npairs = require('nvim-autopairs')
 
   npairs.setup({
     map_bs = true,
     map_cr = false,
     check_ts = true,
-    ignored_next_char = "[%w%.]",
+    ignored_next_char = '[%w%.]',
     -- fast_wrap = {
     --   map = "<C-h>",
     --   chars = { "{", "[", "(", '"', "'" },
@@ -539,42 +556,42 @@ local function autopairs()
 
   MUtils.CR = function()
     if fn.pumvisible() ~= 0 then
-      if fn.complete_info({ "selected" }).selected ~= -1 then
-        return npairs.esc("<c-y>")
+      if fn.complete_info({ 'selected', }).selected ~= -1 then
+        return npairs.esc('<c-y>')
       else
-        return npairs.esc("<c-e>") .. npairs.autopairs_cr()
+        return npairs.esc('<c-e>') .. npairs.autopairs_cr()
       end
     else
       return npairs.autopairs_cr()
     end
   end
-  remap("i", "<cr>", "v:lua.MUtils.CR()", { expr = true, noremap = true })
+  remap('i', '<cr>', 'v:lua.MUtils.CR()', { expr = true, noremap = true, })
 
   MUtils.BS = function()
-    if fn.pumvisible() ~= 0 and fn.complete_info({ "mode" }).mode == "eval" then
-      return npairs.esc("<c-e>") .. npairs.autopairs_bs()
+    if fn.pumvisible() ~= 0 and fn.complete_info({ 'mode', }).mode == 'eval' then
+      return npairs.esc('<c-e>') .. npairs.autopairs_bs()
     else
       return npairs.autopairs_bs()
     end
   end
-  remap("i", "<bs>", "v:lua.MUtils.BS()", { expr = true, noremap = true })
+  remap('i', '<bs>', 'v:lua.MUtils.BS()', { expr = true, noremap = true, })
 end
 
 -------------------- monaqa/dial.nvim
 local function dial()
-  local augend = require("dial.augend")
-  require("dial.config").augends:register_group({
+  local augend = require('dial.augend')
+  require('dial.config').augends:register_group({
     default = {
       augend.integer.alias.decimal,
       augend.integer.alias.decimal_int,
-      augend.date.alias["%Y/%m/%d"],
+      augend.date.alias['%Y/%m/%d'],
       augend.semver.alias.semver,
       augend.constant.alias.bool,
       augend.constant.new({
-        elements = { "let", "const" },
+        elements = { 'let', 'const', },
       }),
       augend.constant.new({
-        elements = { "&&", "||" },
+        elements = { '&&', '||', },
         word = false,
         cyclic = true,
       }),
@@ -584,12 +601,12 @@ end
 
 -------------------- williamboman/mason.nvim
 local function mason()
-  require("mason").setup({ ui = { border = 'single' } })
+  require('mason').setup({ ui = { border = 'single', }, })
 end
 
 -------------------- williamboman/mason-lspconfig.nvim
 local function masonlspconfig()
-  require("mason-lspconfig").setup({
+  require('mason-lspconfig').setup({
     ensure_installed = {
       -- 'tsserver',
       -- 'eslint',
@@ -628,7 +645,7 @@ local function coq()
       replace_prefix_threshold = 3,
       replace_suffix_threshold = 2,
       smart = true,
-      skip_after = { "{", "}", "[", "]", " ", },
+      skip_after = { '{', '}', '[', ']', ' ', },
     },
     match = {
       fuzzy_cutoff = 0.6,
@@ -640,10 +657,10 @@ local function coq()
         x_max_len = 100,
         border = 'single',
       },
-      icons = { mode = 'none' },
+      icons = { mode = 'none', },
       pum = {
-        kind_context = { '   ', '' },
-        source_context = { '', '' },
+        kind_context = { '   ', '', },
+        source_context = { '', '', },
         fast_close = true,
       },
       ghost_text = { enabled = false, },
@@ -684,9 +701,9 @@ end
 
 -------------------- smjonas/live-command.nvim
 local function livecommand()
-  require("live-command").setup {
+  require('live-command').setup {
     commands = {
-      Norm = { cmd = "norm" },
+      Norm = { cmd = 'norm', },
     },
   }
 end
@@ -730,5 +747,6 @@ return {
   printer = printer,
   textobjchainmember = textobjchainmember,
   starry = starry,
-  align = align
+  align = align,
+  nullls = nullls,
 }

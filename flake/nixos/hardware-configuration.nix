1dd0d31e89ef15beebe9d123021e5da694f39516
Runeword <60324746+Runeword@users.noreply.github.com>
2023-06-09 19:44:23 +0200

    Update flake/nixos/hardware-configuration.nix

--- flake/nixos/hardware-configuration.nix
+++ flake/nixos/hardware-configuration.nix
@@ -1,12 +1,16 @@
-{ config, lib, pkgs, modulesPath, ... }:
-
 {
+  config,
+  lib,
+  pkgs,
+  modulesPath,
+  ...
+}: {
   hardware.enableRedistributableFirmware = lib.mkDefault true;
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod" ];
-  boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" ];
-  boot.extraModulePackages = [ ];
+  boot.initrd.availableKernelModules = ["xhci_pci" "thunderbolt" "nvme" "usb_storage" "sd_mod"];
+  boot.initrd.kernelModules = [];
+  boot.kernelModules = ["kvm-intel"];
+  boot.extraModulePackages = [];
 
   fileSystems."/".device = "/dev/disk/by-uuid/febf3a5b-22df-4c5b-9a8f-2a52133e40dc";
   fileSystems."/".fsType = "ext4";
@@ -16,10 +20,37 @@
   fileSystems."/boot/efi".device = "/dev/disk/by-uuid/A7CB-42E9";
   fileSystems."/boot/efi".fsType = "vfat";
 
-  swapDevices = [ { device = "/dev/disk/by-uuid/35362b83-6c2d-4749-90e2-116c3f06f450"; } ];
+  swapDevices = [{device = "/dev/disk/by-uuid/35362b83-6c2d-4749-90e2-116c3f06f450";}];
 
   networking.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+
+  # This enables the brightness and airplane mode keys to work
+  # https://community.frame.work/t/12th-gen-not-sending-xf86monbrightnessup-down/20605/11
+  boot.blacklistedKernelModules = ["hid-sensor-hub"];
+
+  # Further tweak to ensure the brightness and airplane mode keys work
+  # https://community.frame.work/t/responded-12th-gen-not-sending-xf86monbrightnessup-down/20605/67
+  systemd.services.bind-keys-driver = {
+    description = "Bind brightness and airplane mode keys to their driver";
+    wantedBy = ["default.target"];
+    after = ["network.target"];
+    serviceConfig = {
+      Type = "oneshot";
+      User = "root";
+    };
+    script = ''
+      ls -lad /sys/bus/i2c/devices/i2c-*:* /sys/bus/i2c/drivers/i2c_hid_acpi/i2c-*:*
+      if [ -e /sys/bus/i2c/devices/i2c-FRMW0001:00 -a ! -e /sys/bus/i2c/drivers/i2c_hid_acpi/i2c-FRMW0001:00 ]; then
+        echo fixing
+        echo i2c-FRMW0001:00 > /sys/bus/i2c/drivers/i2c_hid_acpi/bind
+        ls -lad /sys/bus/i2c/devices/i2c-*:* /sys/bus/i2c/drivers/i2c_hid_acpi/i2c-*:*
+        echo done
+      else
+        echo no fix needed
+      fi
+    '';
+  };
 }

3a5f4205bfe6d7f8639a9a46d4264466b3d27479
Runeword <60324746+Runeword@users.noreply.github.com>
2023-06-10 11:12:27 +0200

    Update flake/flake.nix

--- flake/flake.nix
+++ flake/flake.nix
@@ -8,6 +8,8 @@
   inputs.neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
   # inputs.neovim-nightly-overlay.inputs.nixpkgs.url = "github:nixos/nixpkgs?rev=fad51abd42ca17a60fc1d4cb9382e2d79ae31836";
 
+  inputs.hyprland.url = "github:hyprwm/Hyprland";
+
   inputs.src-cli.url = "github:sourcegraph/src-cli?dir=contrib";
   inputs.nixified-ai.url = "github:nixified-ai/flake";
 

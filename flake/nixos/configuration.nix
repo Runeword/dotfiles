eb18ff5843cbcac0c4c9b1ba7fcdd45429432233
Runeword <60324746+Runeword@users.noreply.github.com>
2023-07-01 21:45:05 +0200

    Update flake/nixos/configuration.nix

--- flake/nixos/configuration.nix
+++ flake/nixos/configuration.nix
@@ -182,6 +182,22 @@
     # ];
   };
 
+  systemd = {
+    user.services.polkit-gnome-authentication-agent-1 = {
+      description = "polkit-gnome-authentication-agent-1";
+      wantedBy = ["graphical-session.target"];
+      wants = ["graphical-session.target"];
+      after = ["graphical-session.target"];
+      serviceConfig = {
+        Type = "simple";
+        ExecStart = "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+        Restart = "on-failure";
+        RestartSec = 1;
+        TimeoutStopSec = 10;
+      };
+    };
+  };
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;

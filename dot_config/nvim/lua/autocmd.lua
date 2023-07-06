a6551a72139a574ec2f57dd4a893753afe7b4651
Runeword <60324746+Runeword@users.noreply.github.com>
2023-07-06 18:36:23 +0200

    Update .config/nvim/lua/autocmd.lua

--- dot_config/nvim/lua/autocmd.lua
+++ dot_config/nvim/lua/autocmd.lua
@@ -160,16 +160,6 @@ local function packer()
   })
 end
 
-local function indentscope()
-  augroup('indentscope', { clear = true, })
-  autocmd('FileType', {
-    group = 'indentscope',
-    pattern = '*',
-    command =
-    "if index(['help', 'startify', 'dashboard', 'packer', 'neogitstatus', 'NvimTree', 'neo-tree', 'Trouble'], &ft) != -1 || index(['nofile', 'terminal', 'lsp-installer', 'lspinfo'], &bt) != -1 | let b:miniindentscope_disable=v:true | endif",
-  })
-end
-
 local function sj()
   augroup('sj', { clear = true, })
   autocmd('ColorScheme', {

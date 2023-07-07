ba0f60b7051335365150009d49097037afdd031f
Runeword <60324746+Runeword@users.noreply.github.com>
2023-07-07 21:15:20 +0200

    Update .config/nvim/lua/plugins/bufferline.lua

--- dot_config/nvim/lua/plugins/bufferline.lua
+++ dot_config/nvim/lua/plugins/bufferline.lua
@@ -3,6 +3,28 @@ local vim = vim
 return {
   'akinsho/bufferline.nvim',
 
+  init = function()
+    vim.api.nvim_create_augroup('bufferline', { clear = true, })
+    vim.api.nvim_create_autocmd('ColorScheme', {
+      group = 'bufferline',
+      pattern = '*',
+      callback = function()
+        vim.api.nvim_set_hl(0, 'BufferLineFill', { bg = 'none', })
+        vim.api.nvim_set_hl(0, 'BufferLineBackground', { fg = '#7a7c9e', })
+        vim.api.nvim_set_hl(0, 'BufferLineBufferSelected',
+          { fg = 'white', bg = 'none', })
+        vim.api.nvim_set_hl(0, 'BufferLineNumbers',
+          { fg = '#7a7c9e', bg = 'none', italic = false, })
+        vim.api.nvim_set_hl(0, 'BufferLineNumbersSelected',
+          { fg = 'white', bg = 'none', italic = false, })
+        vim.api.nvim_set_hl(0, 'BufferLineDuplicate',
+          { fg = '#7a7c9e', bg = 'none', italic = false, })
+        vim.api.nvim_set_hl(0, 'BufferLineDuplicateSelected',
+          { fg = 'white', bg = 'none', italic = false, })
+      end,
+    })
+  end,
+
   config = function()
     vim.keymap.set({ 'n', 'x', }, '<tab>', '<cmd>BufferLineCycleNext<CR>',
       { silent = true, })

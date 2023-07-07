d56c145411b60b5ab5aa24b2f469d5b7cac17737
Runeword <60324746+Runeword@users.noreply.github.com>
2023-07-07 21:15:25 +0200

    Update .config/nvim/lua/plugins/midnight.lua

--- dot_config/nvim/lua/plugins/midnight.lua
+++ dot_config/nvim/lua/plugins/midnight.lua
@@ -10,9 +10,9 @@ return {
   init = function()
     -- autocommand will have no effect on previously sourced colorschemes
     -- so it must be added before any colorscheme is sourced
-    require('autocmd').bufferline()
-    require('autocmd').matchup()
-    require('autocmd').flash()
+    -- require('autocmd').bufferline()
+    -- require('autocmd').matchup()
+    -- require('autocmd').flash()
     -- require('autocmd').sj()
 
     vim.cmd.colorscheme 'midnight'

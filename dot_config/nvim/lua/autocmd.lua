deb83f2ef04b6cc77839b03bc637953d59366c25
Runeword <60324746+Runeword@users.noreply.github.com>
2023-07-08 08:50:13 +0200

    Update .config/nvim/lua/autocmd.lua

--- dot_config/nvim/lua/autocmd.lua
+++ dot_config/nvim/lua/autocmd.lua
@@ -1,3 +1,4 @@
+local vim = vim
 local cmd = vim.cmd
 local o = vim.o
 local fn = vim.fn
@@ -35,38 +36,6 @@ local function core()
     command = 'silent! !tmux source-file ~/.config/tmux/.tmux.conf',
   })
 
-  autocmd('ColorScheme', {
-    group = 'diagnostic',
-    pattern = '*',
-    callback = function()
-      nvim_set_hl(0, 'DiagnosticFloatingError',
-        { link = 'DiagnosticVirtualTextError', })
-      nvim_set_hl(0, 'DiagnosticFloatingHint',
-        { link = 'DiagnosticVirtualTextHint', })
-      nvim_set_hl(0, 'DiagnosticFloatingInfo',
-        { link = 'DiagnosticVirtualTextInfo', })
-      nvim_set_hl(0, 'DiagnosticFloatingWarn',
-        { link = 'DiagnosticVirtualTextWarn', })
-      nvim_set_hl(0, 'NormalFloat', { bg = 'none', })
-      nvim_set_hl(0, 'FloatBorder', { bg = 'none', fg = '#1e2633', })
-      nvim_set_hl(0, 'Yank', { bg = '#00ffa2', fg = 'black', })
-      nvim_set_hl(0, 'Pmenu', { bg = 'black', fg = '#7a7c9e', })
-      nvim_set_hl(0, 'PmenuSel', { bg = '#1e2633', fg = 'white', })
-      nvim_set_hl(0, 'PmenuSbar', { bg = 'black', })
-      nvim_set_hl(0, 'PmenuThumb', { bg = '#1e2633', })
-      nvim_set_hl(0, 'CursorLine', { bg = '#1e2633', })
-      nvim_set_hl(0, 'CursorColumn', { bg = '#1e2633', })
-      nvim_set_hl(0, 'Search', { bg = '#5d00ff', })
-      nvim_set_hl(0, 'Normal', { bg = 'none', })
-      nvim_set_hl(0, 'SignColumn', { bg = 'none', })
-
-      -- nvim_set_hl(0, 'VertSplit', { fg = '#292e42' })
-      -- nvim_set_hl(0, 'Hlargs', { fg = '#FAFF00' })
-      -- nvim_set_hl(0, 'IncSearch', { bg = '#00ffa2', fg = 'black' })
-      -- #0a172e #1e2633 #7a7c9e
-    end,
-  })
-
   autocmd({ 'BufWinEnter', 'BufRead', 'BufNewFile', }, {
     group = 'disableAutoComment',
     pattern = '*',
@@ -87,6 +56,38 @@ local function core()
     callback = function()
       cmd(math.max(math.min(fn.line('$'), 10), 3) ..
         'wincmd _')
+    end,
+  })
+
+  vim.api.nvim_create_autocmd('ColorScheme', {
+    group = 'diagnostic',
+    pattern = '*',
+    callback = function()
+      vim.api.nvim_set_hl(0, 'DiagnosticFloatingError',
+        { link = 'DiagnosticVirtualTextError', })
+      vim.api.nvim_set_hl(0, 'DiagnosticFloatingHint',
+        { link = 'DiagnosticVirtualTextHint', })
+      vim.api.nvim_set_hl(0, 'DiagnosticFloatingInfo',
+        { link = 'DiagnosticVirtualTextInfo', })
+      vim.api.nvim_set_hl(0, 'DiagnosticFloatingWarn',
+        { link = 'DiagnosticVirtualTextWarn', })
+      vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none', })
+      vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none', fg = '#1e2633', })
+      vim.api.nvim_set_hl(0, 'Yank', { bg = '#00ffa2', fg = 'black', })
+      vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'black', fg = '#7a7c9e', })
+      vim.api.nvim_set_hl(0, 'PmenuSel', { bg = '#1e2633', fg = 'white', })
+      vim.api.nvim_set_hl(0, 'PmenuSbar', { bg = 'black', })
+      vim.api.nvim_set_hl(0, 'PmenuThumb', { bg = '#1e2633', })
+      vim.api.nvim_set_hl(0, 'CursorLine', { bg = '#1e2633', })
+      vim.api.nvim_set_hl(0, 'CursorColumn', { bg = '#1e2633', })
+      vim.api.nvim_set_hl(0, 'Search', { bg = '#5d00ff', })
+      vim.api.nvim_set_hl(0, 'Normal', { bg = 'none', })
+      vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'none', })
+
+      -- vim.api.nvim_set_hl(0, 'VertSplit', { fg = '#292e42' })
+      -- vim.api.nvim_set_hl(0, 'Hlargs', { fg = '#FAFF00' })
+      -- vim.api.nvim_set_hl(0, 'IncSearch', { bg = '#00ffa2', fg = 'black' })
+      -- #0a172e #1e2633 #7a7c9e
     end,
   })
 end

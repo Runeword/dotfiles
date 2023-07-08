c29158992d4a4a2cf20ede4f64eabb6248180f64
Runeword <60324746+Runeword@users.noreply.github.com>
2023-07-08 09:50:01 +0200

    Update .config/nvim/lua/setup/init.lua

--- dot_config/nvim/lua/setup/init.lua
+++ dot_config/nvim/lua/setup/init.lua
@@ -25,44 +25,6 @@ local function ssr()
   }
 end
 
--------------------- woosaaahh/sj.nvim
-local function sj()
-  require('sj').setup({
-    auto_jump = false,
-    forward_search = true,
-    highlights_timeout = 0,
-    max_pattern_length = 0,
-    pattern_type = 'vim',
-    preserve_highlights = true,
-    prompt_prefix = '',
-    relative_labels = false,
-    search_scope = 'visible_lines',
-    separator = '',
-    update_search_register = false,
-    use_last_pattern = false,
-    use_overlay = false,
-    wrap_jumps = o.wrapscan,
-    labels = {
-      "'", ',', 'p', 'y', 'a', 'o', 'e', 'u', 'i', 'd', 'h', 't',
-      'n', 's', 'f', 'g', 'c', 'r', 'l', ';', 'q', 'j', 'k', 'x', 'b',
-      'm', 'w', 'v', 'z',
-    },
-    keymaps = {
-      cancel = '<Esc>',
-      validate = '<Tab>',
-      prev_match = '<S-Enter>',
-      next_match = '<Enter>',
-      prev_pattern = '<C-p>',
-      next_pattern = '<C-n>',
-      delete_prev_char = '<BS>',
-      delete_prev_word = '<C-w>',
-      delete_pattern = '<C-u>',
-      restore_pattern = '<A-BS>',
-      send_to_qflist = '<A-q>',
-    },
-  })
-end
-
 -------------------- is0n/fm-nvim
 local function fm()
   require('fm-nvim').setup({
@@ -133,5 +95,4 @@ return {
   mason = mason,
   masonlspconfig = masonlspconfig,
   ssr = ssr,
-  sj = sj,
 }

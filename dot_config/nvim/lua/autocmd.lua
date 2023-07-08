7d1ee06aab7862410ee4c9f1ae879a1a86401729
Runeword <60324746+Runeword@users.noreply.github.com>
2023-07-08 09:49:53 +0200

    Update .config/nvim/lua/autocmd.lua

--- dot_config/nvim/lua/autocmd.lua
+++ dot_config/nvim/lua/autocmd.lua
@@ -9,7 +9,6 @@ local nvim_set_hl = vim.api.nvim_set_hl
 local function core()
   augroup('chezmoi', { clear = true, })
   augroup('tmux', { clear = true, })
-  augroup('bash', { clear = true, })
   augroup('diagnostic', { clear = true, })
   augroup('quickfix', { clear = true, })
   augroup('disableAutoComment', { clear = true, })
@@ -80,7 +79,7 @@ local function core()
       vim.api.nvim_set_hl(0, 'CursorLine', { bg = '#1e2633', })
       vim.api.nvim_set_hl(0, 'CursorColumn', { bg = '#1e2633', })
       vim.api.nvim_set_hl(0, 'Search', { bg = '#5d00ff', })
-      vim.api.nvim_set_hl(0, 'IncSearch', { bg = '#ffe100', fg = 'black', })
+      vim.api.nvim_set_hl(0, 'IncSearch', { bg = '#FAFF00', fg = 'black', })
       vim.api.nvim_set_hl(0, 'Normal', { bg = 'none', })
       vim.api.nvim_set_hl(0, 'SignColumn', { bg = 'none', })
 
@@ -91,36 +90,11 @@ local function core()
   })
 end
 
-local function packer()
-  augroup('packer_user_config', { clear = true, })
-  autocmd('BufWritePost', {
-    group = 'packer_user_config',
-    pattern = '*/.config/nvim/**',
-    command = 'PackerCompile',
-  })
-end
-
-local function sj()
-  augroup('sj', { clear = true, })
-  autocmd('ColorScheme', {
-    group = 'sj',
-    pattern = '*',
-    callback = function()
-      nvim_set_hl(0, 'SjFocusedLabel',
-        { bg = '#ffe100', fg = 'black', bold = false, })
-      nvim_set_hl(0, 'SjLabel', { bg = '#5d00ff', fg = 'white', bold = false, })
-      nvim_set_hl(0, 'SjMatches', { bg = '#222b66', fg = 'white', bold = false, })
-    end,
-  })
-end
-
 -- local function telescope()
 --   cmd([[autocmd ColorScheme * highlight TelescopeBorder guibg=none]])
 --   cmd([[autocmd ColorScheme * highlight TelescopeNormal guibg=none]])
 -- end
 
 return {
-  packer = packer,
   core = core,
-  sj = sj,
 }

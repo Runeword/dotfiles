a571da5049bc697a80cc580820bc944bd76186b7
Runeword <60324746+Runeword@users.noreply.github.com>
2023-07-11 13:24:04 +0200

    Remove flake/home-manager/charles/modules/picom.nix

--- flake/home-manager/charles/modules/picom.nix
+++
@@ -1,9 +0,0 @@
-{ config, pkgs, libs, ... }: {
-  services.picom = {
-    enable = true;
-    shadow = false;
-    vSync = true;
-    backend = "glx";
-    opacityRules = [ "0:_NET_WM_STATE@:32a *= '_NET_WM_STATE_HIDDEN'" ];
-  };
-}

8dde9dd1d7ac0f14d3186346426f6211b9a0330b
Runeword <60324746+Runeword@users.noreply.github.com>
2023-07-12 13:12:55 +0200

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -38,7 +38,12 @@ return {
     'echasnovski/mini.align',
     version = false,
     config = function()
-      require('mini.align').setup()
+      require('mini.align').setup({
+        mappings = {
+          start = 'gl',
+          start_with_preview = '',
+        },
+      })
     end,
   },
 

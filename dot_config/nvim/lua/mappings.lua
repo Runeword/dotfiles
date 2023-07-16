96838b1b212272305af28327b5a24c87af90f354
Runeword <60324746+Runeword@users.noreply.github.com>
2023-07-17 00:29:36 +0200

    Update .config/nvim/lua/mappings.lua

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -9,8 +9,8 @@ vim.keymap.set('n', '<Leader>t', '<cmd>te<CR>')
 vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]])
 
 -- Unmap
-vim.keymap.set('n', 'rr', 'r')
-vim.keymap.set('n', 'r', '<Nop>')
+vim.keymap.set({ 'x', 'n', }, 'rr', 'r')
+-- vim.keymap.set('n', 'r', '<Nop>')
 vim.keymap.set('n', '<Enter>', '<Nop>')
 vim.keymap.set('n', '<C-n>', '<Nop>')
 vim.keymap.set('n', '<C-p>', '<Nop>')
@@ -30,69 +30,40 @@ vim.keymap.set('i', 'ù', '<Esc>`^u')
 vim.keymap.set('i', '<C-BS>', '<Esc>cvb')
 -- vim.keymap.set('n', '<BS>', '"_ciw')
 
-local input_cache = nil
 vim.api.nvim_set_hl(0, 'BoosterAppendChar', { fg = 'white', bg = 'none', })
+local input_cache = nil
 local namespace = vim.api.nvim_create_namespace('booster')
 
-local function setVirtualText(row, col)
-end
-
--------------------- Main function
-local function appendSingleChar(row, col)
-  if input_cache then
-    -- Set character
-    vim.api.nvim_buf_set_text(0, row, col, row, col,
-      { string.rep(input_cache, vim.v.count1), })
-  end
-end
-
--------------------- Position
 local function getLineStr(row)
   return vim.api.nvim_buf_get_lines(0, row - 1, row, false)[1]
 end
 
--- local function endOfLinePos()
---   local row = vim.api.nvim_win_get_cursor(0)[1]
---   return
---       row - 1,
---       string.len(getLineStr(row))
--- end
-
--- local function startOfLinePos()
---   local row = vim.api.nvim_win_get_cursor(0)[1]
---   return
---       row - 1,
---       (string.find(getLineStr(row), '(%S)') or 1) - 1
--- end
-
-local function startOfLineCol(row)
+-------------------- Column position to insert the new character
+local function colBeforeLine(row)
   return (string.find(getLineStr(row), '(%S)') or 1) - 1
 end
 
-local function endOfLineCol(row)
+local function colAfterLine(row)
   return string.len(getLineStr(row))
 end
 
-local function beforePos()
-  local pos = vim.api.nvim_win_get_cursor(0)
-  return
-      pos[1] - 1,
-      pos[2]
+local function colBeforeCursor()
+  return vim.api.nvim_win_get_cursor(0)[2]
 end
 
-local function afterPos()
-  local pos = vim.api.nvim_win_get_cursor(0)
-  return
-      pos[1] - 1,
-      pos[2] + 1
+local function colAfterCursor()
+  return vim.api.nvim_win_get_cursor(0)[2] + 1
 end
 
-local function main(acol)
-  local isVisual = string.match(vim.api.nvim_get_mode().mode, '[vV]')
-  local startPos = vim.api.nvim_buf_get_mark(0, isVisual and '<' or '[')
-  local endPos = vim.api.nvim_buf_get_mark(0, isVisual and '>' or ']')
-  local lines = vim.api.nvim_buf_get_lines(0, startPos[1] - 1, endPos[1],
-    false)
+-------------------- Main function
+local function appendSingleChar(getColumn)
+  local isVisual = string.match(vim.api.nvim_get_mode().mode, '[vV\22]')
+  local startRow = vim.api.nvim_buf_get_mark(0, isVisual and '<' or '[')[1]
+  local endRow = vim.api.nvim_buf_get_mark(0, isVisual and '>' or ']')[1]
+  local lines = vim.api.nvim_buf_get_lines(0, startRow - 1, endRow, false)
+
+  -- vim.print(startPos)
+  -- vim.print(endPos)
 
   if not input_cache then
     local extmarks = {}
@@ -100,8 +71,8 @@ local function main(acol)
     -- Set virtual text
     for i, str in ipairs(lines) do
       table.insert(extmarks, vim.api.nvim_buf_set_extmark(
-        0, namespace, startPos[1] - 2 + i,
-        acol(startPos[1] - 1 + i),
+        0, namespace, startRow - 2 + i,
+        getColumn(startRow - 1 + i),
         {
           virt_text = { { '_', 'BoosterAppendChar', }, },
           virt_text_pos = 'inline',
@@ -131,8 +102,8 @@ local function main(acol)
 
   -- Set character
   for i, str in ipairs(lines) do
-    local row = startPos[1] - 2 + i
-    local col = acol(startPos[1] - 1 + i)
+    local row = startRow - 2 + i
+    local col = getColumn(startRow - 1 + i)
     vim.api.nvim_buf_set_text(0, row, col, row, col,
       { string.rep(input_cache, vim.v.count1), })
   end
@@ -140,26 +111,30 @@ end
 
 -------------------- Initialization
 function _G._appendCharEndLine()
-  return main(endOfLineCol)
+  return appendSingleChar(colAfterLine)
 end
 
 function _G._appendCharStartLine()
-  return main(startOfLineCol)
+  return appendSingleChar(colBeforeLine)
 end
 
 function _G._appendCharBefore()
-  return appendSingleChar(beforePos())
+  return appendSingleChar(colBeforeCursor)
 end
 
 function _G._appendCharAfter()
-  return appendSingleChar(afterPos())
+  return appendSingleChar(colAfterCursor)
 end
 
 -------------------- Dot repeat
 local function dot_repeat_wrapper(name)
   vim.go.operatorfunc = 'v:lua.' .. name
 
-  local isVisual = string.match(vim.api.nvim_get_mode().mode, '[vV]')
+  local isVisual = string.match(vim.api.nvim_get_mode().mode, '[vV\22]')
+
+  vim.print(vim.api.nvim_get_mode())
+  vim.print(isVisual)
+
   if isVisual then
     vim.api.nvim_feedkeys('g@', 'n', false)
   else
@@ -190,8 +165,8 @@ end
 -------------------- Mappings
 vim.keymap.set({ 'x', 'n', }, 'ga', appendCharEndLine, { expr = true, })
 vim.keymap.set({ 'x', 'n', }, 'gi', appendCharStartLine, { expr = true, })
-vim.keymap.set('n', 'ra', appendCharAfter, { expr = true, })
-vim.keymap.set('n', 'ri', appendCharBefore, { expr = true, })
+vim.keymap.set({ 'x', 'n', }, 'ra', appendCharAfter, { expr = true, })
+vim.keymap.set({ 'x', 'n', }, 'ri', appendCharBefore, { expr = true, })
 
 function _G._appendNewlineBelow()
   local newLines = {}; for i = 1, vim.v.count1 do newLines[i] = '' end

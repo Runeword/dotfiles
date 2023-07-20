64d4841e274d0107f8d98c90c4bcf1ae12df0e50
Runeword <60324746+Runeword@users.noreply.github.com>
2023-07-20 22:39:50 +0200

    Update .config/nvim/lua/plugins/putter.lua

--- dot_config/nvim/lua/plugins/putter.lua
+++ dot_config/nvim/lua/plugins/putter.lua
@@ -2,17 +2,17 @@ local vim = vim
 
 return {
   enabled = false,
-  -- 'Runeword/putter.nvim',
   dir = '/home/charles/Documents/dev/putter.nvim',
+  -- 'Runeword/putter.nvim',
 
   config = function()
     vim.keymap.set({ 'n', 'x', }, 'gp', require('putter').putLinewise(']p`]'))
     vim.keymap.set({ 'n', 'x', }, 'gP', require('putter').putLinewise(']P`]'))
 
+    vim.keymap.set({ 'n', 'x', }, 'p', require('putter').putCharwise('p'))
     vim.keymap.set({ 'n', 'x', }, 'P', require('putter').putCharwise('P'))
-    vim.keymap.set({ 'n', 'x', }, 'p', require('putter').putWordwise())
-    -- vim.keymap.set({ 'n', 'x', }, 'p', require('putter').putCharwise('p'))
 
+    -- vim.keymap.set({ 'n', 'x', }, 'p', require('putter').putWordwise())
     -- vim.keymap.set({ 'n', 'x', }, 'gp', require('putter').putCharwisePrefix('p'))
     -- vim.keymap.set({ 'n', 'x', }, 'gP', require('putter').putCharwiseSuffix('P'))
 
@@ -29,24 +29,5 @@ return {
     -- vim.keymap.set("n", "<leader><Tab>", require("putter").addBuffersToQfList)
     -- vim.keymap.set("n", "<C-down>", require("putter").cycleNextLocItem, silent)
     -- vim.keymap.set("n", "<C-up>", require("putter").cyclePrevLocItem, silent)
-    -- vim.keymap.set("n", "<C-q>", "&buftype is# 'quickfix' ? ':try | cclose | catch | q! | catch | endtry<CR>' : ':q!<CR>'", expr)
-
-    -- vim.keymap.set("n", "t", require("putter").__dot_repeat('p'))
-
-    -- local counter = 0
-    --
-    -- function _G.__dot_repeat(motion) -- 4.
-    --     if motion == nil then
-    --         vim.o.operatorfunc = "v:lua.require('putter').snapToLineEnd('_x')()"
-    --         return "g@$" -- 2.
-    --     end
-    --
-    --     -- print("counter:", counter, "motion:", motion)
-    --     counter = counter + 1
-    -- end
-    --
-    -- vim.keymap.set("n", "ga", _G.__dot_repeat, expr)
-
-    -- -- stylua: ignore
   end,
 }

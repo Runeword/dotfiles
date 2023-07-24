c9d5cf1376ed186d31695f19a43dfe1acda9d7ab
Runeword <60324746+Runeword@users.noreply.github.com>
2023-07-24 19:18:55 +0200

    Update flake/home-manager/charles/modules/sh/functions/fm.sh

--- flake/home-manager/charles/modules/sh/functions/fm.sh
+++ flake/home-manager/charles/modules/sh/functions/fm.sh
@@ -11,7 +11,8 @@ __open_file() {
 	}
 
 	# Find then select file(s)
-	local selected_files=$(
+	local selected_files
+	selected_files=$(
 		find . \
 			\( -path './.git' -o -path './flake-inputs' -o -path './.nix-defexpr' \
 			-o -path './.nix-profile' -o -path './node_modules' -o -path './.local' \) \
@@ -34,12 +35,13 @@ __open_file() {
 	[ -z "$selected_files" ] && return 0
 
 	# Check the number of selected files
-	local num_lines=$(echo "$selected_files" | wc -l)
+	local num_lines
+	num_lines="$(echo "$selected_files" | wc -l)"
 
 	# If single directory selection
 	if [ "$num_lines" -eq 1 ] && [ -d "$selected_files" ]; then
 		# Then cd into it
-		if cd $selected_files; then
+		if cd "$selected_files"; then
 			history -s "cd $selected_files"
 		else
 			echo "Error: could not change directory to $selected_files"
@@ -47,8 +49,16 @@ __open_file() {
 		fi
 	else # If single or multiple file selection
 		# Then open it in editor
-		if $EDITOR $selected_files; then
-			history -s "$EDITOR $selected_files"
+
+		local selected_files_formated
+		selected_files_formated=$(echo "$selected_files" | paste -sd ' ')
+
+		if eval "$EDITOR $selected_files_formated"; then
+			if [ -n "$BASH_VERSION" ]; then
+				history -s "$EDITOR $selected_files_formated"
+			elif [ -n "$ZSH_VERSION" ]; then
+				print -s "$EDITOR $selected_files_formated"
+			fi
 		else
 			echo "Error: could not open $selected_files with $EDITOR"
 			return 1

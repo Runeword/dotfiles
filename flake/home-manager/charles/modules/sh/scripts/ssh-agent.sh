38eed69a35c9db1189bd41c5adcb910589bd391c
Runeword <60324746+Runeword@users.noreply.github.com>
2023-07-26 18:54:59 +0200

    Update flake/home-manager/charles/modules/sh/scripts/ssh-agent.sh

--- flake/home-manager/charles/modules/sh/scripts/ssh-agent.sh
+++ flake/home-manager/charles/modules/sh/scripts/ssh-agent.sh
@@ -1,6 +1,8 @@
+#!/bin/sh
+
 if ! pgrep -u "$USER" ssh-agent >/dev/null; then
-  ssh-agent -t 2h >"$XDG_RUNTIME_DIR/ssh-agent.env"
+	ssh-agent -t 2h >"$XDG_RUNTIME_DIR/ssh-agent.env"
 fi
-if [[ ! "$SSH_AUTH_SOCK" ]]; then
-  source "$XDG_RUNTIME_DIR/ssh-agent.env" >/dev/null
+if [ -z "$SSH_AUTH_SOCK" ]; then
+	. "$XDG_RUNTIME_DIR/ssh-agent.env" >/dev/null
 fi

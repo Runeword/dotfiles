e349249cdf689e8cc94d3990628f8c19d67fdbaa
Runeword <60324746+Runeword@users.noreply.github.com>
2023-07-26 20:17:26 +0200

    Update flake/home-manager/charles/modules/zsh/default.nix

--- flake/home-manager/charles/modules/zsh/default.nix
+++ flake/home-manager/charles/modules/zsh/default.nix
@@ -1,10 +1,12 @@
 {
   programs.zsh.enable = true;
-  programs.zsh.enableAutosuggestions = true;
+  programs.zsh.enableAutosuggestions = false;
   programs.zsh.enableCompletion = true;
   programs.zsh.enableVteIntegration = false;
   programs.zsh.autocd = false;
+
   programs.zsh.initExtra = builtins.readFile ./.zshrc;
+
   programs.zsh.zplug = {
     enable = true;
     plugins = [

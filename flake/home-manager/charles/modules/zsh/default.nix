de35d1b1f9c3744cb83d87758ebc02cd707ecf45
Runeword <60324746+Runeword@users.noreply.github.com>
2023-07-27 20:57:37 +0200

    Update flake/home-manager/charles/modules/zsh/default.nix

--- flake/home-manager/charles/modules/zsh/default.nix
+++ flake/home-manager/charles/modules/zsh/default.nix
@@ -1,16 +1,20 @@
 {
   programs.zsh.enable = true;
-  programs.zsh.enableAutosuggestions = false;
-  programs.zsh.enableCompletion = true;
-  programs.zsh.enableVteIntegration = false;
-  programs.zsh.autocd = false;
+  programs.zsh.enableAutosuggestions = true;
+  programs.zsh.enableCompletion = false;
 
   programs.zsh.initExtra = builtins.readFile ./.zshrc;
+  programs.zsh.initExtraFirst = builtins.readFile ./.zshrc_first;
 
   programs.zsh.zplug = {
     enable = true;
     plugins = [
-      {name = "olets/zsh-abbr";}
+      {name = "marlonrichert/zsh-autocomplete";}
+      # {name = zdharma-continuum/fast-syntax-highlighting";}
+      {
+        name = "olets/zsh-abbr";
+        # tags = ["at:82fc2b53d7b1c2df60fdea6f13701cff9c536197"];
+      }
     ];
   };
 }

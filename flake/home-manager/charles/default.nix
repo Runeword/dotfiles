0953d925ad2024742b84350b34eb4893eff397c9
Runeword <60324746+Runeword@users.noreply.github.com>
2023-07-28 19:36:06 +0200

    Update flake/home-manager/charles/default.nix

--- flake/home-manager/charles/default.nix
+++ flake/home-manager/charles/default.nix
@@ -10,31 +10,31 @@
   ];
 
   fonts.fontconfig.enable = true;
-  xdg.mime.enable = true;
-  xdg.mimeApps.enable = true;
-  xdg.mimeApps.defaultApplications = {
-    "application/zip" = ["xarchiver.desktop"];
-
-    "image/jpeg" = ["imv.desktop"];
-    "image/png" = ["imv.desktop"];
-    "image/avif" = ["imv.desktop"];
-    "image/bmp" = ["imv.desktop"];
-    "image/tiff" = ["imv.desktop"];
-    "image/svg+xml" = ["imv.desktop"];
-    "image/gif" = ["imv.desktop"];
-    "image/vnd.microsoft.icon" = ["imv.desktop"];
-    "image/webp" = ["imv.desktop"];
-
-    "application/json" = ["nvim.desktop"];
-    "application/ld+json" = ["nvim.desktop"];
-    "application/x-sh" = ["nvim.desktop"];
-    "application/xml" = ["nvim.desktop"];
-    "text/plain" = ["nvim.desktop"];
-    "text/css" = ["nvim.desktop"];
-    "text/csv" = ["nvim.desktop"];
-    "text/html" = ["nvim.desktop"];
-    "text/calendar" = ["nvim.desktop"];
-    "text/javascript" = ["nvim.desktop"];
+  # xdg.mime.enable = true;
+  # xdg.mimeApps.enable = true;
+  # xdg.mimeApps.defaultApplications = {
+  #   "application/zip" = ["xarchiver.desktop"];
+  #
+  #   "image/jpeg" = ["imv.desktop"];
+  #   "image/png" = ["imv.desktop"];
+  #   "image/avif" = ["imv.desktop"];
+  #   "image/bmp" = ["imv.desktop"];
+  #   "image/tiff" = ["imv.desktop"];
+  #   "image/svg+xml" = ["imv.desktop"];
+  #   "image/gif" = ["imv.desktop"];
+  #   "image/vnd.microsoft.icon" = ["imv.desktop"];
+  #   "image/webp" = ["imv.desktop"];
+  #
+  #   "application/json" = ["nvim.desktop"];
+  #   "application/ld+json" = ["nvim.desktop"];
+  #   "application/x-sh" = ["nvim.desktop"];
+  #   "application/xml" = ["nvim.desktop"];
+  #   "text/plain" = ["nvim.desktop"];
+  #   "text/css" = ["nvim.desktop"];
+  #   "text/csv" = ["nvim.desktop"];
+  #   "text/html" = ["nvim.desktop"];
+  #   "text/calendar" = ["nvim.desktop"];
+  #   "text/javascript" = ["nvim.desktop"];
 
     # [Added Associations]
     # application/zip=userapp-unzip-S2SZ71.desktop;
@@ -62,7 +62,8 @@
     # text/plain=nvim.desktop
     #
     # [Removed Associations]
-  };
+
+  # };
 
   # home.packages = [ inputs.src-cli.packages.x86_64-linux.default ];
   # xresources.properties."Xft.dpi" = 200;

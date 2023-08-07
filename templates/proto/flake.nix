3303e9eb9c5e37bdc9652a3159260af4450e64e8
Runeword <60324746+Runeword@users.noreply.github.com>
2023-08-07 13:22:54 +0200

    Update templates/proto/flake.nix

--- templates/proto/flake.nix
+++ templates/proto/flake.nix
@@ -4,42 +4,44 @@
   outputs = {self, ...} @ inputs: let
     pkgs = import inputs.nixpkgs {
       system = "x86_64-linux";
-      overlays = [
-        (self: super: {
-          supabase-cli = super.supabase-cli.override (old: {
-            buildGoModule = args:
-              pkgs.buildGoModule (args
-                // {
-                  version = "1.50.3";
-                  src = super.fetchFromGitHub {
-                    owner = "supabase";
-                    repo = "cli";
-                    rev = "v1.50.3";
-                    sha256 = "sha256-fj8kMvHSs1ZOnMgyaWAyIAwzK6LJz0gxhJ+Dpf5hwis=";
-                  };
-                  ldflags = ["-s" "-w" "-X" "github.com/supabase/cli/cmd.version=1.50.3"];
-                  vendorSha256 = "sha256-bxqfg77f6BuUy+9KrcToWJRzfwshOdqEQkAq4IFaxS8=";
-                  postInstall = ''
-                    rm $out/bin/{codegen,listdep}
-                    mv $out/bin/{cli,supabase}
-                    installShellCompletion --cmd supabase \
-                      --bash <($out/bin/supabase completion bash) \
-                      --fish <($out/bin/supabase completion fish) \
-                      --zsh <($out/bin/supabase completion zsh)
-                  '';
-                });
-          });
-        })
-        # supabase-cli = super.supabase-cli.overrideAttrs (old: {
-        #   src = super.fetchFromGitHub {
-        #     owner = "supabase";
-        #     repo = "cli";
-        #     rev = "v1.45.1";
-        #     sha256 = "sha256-fj8kMvHSs1ZOnMgyaWAyIAwzK6LJz0gxhJ+Dpf5hwis=";
-        #   };
-        #   ldflags = [ "-s" "-w" "-X" "github.com/supabase/cli/cmd.version=1.45.1" ];
-        # });
-      ];
+      # overlays = [
+        # (self: super: {
+        #   supabase-cli = super.supabase-cli.override (old: {
+        #     buildGoModule = args:
+        #       pkgs.buildGoModule (args
+        #         // {
+        #           version = "1.50.3";
+        #           src = super.fetchFromGitHub {
+        #             owner = "supabase";
+        #             repo = "cli";
+        #             rev = "v1.50.3";
+        #             sha256 = "sha256-fj8kMvHSs1ZOnMgyaWAyIAwzK6LJz0gxhJ+Dpf5hwis=";
+        #           };
+        #           ldflags = ["-s" "-w" "-X" "github.com/supabase/cli/cmd.version=1.50.3"];
+        #           vendorSha256 = "sha256-bxqfg77f6BuUy+9KrcToWJRzfwshOdqEQkAq4IFaxS8=";
+        #           postInstall = ''
+        #             rm $out/bin/{codegen,listdep}
+        #             mv $out/bin/{cli,supabase}
+        #             installShellCompletion --cmd supabase \
+        #               --bash <($out/bin/supabase completion bash) \
+        #               --fish <($out/bin/supabase completion fish) \
+        #               --zsh <($out/bin/supabase completion zsh)
+        #           '';
+        #         });
+        #   });
+        # })
+
+        ## supabase-cli = super.supabase-cli.overrideAttrs (old: {
+        ##   src = super.fetchFromGitHub {
+        ##     owner = "supabase";
+        ##     repo = "cli";
+        ##     rev = "v1.45.1";
+        ##     sha256 = "sha256-fj8kMvHSs1ZOnMgyaWAyIAwzK6LJz0gxhJ+Dpf5hwis=";
+        ##   };
+        ##   ldflags = [ "-s" "-w" "-X" "github.com/supabase/cli/cmd.version=1.45.1" ];
+        ## });
+
+      # ];
     };
   in {
     devShells.x86_64-linux.default = pkgs.mkShell {

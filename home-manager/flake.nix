da6766da24f68b042481c9227d7eb786cec1d9e4
Runeword <60324746+Runeword@users.noreply.github.com>
2023-08-13 12:35:37 +0200

    Add home-manager/flake.lock Add home-manager/flake.nix

---
+++ home-manager/flake.nix
@@ -0,0 +1,37 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  inputs.home-manager.url = "github:nix-community/home-manager";
+  inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+  inputs.neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+  # inputs.neovim-nightly-overlay.inputs.nixpkgs.url = "github:nixos/nixpkgs?rev=fad51abd42ca17a60fc1d4cb9382e2d79ae31836";
+
+  inputs.hyprland.url = "github:hyprwm/Hyprland";
+
+  inputs.hyprland-contrib.url = "github:hyprwm/contrib";
+  inputs.hyprland-contrib.inputs.nixpkgs.follows = "nixpkgs";
+
+  # inputs.src-cli.url = "github:sourcegraph/src-cli?dir=contrib";
+  # inputs.nixified-ai.url = "github:nixified-ai/flake";
+
+  outputs = {self, ...} @ inputs: let
+    system = "x86_64-linux";
+  in {
+    homeConfigurations.charles = inputs.home-manager.lib.homeManagerConfiguration {
+      pkgs = import inputs.nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+        overlays = [
+          inputs.neovim-nightly-overlay.overlay
+        ];
+      };
+
+      extraSpecialArgs = {inherit inputs;};
+
+      modules = [
+        ./charles
+      ];
+    };
+  };
+}

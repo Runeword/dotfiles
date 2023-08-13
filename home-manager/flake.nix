17339ac11925dcff026d7d25ccae38f8f837ba58
Runeword <60324746+Runeword@users.noreply.github.com>
2023-08-13 12:46:28 +0200

    Update home-manager/flake.nix

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -7,11 +7,6 @@
   inputs.neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
   # inputs.neovim-nightly-overlay.inputs.nixpkgs.url = "github:nixos/nixpkgs?rev=fad51abd42ca17a60fc1d4cb9382e2d79ae31836";
 
-  inputs.hyprland.url = "github:hyprwm/Hyprland";
-
-  inputs.hyprland-contrib.url = "github:hyprwm/contrib";
-  inputs.hyprland-contrib.inputs.nixpkgs.follows = "nixpkgs";
-
   # inputs.src-cli.url = "github:sourcegraph/src-cli?dir=contrib";
   # inputs.nixified-ai.url = "github:nixified-ai/flake";
 

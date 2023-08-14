034f265935823d03bc20614b5e987045ff79846d
Runeword <60324746+Runeword@users.noreply.github.com>
2023-08-14 20:01:44 +0200

    Update nixos/flake.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,8 +1,14 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  # inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs?rev=b12803b6d90e2e583429bb79b859ca53c348b39a";
+  # inputs.nixpkgs.url = "github:nixos/nixpkgs";
+
   inputs.nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
   inputs.hyprland.url = "github:hyprwm/Hyprland";
+  # inputs.hyprland.inputs.nixpkgs.follows = "nixpgks";
+  # inputs.hyprland.inputs.nixpkgs.url = "github:nixos/nixpkgs?rev=b12803b6d90e2e583429bb79b859ca53c348b39a";
+  # inputs.hyprland.inputs.nixpkgs.follows = "nixpkgs-unstable";
 
   outputs = {self, ...} @ inputs: let
     system = "x86_64-linux";

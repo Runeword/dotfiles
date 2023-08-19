c7e6265110fdfb6eaaa24363c09edbffd5b0913d
Runeword <60324746+Runeword@users.noreply.github.com>
2023-08-19 20:27:30 +0200

    Update templates/flake.nix

--- templates/flake.nix
+++ templates/flake.nix
@@ -12,6 +12,14 @@
       firebase.path = ./firebase;
       firebase.description = "firebase";
       firebase.welcomeText = "firebase";
+
+      go.path = ./go;
+      go.description = "go";
+      go.welcomeText = "go";
+
+      rust.path = ./rust;
+      rust.description = "rust";
+      rust.welcomeText = "rust";
     };
   };
 }

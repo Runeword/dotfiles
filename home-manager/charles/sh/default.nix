77269b778dd96f56ef2185f92ff1b29e8da8bac4
Runeword <60324746+Runeword@users.noreply.github.com>
2023-08-23 19:18:12 +0200

    Update home-manager/charles/sh/default.nix

--- home-manager/charles/sh/default.nix
+++ home-manager/charles/sh/default.nix
@@ -3,5 +3,15 @@
     ./aliases.nix
   ];
 
-  home.sessionVariables._ZO_FZF_OPTS = "--reverse --height 40% --no-separator --border none"; # zoxide fzf options
+  # fzf
+  home.sessionVariables._ZO_FZF_OPTS = "--reverse --height 40% --no-separator --border none";
+  home.sessionVariables.FZF_DEFAULT_COMMAND = "fd --hidden --follow --no-ignore --exclude .git --exclude node_modules";
+  home.sessionVariables.FZF_DEFAULT_OPTS = "
+  --reverse \
+  --no-separator \
+  --border none --color=fg:#d0d0d0,bg:-1,hl:#7e6fc4 \
+  --color=fg+:#ffffff,bg+:-1,hl+:#3dd4a5 \
+  --color=info:#d0d0d0,prompt:#ffee85,pointer:#3dd4a5 \
+  --color=marker:#3dd4a5,spinner:#ffee85,header:#d0d0d0
+  ";
 }

8f86e6dc00dd82f5b501d23bfec8ef3b21656540
Runeword <60324746+Runeword@users.noreply.github.com>
2023-08-25 22:41:41 +0200

    Update home-manager/charles/default.nix

--- home-manager/charles/default.nix
+++ home-manager/charles/default.nix
@@ -40,10 +40,13 @@
 
     zellij.enable = true;
 
-    # obs-studio.enable = true;
-    # obs-studio.plugins = [
-    #   pkgs.obs-studio-plugins.wlrobs
-    # ];
+    obs-studio.enable = true;
+    obs-studio.plugins = with pkgs.obs-studio-plugins; [
+      obs-gstreamer
+      obs-pipewire-audio-capture
+      obs-vkcapture
+      wlrobs
+    ];
   };
 
   fonts.fontconfig.enable = true;

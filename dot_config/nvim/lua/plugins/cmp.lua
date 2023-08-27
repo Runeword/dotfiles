a7de740f01610b7c79b1039988d19dd06555e6cc
Runeword <60324746+Runeword@users.noreply.github.com>
2023-08-27 17:50:09 +0200

    Update .config/nvim/lua/plugins/cmp.lua

--- dot_config/nvim/lua/plugins/cmp.lua
+++ dot_config/nvim/lua/plugins/cmp.lua
@@ -3,6 +3,15 @@ local vim = vim
 return {
   'hrsh7th/nvim-cmp',
 
+  event = 'InsertEnter',
+
+  dependencies = {
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-cmdline',
+  },
+
   config = function()
     local cmp = require 'cmp'
 

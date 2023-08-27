6dfe690c18e608d9d1774ec244433a1d87be1d2d
Runeword <60324746+Runeword@users.noreply.github.com>
2023-08-27 17:50:10 +0200

    Update .config/nvim/lua/plugins/coq.lua

--- dot_config/nvim/lua/plugins/coq.lua
+++ dot_config/nvim/lua/plugins/coq.lua
@@ -88,18 +88,71 @@ return {
       command = 'silent! COQsnips compile',
     })
 
-    vim.keymap.set('i', '<Esc>',
-      function() return vim.fn.pumvisible() == 1 and '<C-e><Esc>`^' or '<Esc>`^' end,
-      { expr = true, })
-    vim.keymap.set('i', '<C-c>',
-      function() return vim.fn.pumvisible() == 1 and '<C-e><C-c>' or '<C-c>' end,
-      { expr = true, })
-    vim.keymap.set('i', '<Tab>',
-      function() return vim.fn.pumvisible() == 1 and '<C-n>' or '<Tab>' end,
-      { expr = true, })
-    vim.keymap.set('i', '<S-Tab>',
-      function() return vim.fn.pumvisible() == 1 and '<C-p>' or '<BS>' end,
-      { expr = true, })
-    vim.keymap.set('n', '<Leader>cs', function() require('coq').Snips('edit') end)
+    ----------------------------------------------------- windwp/nvim-autopairs
+    --   local npairs = require('nvim-autopairs')
+
+    --   npairs.setup({
+    --     map_bs = true,
+    --     map_cr = false,
+    --     check_ts = true,
+    --     ignored_next_char = '[%w%.]',
+    --     -- fast_wrap = {
+    --     --   map = "<C-h>",
+    --     --   chars = { "{", "[", "(", '"', "'" },
+    --     --   pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+    --     --   offset = 0,
+    --     --   end_key = "s",
+    --     --   keys = "aoeuhtns",
+    --     --   check_comma = true,
+    --     --   highlight = "Search",
+    --     --   highlight_grey = "Comment",
+    --     -- },
+    --   })
+
+    -- vim.api.nvim_create_augroup('coq', { clear = true, })
+    -- vim.api.nvim_create_autocmd('BufWritePost', {
+    --   group = 'coq',
+    --   pattern = '*/.config/nvim/coq-user-snippets/*.snip',
+    --   command = 'silent! COQsnips compile',
+    -- })
+
+    -- vim.keymap.set('i', '<Esc>',
+    --   function() return vim.fn.pumvisible() == 1 and '<C-e><Esc>`^' or '<Esc>`^' end,
+    --   { expr = true, })
+    -- vim.keymap.set('i', '<C-c>',
+    --   function() return vim.fn.pumvisible() == 1 and '<C-e><C-c>' or '<C-c>' end,
+    --   { expr = true, })
+    -- vim.keymap.set('i', '<Tab>',
+    --   function() return vim.fn.pumvisible() == 1 and '<C-n>' or '<Tab>' end,
+    --   { expr = true, })
+    -- vim.keymap.set('i', '<S-Tab>',
+    --   function() return vim.fn.pumvisible() == 1 and '<C-p>' or '<BS>' end,
+    --   { expr = true, })
+    -- vim.keymap.set('n', '<Leader>cs', function() require('coq').Snips('edit') end)
+
+    --   -- skip it, if you use another global object
+    --   _G.MUtils = {}
+
+    --   MUtils.CR = function()
+    --     if vim.fn.pumvisible() ~= 0 then
+    --       if vim.fn.complete_info({ 'selected', }).selected ~= -1 then
+    --         return npairs.esc('<c-y>')
+    --       else
+    --         return npairs.esc('<c-e>') .. npairs.autopairs_cr()
+    --       end
+    --     else
+    --       return npairs.autopairs_cr()
+    --     end
+    --   end
+    --   vim.api.nvim_set_keymap('i', '<cr>', 'v:lua.MUtils.CR()', { expr = true, noremap = true, })
+
+    --   MUtils.BS = function()
+    --     if vim.fn.pumvisible() ~= 0 and vim.fn.complete_info({ 'mode', }).mode == 'eval' then
+    --       return npairs.esc('<c-e>') .. npairs.autopairs_bs()
+    --     else
+    --       return npairs.autopairs_bs()
+    --     end
+    --   end
+    --   vim.api.nvim_set_keymap('i', '<bs>', 'v:lua.MUtils.BS()', { expr = true, noremap = true, })
   end,
 }

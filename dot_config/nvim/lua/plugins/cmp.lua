2739f1ae88630f5fd4bc00fafbf220e268c92c32
Runeword <60324746+Runeword@users.noreply.github.com>
2023-08-27 19:18:47 +0200

    Update .config/nvim/lua/plugins/cmp.lua

--- dot_config/nvim/lua/plugins/cmp.lua
+++ dot_config/nvim/lua/plugins/cmp.lua
@@ -35,12 +35,26 @@ return {
       },
 
       mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-        ['<C-f>'] = cmp.mapping.scroll_docs(4),
-        ['<C-Space>'] = cmp.mapping.complete(),
-        ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm({ select = true, }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+        -- ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        -- ['<C-f>'] = cmp.mapping.scroll_docs(4),
+        -- ['<C-Space>'] = cmp.mapping.complete(),
+        -- ['<C-e>'] = cmp.mapping.abort(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true, }),
       }),
+
+      formatting = {
+        format = function(entry, vim_item)
+          vim_item.menu = ({
+            buffer = '[Buffer]',
+            nvim_lsp = '[LSP]',
+            luasnip = '[LuaSnip]',
+            codeium = '[codeium]',
+            nvim_lua = '[Lua]',
+            latex_symbols = '[LaTeX]',
+          })[entry.source.name]
+          return vim_item
+        end,
+      },
 
       sources = cmp.config.sources({
         { name = 'nvim_lsp', },

36ee105fef2553b5cb1ec64e5e6ef25da7cc0d2b
Runeword <60324746+Runeword@users.noreply.github.com>
2023-08-29 20:53:19 +0200

    Update home-manager/charles/packages.nix

--- home-manager/charles/packages.nix
+++ home-manager/charles/packages.nix
@@ -1,10 +1,22 @@
 {
   pkgs,
   inputs,
-  # lib,
+  lib,
   ...
 }: {
   # }: let
+
+  # tmuxKeylocker = pkgs.tmuxPlugins.mkTmuxPlugin {
+  #   pluginName = "tmux-keylocker";
+  #   version = "1.0";
+  #   src = pkgs.fetchFromGitHub {
+  #     owner = "TheSast";
+  #     repo = "tmux-keylocker";
+  #     rev = "c98dfd0956b458a8e71304f3532e8c3053df9555";
+  #     sha256 = "sha256-AdVPL7tZxTJ05Q9b41ejCw/2kFNXrrsKkAIm8MAlbdw=";
+  #     # sha256 = lib.fakeSha256;
+  #   };
+  # };
 
   # pkgs-old = import (builtins.fetchGit {
   #   # Descriptive name to make the store path easier to identify
@@ -13,7 +25,6 @@
   #   ref = "refs/heads/nixpkgs-unstable";
   #   rev = "3c3b3ab88a34ff8026fc69cb78febb9ec9aedb16";
   # }) {inherit (pkgs) system;};
-
   # pkgs-old = import (builtins.fetchTarball {
   #   url = "https://github.com/NixOS/nixpkgs/archive/2f04861c8ad1aaa69d532f34b63bb03da9912ae7.tar.gz";
   #   sha256 = "sha256:11dxbmxqcyw87fcyj2dm7d94wax2s1phajwp99rycwjikq3ks8s6";
@@ -21,7 +32,6 @@
   # }) {inherit (pkgs) system;};
 
   # in {
-
   home.packages = with pkgs; [
     (nerdfonts.override {fonts = ["Hack" "DroidSansMono" "SourceCodePro"];})
     python311
@@ -36,6 +46,7 @@
     kitty
     tmux
     tmuxPlugins.resurrect
+    # tmuxKeylocker
 
     # ---------------------------------- Browsers
     google-chrome

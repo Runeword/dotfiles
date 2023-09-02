9f31cc18a56f15733f911858692ace951554f4f0
Runeword <60324746+Runeword@users.noreply.github.com>
2023-09-02 18:13:15 +0200

    Update .config/nvim/lua/options.lua

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -36,6 +36,8 @@ o.wildcharm = ('\t'):byte()
 o.wildignorecase = true
 o.completeopt = 'menuone,noinsert' -- Options for Insert mode completion
 o.pumblend = 10
+-- o.pumwidth = 40
+-- o.pumheight = 15
 o.clipboard =
 'unnamedplus'                      -- Have the clipboard be the same as my regular clipboard
 o.updatetime = 50                  -- Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable delays and poor user experience

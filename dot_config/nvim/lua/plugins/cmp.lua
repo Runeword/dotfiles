ba85f460b047d03bf96315cb2542b94069d73a60
Runeword <60324746+Runeword@users.noreply.github.com>
2023-09-02 23:53:12 +0200

    Update .config/nvim/lua/plugins/cmp.lua

--- dot_config/nvim/lua/plugins/cmp.lua
+++ dot_config/nvim/lua/plugins/cmp.lua
@@ -32,8 +32,11 @@ return {
       },
 
       window = {
-        -- completion = cmp.config.window.bordered(),
-        -- documentation = cmp.config.window.bordered(),
+        completion = {
+        },
+        documentation = {
+          winhighlight = 'Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None',
+        },
       },
 
       mapping = cmp.mapping.preset.insert({
@@ -53,6 +56,20 @@ return {
             codeium = 'codeium',
             nvim_lua = 'lua',
           })[entry.source.name]
+
+          local MAX_LABEL_WIDTH = 20
+          local MIN_LABEL_WIDTH = 10
+          local ELLIPSIS_CHAR = '...'
+          local label = vim_item.abbr
+          local truncated_label = vim.fn.strcharpart(label, 0, MAX_LABEL_WIDTH)
+
+          if truncated_label ~= label then
+            vim_item.abbr = truncated_label .. ELLIPSIS_CHAR
+          elseif string.len(label) < MIN_LABEL_WIDTH then
+            local padding = string.rep(' ', MIN_LABEL_WIDTH - string.len(label))
+            vim_item.abbr = label .. padding
+          end
+
           return vim_item
         end,
       },

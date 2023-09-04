2875011e6173ddbb6e72bb5ded7742d33b5fe5fc
Runeword <60324746+Runeword@users.noreply.github.com>
2023-09-04 22:25:05 +0200

    Update home-manager/charles/sh/functions/fm.sh

--- home-manager/charles/sh/functions/fm.sh
+++ home-manager/charles/sh/functions/fm.sh
@@ -7,7 +7,7 @@ __open_file() {
 			tree -Ca -L 2 "$1" | sed 's/^/  /; 1s/^/\n/'
 		else
 			if command -v bat >/dev/null; then
-				bat --style=plain color=always "$1" | sed 's/^/  /; 1s/^/\n/'
+				bat --style=plain --color=always "$1" | sed 's/^/  /; 1s/^/\n/'
 			else
 				cat "$1"
 			fi

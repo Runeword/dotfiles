92deabd56d7bc7c675df79821d652513f2198e4d
Runeword <60324746+Runeword@users.noreply.github.com>
2023-09-10 21:56:05 +0200

    Update .config/nvim/lua/plugins/tablemode.lua

--- dot_config/nvim/lua/plugins/tablemode.lua
+++ dot_config/nvim/lua/plugins/tablemode.lua
@@ -3,6 +3,8 @@ local vim = vim
 return {
   'dhruvasagar/vim-table-mode',
 
+  enabled = false,
+
   init = function()
     vim.g.table_mode_disable_mappings = 1
     vim.g.table_mode_disable_tableize_mappings = 1
@@ -10,6 +12,6 @@ return {
   end,
 
   config = function()
-    vim.keymap.set('n', '<Leader>tt', '<Cmd>TableModeToggle<CR>')
+    vim.keymap.set('n', '<Leader>ta', '<Cmd>TableModeToggle<CR>')
   end,
 }

451a3fd7f283673555e1ed61433823dc89d5d648
Runeword <60324746+Runeword@users.noreply.github.com>
2023-09-29 21:17:33 +0200

    Update .config/nvim/lua/plugins/midnight.lua

--- dot_config/nvim/lua/plugins/midnight.lua
+++ dot_config/nvim/lua/plugins/midnight.lua
@@ -18,9 +18,12 @@ return {
       pattern = '*',
       callback = function()
         vim.api.nvim_set_hl(0, 'MatchParen',
-          { fg = '#7429ff', italic = true, bold = true, })
-        vim.api.nvim_set_hl(0, 'MatchWord',       { fg = '#7429ff', })
-        vim.api.nvim_set_hl(0, 'MatchBackground', { bg = '#1a1a3b', })
+          { fg = '#ff1994', bg = '#1a1a3b', italic = true, bold = true, })
+        vim.api.nvim_set_hl(0, 'MatchWord', { fg = '#ff1994', })
+        -- vim.api.nvim_set_hl(0, 'MatchParen',
+        --   { fg = '#7429ff', italic = true, bold = true, })
+        -- vim.api.nvim_set_hl(0, 'MatchWord',       { fg = '#7429ff', })
+        -- vim.api.nvim_set_hl(0, 'MatchBackground', { bg = '#1a1a3b', })
       end,
     })
 

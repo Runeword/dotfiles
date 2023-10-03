b6bb72341896074b9f4d00cf37cc578eba4c8a3a
Runeword <60324746+Runeword@users.noreply.github.com>
2023-10-03 21:54:03 +0200

    Update home-manager/charles/sh/functions/tmux.sh

--- home-manager/charles/sh/functions/tmux.sh
+++ home-manager/charles/sh/functions/tmux.sh
@@ -5,7 +5,7 @@ __tmux() {
 			--height 50% \
 			--delimiter=':' \
 			--header-first \
-			--header='<CR> attach  <C-k> kill  <C-a> kill all' \
+			--header='<CR> attach  <C-k> kill  <C-q> kill all' \
 			--bind='ctrl-k:reload-sync(tmux kill-session -t {1})' \
 			--bind='ctrl-q:execute(tmux kill-server)+abort' \
 			--bind='enter:execute(echo {1})+abort' \

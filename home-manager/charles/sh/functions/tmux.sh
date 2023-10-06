d48c7feb25e30a85a3c8a0588ce3ab8f156d1f47
Runeword <60324746+Runeword@users.noreply.github.com>
2023-10-06 21:22:29 +0200

    Update home-manager/charles/sh/functions/tmux.sh

--- home-manager/charles/sh/functions/tmux.sh
+++ home-manager/charles/sh/functions/tmux.sh
@@ -1,17 +1,16 @@
 __tmux() {
 	if [ -z "$(tmux list-sessions 2>/dev/null)" ]; then
-		printf 'new session name : ' && read -r session
+		trap 'return' INT
+		printf 'new session name : ' && read -r input
 
-		[ -z "$session" ] && return 1
-
-		tmux new-session -s "$session"
+		tmux new-session ${input:+-s"$input"}
 
 		return 1
 	fi
 
 	local item_pos
 
-	item_pos=$(tmux list-sessions -F '#{session_id} #{session_attached}' 2>/dev/null | awk '$2 == "1" {print NR}')
+  item_pos=$(tmux list-sessions -F '#{session_id}' | awk '{if ($1 == "'$(tmux display-message -p "#{session_id}")'") print NR}')
 
 	# tmux display-popup -E "tmux list-panes -a -F '#{window_index} #{window_name}' | fzf | cut -c 1-1 | xargs tmux select-window -t"
 	# ${TMUX:+--no-header} \
@@ -42,15 +41,46 @@ __tmux() {
 	fi
 }
 
-__new_session() {
-	printf 'new session name : ' && read -r session
+__switch_window() {
 
-	[ -z "$session" ] && return 1
+local item_pos
+item_pos=$(tmux list-sessions -F '#{session_id} #{session_attached}' 2>/dev/null | awk '$2 == "1" {print NR}')
+
+# tmux display-popup -E "tmux list-panes -a -F '#{window_index} #{window_name}' | fzf | cut -c 1-1 | xargs tmux select-window -t"
+# ${TMUX:+--no-header} \
+# tmux display-popup -E "tmux list-panes -a -F '#{window_index} #{window_id} #{window_name} #{session_name}' | fzf | cut -c 1-1 | xargs tmux select-window -t"
+
+session=$(
+  tmux ls -F '#{window_name} #{window_linked_sessions_list}' 2>/dev/null | fzf \
+    --reverse \
+    --cycle \
+    --jump-labels=3 \
+    --height 50% \
+    --delimiter=' ' \
+    --header-first \
+    --bind='tab:down,btab:up' \
+    ${TMUX:+--bind='focus:execute-silent(tmux select-window -t {1})'} \
+    ${TMUX:+--bind="load:pos($item_pos)"}
+)
+
+[ -z "$session" ] && return 1
+
+if [ -n "$TMUX" ]; then
+  tmux switch-client -t "$session"
+else
+  tmux attach-session -t "$session"
+fi
+}
+
+__new_session() {
+	trap 'return' INT
+	printf 'new session name : ' && read -r input
+
+	session=$(tmux new-session -d ${input:+-s"$input"} -P -F "#{session_name}")
 
 	if [ -n "$TMUX" ]; then
-		tmux new-session -d -s "$session"
 		tmux switch-client -t "$session"
 	else
-		tmux new-session -s "$session"
+		tmux attach-session -t "$session"
 	fi
 }

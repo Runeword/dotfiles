5a35e65f77231d1a2f19626855cc5344c003df69
Runeword <60324746+Runeword@users.noreply.github.com>
2023-10-07 19:01:02 +0200

    Update home-manager/charles/sh/functions/tmux.sh

--- home-manager/charles/sh/functions/tmux.sh
+++ home-manager/charles/sh/functions/tmux.sh
@@ -9,10 +9,10 @@ __tmux() {
 	fi
 
 	local item_pos
-  local session_id
+	local session_id
 
-  session_id=$(tmux display-message -p '#{session_id}')
-  item_pos=$(tmux list-sessions -F '#{session_id}' | awk '{if ($1 == "'$session_id'") print NR}')
+	session_id=$(tmux display-message -p '#{session_id}')
+	item_pos=$(tmux list-sessions -F '#{session_id}' | awk '{if ($1 == "'$session_id'") print NR}')
 
 	# tmux display-popup -E "tmux list-panes -a -F '#{window_index} #{window_name}' | fzf | cut -c 1-1 | xargs tmux select-window -t"
 	# ${TMUX:+--no-header} \
@@ -21,7 +21,6 @@ __tmux() {
 		tmux ls -F "#{session_name}" 2>/dev/null | fzf \
 			--reverse \
 			--cycle \
-			--jump-labels=3 \
 			--height 50% \
 			--delimiter=' ' \
 			--header-first \
@@ -44,33 +43,25 @@ __tmux() {
 }
 
 __switch_window() {
-local item_pos
-local window_id
-local window
+	local item_pos
+	local window_id
+	local window
 
-window_id=$(tmux display-message -p '#{window_id}')
-item_pos=$(tmux list-windows -a -F '#{window_id}' | awk '{if ($1 == "'$window_id'") print NR}')
+	window_id=$(tmux display-message -p '#{window_id}')
+	item_pos=$(tmux list-windows -a -F '#{window_id}' | awk '{if ($1 == "'$window_id'") print NR}')
 
-window=$(
-tmux list-windows -a -F '#{window_name} #{session_name} #{window_id} #{session_id}' 2>/dev/null | fzf \
-    --reverse \
-    --cycle \
-    --jump-labels=3 \
-    --height 50% \
-    --delimiter=' ' \
-    --header-first \
-    --bind='tab:down,btab:up' \
-    ${TMUX:+--bind='focus:execute-silent(tmux switch-client -t {4}; tmux select-window -t {3})'} \
-    ${TMUX:+--bind="load:pos($item_pos)"}
-)
-
-# [ -z "$session" ] && return 1
-#
-# if [ -n "$TMUX" ]; then
-#   tmux switch-client -t "$session"
-# else
-#   tmux attach-session -t "$session"
-# fi
+	window=$(
+		tmux list-windows -a -F '#{session_name} #{window_name} #{window_id} #{session_id}' 2>/dev/null | fzf \
+		  --with-nth='1,2' \
+			--reverse \
+			--cycle \
+			--height 50% \
+			--delimiter=' ' \
+			--header-first \
+			--bind='tab:down,btab:up' \
+			${TMUX:+--bind='focus:execute-silent(tmux switch-client -t {4}; tmux select-window -t {3})'} \
+			${TMUX:+--bind="load:pos($item_pos)"}
+	)
 }
 
 __new_session() {

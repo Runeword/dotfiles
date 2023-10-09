7f026a2454be3b95384accd048f25b94d8cb1cc0
Runeword <60324746+Runeword@users.noreply.github.com>
2023-10-09 21:33:50 +0200

    Update .config/nvim/lua/autocmd.lua

--- dot_config/nvim/lua/autocmd.lua
+++ dot_config/nvim/lua/autocmd.lua
@@ -10,6 +10,13 @@ augroup('quickfix',           { clear = true, })
 augroup('disableAutoComment', { clear = true, })
 augroup('term',               { clear = true, })
 augroup('view',               { clear = true, })
+augroup('cursor',             { clear = true, })
+
+vim.api.nvim_create_autocmd('ExitPre', {
+  group = 'cursor',
+  command = 'set guicursor=a:ver90',
+  desc = 'Set cursor back to beam when leaving Neovim.',
+})
 
 autocmd('TermOpen', {
   group = 'term',
@@ -81,36 +88,36 @@ vim.api.nvim_create_autocmd('colorscheme', {
   callback = function()
     -- #0a172e #1e2633 #424a57 #7a7c9e #222b66
 
-    vim.api.nvim_set_hl(0, 'diagnosticerror', { bg = 'NONE', fg = '#ff75a9', bold = false, italic = true, })
+    vim.api.nvim_set_hl(0, 'diagnosticerror',          { bg = 'NONE', fg = '#ff75a9', bold = false, italic = true, })
     vim.api.nvim_set_hl(0, 'diagnosticunderlineerror', { bg = 'NONE', undercurl = true, sp = '#ff75a9', })
 
-    vim.api.nvim_set_hl(0, 'diagnosticfloatingerror', { link = 'diagnosticvirtualtexterror', })
-    vim.api.nvim_set_hl(0, 'diagnosticfloatinghint',  { link = 'diagnosticvirtualtexthint', })
-    vim.api.nvim_set_hl(0, 'diagnosticfloatinginfo',  { link = 'diagnosticvirtualtextinfo', })
-    vim.api.nvim_set_hl(0, 'diagnosticfloatingwarn',  { link = 'diagnosticvirtualtextwarn', })
+    vim.api.nvim_set_hl(0, 'diagnosticfloatingerror',  { link = 'diagnosticvirtualtexterror', })
+    vim.api.nvim_set_hl(0, 'diagnosticfloatinghint',   { link = 'diagnosticvirtualtexthint', })
+    vim.api.nvim_set_hl(0, 'diagnosticfloatinginfo',   { link = 'diagnosticvirtualtextinfo', })
+    vim.api.nvim_set_hl(0, 'diagnosticfloatingwarn',   { link = 'diagnosticvirtualtextwarn', })
 
-    vim.api.nvim_set_hl(0, 'normalfloat',             { bg = 'none', })
-    vim.api.nvim_set_hl(0, 'floatborder',             { bg = 'none', fg = '#1e2633', })
+    vim.api.nvim_set_hl(0, 'normalfloat',              { bg = 'none', })
+    vim.api.nvim_set_hl(0, 'floatborder',              { bg = 'none', fg = '#1e2633', })
 
-    vim.api.nvim_set_hl(0, 'pmenu',                   { bg = 'black', fg = '#7a7c9e', })
-    vim.api.nvim_set_hl(0, 'pmenusel',                { bg = '#1e2633', fg = 'white', })
-    vim.api.nvim_set_hl(0, 'pmenusbar',               { bg = 'black', })
-    vim.api.nvim_set_hl(0, 'pmenuthumb',              { bg = '#1e2633', })
+    vim.api.nvim_set_hl(0, 'pmenu',                    { bg = 'black', fg = '#7a7c9e', })
+    vim.api.nvim_set_hl(0, 'pmenusel',                 { bg = '#1e2633', fg = 'white', })
+    vim.api.nvim_set_hl(0, 'pmenusbar',                { bg = 'black', })
+    vim.api.nvim_set_hl(0, 'pmenuthumb',               { bg = '#1e2633', })
 
-    vim.api.nvim_set_hl(0, 'linenr',                  { bg = 'none', fg = '#4b586e', })
-    vim.api.nvim_set_hl(0, 'cursorlinenr',            { bg = 'none', fg = 'white', })
-    vim.api.nvim_set_hl(0, 'cursorline',              { bg = 'none', })
-    vim.api.nvim_set_hl(0, 'cursorcolumn',            { bg = '#1e2633', })
+    vim.api.nvim_set_hl(0, 'linenr',                   { bg = 'none', fg = '#4b586e', })
+    vim.api.nvim_set_hl(0, 'cursorlinenr',             { bg = 'none', fg = 'white', })
+    vim.api.nvim_set_hl(0, 'cursorline',               { bg = 'none', })
+    vim.api.nvim_set_hl(0, 'cursorcolumn',             { bg = '#1e2633', })
 
-    vim.api.nvim_set_hl(0, 'search',                  { bg = '#5d00ff', })
-    vim.api.nvim_set_hl(0, 'incsearch',               { bg = '#faff00', fg = 'black', })
-    vim.api.nvim_set_hl(0, 'normal',                  { bg = 'none', })
+    vim.api.nvim_set_hl(0, 'search',                   { bg = '#5d00ff', })
+    vim.api.nvim_set_hl(0, 'incsearch',                { bg = '#faff00', fg = 'black', })
+    vim.api.nvim_set_hl(0, 'normal',                   { bg = 'none', })
 
-    vim.api.nvim_set_hl(0, 'signcolumn',              { bg = 'none', })
-    vim.api.nvim_set_hl(0, 'foldcolumn',              { bg = 'none', })
-    vim.api.nvim_set_hl(0, 'folded',                  { bg = 'none', })
+    vim.api.nvim_set_hl(0, 'signcolumn',               { bg = 'none', })
+    vim.api.nvim_set_hl(0, 'foldcolumn',               { bg = 'none', })
+    vim.api.nvim_set_hl(0, 'folded',                   { bg = 'none', })
 
-    vim.api.nvim_set_hl(0, 'nontext',                 { bg = 'none', fg = '#384354', })
-    vim.api.nvim_set_hl(0, 'whitespace',              { bg = 'none', fg = '#384354', })
+    vim.api.nvim_set_hl(0, 'nontext',                  { bg = 'none', fg = '#384354', })
+    vim.api.nvim_set_hl(0, 'whitespace',               { bg = 'none', fg = '#384354', })
   end,
 })

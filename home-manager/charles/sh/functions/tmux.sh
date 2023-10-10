480046200753ca95b661a51093e435387fb957e0
Runeword <60324746+Runeword@users.noreply.github.com>
2023-10-10 19:50:55 +0200

    Update home-manager/charles/sh/functions/tmux.sh

--- home-manager/charles/sh/functions/tmux.sh
+++ home-manager/charles/sh/functions/tmux.sh
@@ -20,12 +20,8 @@ __switch_session() {
 			--cycle \
 			--height 50% \
 			--delimiter=' ' \
-			--header-first \
-			--header='<C-k> kill  <C-Del> kill all' \
-			--bind='ctrl-k:reload-sync(tmux kill-session -t {1})' \
-			--bind='ctrl-delete:execute(tmux kill-server)+abort' \
-			--bind='enter:execute(echo {1})+abort' \
 			--bind='tab:down,btab:up' \
+      --bind='enter:execute(echo {1})+abort' \
 			${TMUX:+--bind='focus:execute-silent(tmux switch-client -t {1})'} \
 			${TMUX:+--bind="load:pos($item_pos)"}
 	)
@@ -54,7 +50,6 @@ __switch_window() {
 			--cycle \
 			--height 50% \
 			--delimiter=' ' \
-			--header-first \
 			--bind='tab:down,btab:up' \
 			${TMUX:+--bind='focus:execute-silent(tmux switch-client -t {4}; tmux select-window -t {3})'} \
 			${TMUX:+--bind="load:pos($item_pos)"}
@@ -63,7 +58,7 @@ __switch_window() {
 
 __new_session() {
 	trap 'return' INT
-	printf 'new session name : ' && read -r input
+	printf 'new session : ' && read -r input
 
 	session=$(tmux new-session -d ${input:+-s"$input"} -P -F "#{session_name}")
 

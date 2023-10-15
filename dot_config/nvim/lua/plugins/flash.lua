6dc7448d7fe44866a3c2f57dc2c8d491c0fe4857
Runeword <60324746+Runeword@users.noreply.github.com>
2023-10-15 23:52:09 +0200

    Update .config/nvim/lua/plugins/flash.lua

--- dot_config/nvim/lua/plugins/flash.lua
+++ dot_config/nvim/lua/plugins/flash.lua
@@ -89,5 +89,13 @@ return {
       function() require('flash').jump() end,
       desc = 'Flash',
     },
+    {
+      '<C-o>',
+      mode = { 'i', },
+      function()
+        require('flash').jump()
+      end,
+      desc = 'Flash',
+    },
   },
 }

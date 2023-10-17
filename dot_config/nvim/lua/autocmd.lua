e0f9557bec50c95bef76ec318799a69c37f36d55
Runeword <60324746+Runeword@users.noreply.github.com>
2023-10-17 22:00:00 +0200

    Update .config/nvim/lua/autocmd.lua

--- dot_config/nvim/lua/autocmd.lua
+++ dot_config/nvim/lua/autocmd.lua
@@ -12,6 +12,14 @@ augroup('term',               { clear = true, })
 augroup('view',               { clear = true, })
 augroup('cursor',             { clear = true, })
 
+-- Prevent escape from moving the cursor one character to the left
+vim.cmd([[
+let CursorColumnI = 0
+autocmd InsertEnter * let CursorColumnI = col('.')
+autocmd CursorMovedI * let CursorColumnI = col('.')
+autocmd InsertLeave * if col('.') != CursorColumnI | call cursor(0, col('.')+1) | endif
+]])
+
 vim.api.nvim_create_autocmd('ExitPre', {
   group = 'cursor',
   command = 'set guicursor=a:ver90',

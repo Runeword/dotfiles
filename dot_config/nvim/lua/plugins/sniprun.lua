99f402d8acf2edd06c0ad61c84b3e2533edf5143
Runeword <60324746+Runeword@users.noreply.github.com>
2023-10-29 21:33:06 +0100

    Update .config/nvim/lua/plugins/sniprun.lua

--- dot_config/nvim/lua/plugins/sniprun.lua
+++ dot_config/nvim/lua/plugins/sniprun.lua
@@ -14,11 +14,13 @@ return {
       live_mode_toggle = 'enable',
     })
 
-    vim.keymap.set({ 'n', 'v', }, '<leader>rr', '<Plug>SnipRun')
-    vim.keymap.set('n', '<leader>re', '<Plug>SnipReset')
-    vim.keymap.set('n', '<leader>rl', '<Plug>SnipLive')
-    vim.keymap.set('n', '<leader>rc', '<Plug>SnipClose')
-    vim.keymap.set('n', '<leader>ri', '<Plug>SnipInfo')
-    vim.keymap.set('n', '<leader>rm', '<Plug>SnipReplMemoryClean')
+    vim.keymap.set({ 'n', 'v' }, '<Leader>rr', '<Plug>SnipRun')
+    vim.keymap.set('n', '<Leader>re', '<Plug>SnipReset')
+    vim.keymap.set('n', 'gr', '<Plug>SnipRunOperator')
+    vim.keymap.set('n', '<Leader>rl', '<Plug>SnipLive')
+    vim.keymap.set('n', '<Leader>rc', '<Plug>SnipClose')
+    vim.keymap.set('n', '<Leader>ri', '<Plug>SnipInfo')
+    vim.keymap.set('n', '<Leader>rm', '<Plug>SnipReplMemoryClean')
+    -- vim.keymap.set('n', '<Leader>ra', ":let b:caret=winsaveview() <CR> | :%SnipRun <CR>| :call winrestview(b:caret) <CR>", { silent = true, })
   end,
 }

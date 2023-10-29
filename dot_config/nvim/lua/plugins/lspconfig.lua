077fe66714007ac66705ec7bb92d5ef954ce25cf
Runeword <60324746+Runeword@users.noreply.github.com>
2023-10-29 18:25:48 +0100

    Update .config/nvim/lua/plugins/lspconfig.lua

--- dot_config/nvim/lua/plugins/lspconfig.lua
+++ dot_config/nvim/lua/plugins/lspconfig.lua
@@ -79,19 +79,20 @@ return {
 
     local lspconfig = require('lspconfig')
 
-    lspconfig['eslint'].setup(set_config())
+    lspconfig.eslint.setup(set_config())
     lspconfig['lua_ls'].setup(set_config())
-    lspconfig['yamlls'].setup(set_config())
-    lspconfig['ccls'].setup(set_config())
-    lspconfig['jsonls'].setup(set_config())
-    lspconfig['gopls'].setup(set_config())
+    lspconfig.yamlls.setup(set_config())
+    lspconfig.ccls.setup(set_config())
+    lspconfig.jsonls.setup(set_config())
+    lspconfig.gopls.setup(set_config())
     lspconfig['rust_analyzer'].setup(set_config())
-    lspconfig['marksman'].setup(set_config())
-    lspconfig['terraformls'].setup(set_config())
+    lspconfig.marksman.setup(set_config())
+    lspconfig.terraformls.setup(set_config())
     -- lspconfig['terraform_lsp'].setup(set_config())
-    lspconfig['bashls'].setup(set_config())
-    lspconfig['cssls'].setup(set_config())
-    lspconfig['vuels'].setup(set_config({ on_attach = on_attach_server(false), }))
+    lspconfig.bashls.setup(set_config({ filetypes = { 'sh', 'zsh', }, }))
+    lspconfig.cssls.setup(set_config())
+    lspconfig.vuels.setup(set_config({ on_attach = on_attach_server(false), }))
+
 
     lspconfig.tsserver.setup(set_config(
       {

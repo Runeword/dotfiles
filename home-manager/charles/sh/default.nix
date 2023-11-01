5c8ea2887ca755e3c4a5b1b8907d80dca60fa782
Runeword <60324746+Runeword@users.noreply.github.com>
2023-11-01 19:54:40 +0100

    Update home-manager/charles/sh/default.nix

--- home-manager/charles/sh/default.nix
+++ home-manager/charles/sh/default.nix
@@ -10,8 +10,9 @@
   --reverse \
   --no-separator \
   --info=inline:'' \
-  --border none --color=fg:#d0d0d0,bg:-1,hl:#7e6fc4 \
-  --color=fg+:#ffffff,bg+:-1,hl+:#4fe8e3,hl+:regular,query:regular \
+  --border none \
+  --color=fg:#d0d0d0,bg:-1,hl:#8d84cf \
+  --color=fg+:#ffffff,fg+:regular,bg+:-1,hl+:#6bdbd8,hl+:regular,query:regular \
   --color=info:#d0d0d0,prompt:#ffffff,pointer:#ff75a9 \
   --color=marker:#ff75a9,spinner:#ffffff,header:#535e73 \
   --prompt='  ' \

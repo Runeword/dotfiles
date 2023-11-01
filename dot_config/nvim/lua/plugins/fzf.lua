16bb974ec71462aa9b0222f3db2dd00d6fe3dba3
Runeword <60324746+Runeword@users.noreply.github.com>
2023-11-01 19:54:37 +0100

    Update .config/nvim/lua/plugins/fzf.lua

--- dot_config/nvim/lua/plugins/fzf.lua
+++ dot_config/nvim/lua/plugins/fzf.lua
@@ -1,11 +1,35 @@
 local vim = vim
 
 return {
+  --   {
+  --     'junegunn/fzf',
+  --     config = function()
+  --       vim.cmd [[
+  --     autocmd! FileType fzf
+  --     autocmd FileType fzf tnoremap <buffer> <esc> <c-c>
+  --     autocmd  FileType fzf set laststatus=0 noshowmode noruler
+  --       \| autocmd BufLeave <buffer> set laststatus=2 showmode ruler
+  --     ]]
+  --
+  --       vim.keymap.set('n', '<Tab>',
+  --         function()
+  --           vim.fn['fzf#run'] {
+  --             sink = 'e',
+  --             window = { width = 0.9, height = 0.6, },
+  --             options = { '--preview=bat {}' }
+  --           }
+  --         end,
+  --         { silent = true, })
+  --     end,
+  --   },
+  -- }
+
   'ibhagwan/fzf-lua',
+  enabled = true,
 
   config = function()
-    vim.keymap.set('n', '<Leader><Leader>', require('fzf-lua').files)
-    vim.keymap.set('n', '<Leader><Space>', require('fzf-lua').live_grep_resume)
+    vim.keymap.set('n', '<Tab>',     require('fzf-lua').files)
+    vim.keymap.set('n', '<S-Tab>',   require('fzf-lua').live_grep_resume)
     vim.keymap.set('n', '<Leader>h', require('fzf-lua').help_tags)
     vim.keymap.set('n', '<Leader>k', require('fzf-lua').keymaps)
     vim.keymap.set('n', '<Leader>i', require('fzf-lua').highlights)

a695c86380f5185a30aa1a5fb13e4423bf890a96
Runeword <60324746+Runeword@users.noreply.github.com>
2023-11-05 21:36:42 +0100

    Update .config/nvim/lua/plugins/varioustextobjs.lua

--- dot_config/nvim/lua/plugins/varioustextobjs.lua
+++ dot_config/nvim/lua/plugins/varioustextobjs.lua
@@ -6,6 +6,8 @@ return {
   config = function()
     require('various-textobjs').setup({ useDefaultKeymaps = false, })
 
+    vim.keymap.set({ 'o', 'x', }, 'd',
+      function() require('various-textobjs').diagnostic() end)
     vim.keymap.set({ 'o', 'x', }, 'ak',
       function() require('various-textobjs').key(false) end)
     vim.keymap.set({ 'o', 'x', }, 'ik',

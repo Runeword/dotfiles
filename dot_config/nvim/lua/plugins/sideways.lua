f63620c965dbc9a4cd34e3a6e510bc197182b401
Runeword <60324746+Runeword@users.noreply.github.com>
2023-11-08 21:21:02 +0100

    Update .config/nvim/lua/plugins/sideways.lua

--- dot_config/nvim/lua/plugins/sideways.lua
+++ dot_config/nvim/lua/plugins/sideways.lua
@@ -5,11 +5,9 @@ return {
   enabled = true,
 
   config = function()
-    vim.keymap.set('n', '<S-Left>',  '<cmd>SidewaysLeft<CR>')
+    vim.keymap.set('n', '<Left>', '<cmd>SidewaysJumpLeft<CR>')
+    vim.keymap.set('n', '<Right>', '<cmd>SidewaysJumpRight<CR>')
+    vim.keymap.set('n', '<S-Left>', '<cmd>SidewaysLeft<CR>')
     vim.keymap.set('n', '<S-Right>', '<cmd>SidewaysRight<CR>')
-    -- vim.keymap.set('n', '<Left>', '<cmd>SidewaysJumpLeft<CR>')
-    -- vim.keymap.set('n', '<Right>', '<cmd>SidewaysJumpRight<CR>')
-    -- vim.keymap.set('n', '<S-Left>', '<cmd>SidewaysLeft<CR>')
-    -- vim.keymap.set('n', '<S-Right>', '<cmd>SidewaysRight<CR>')
   end,
 }

8012833fac4d5086d5af20a67af4b33b7d1940e1
Runeword <60324746+Runeword@users.noreply.github.com>
2023-11-08 21:21:00 +0100

    Update .config/nvim/lua/plugins/fzf.lua

--- dot_config/nvim/lua/plugins/fzf.lua
+++ dot_config/nvim/lua/plugins/fzf.lua
@@ -29,7 +29,7 @@ return {
 
   config = function()
     vim.keymap.set('n', '<Leader><CR>',     require('fzf-lua').files)
-    vim.keymap.set('n', '<S-CR>',   require('fzf-lua').live_grep_resume)
+    vim.keymap.set('n', '<Leader><S-CR>',   require('fzf-lua').live_grep_resume)
     vim.keymap.set('n', '<Leader>h', require('fzf-lua').help_tags)
     vim.keymap.set('n', '<Leader>k', require('fzf-lua').keymaps)
     vim.keymap.set('n', '<Leader>i', require('fzf-lua').highlights)

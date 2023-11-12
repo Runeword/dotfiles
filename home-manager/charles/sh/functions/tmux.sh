5daeda0523c5ee9c25a53d84942ff1a0972a2c32
Runeword <60324746+Runeword@users.noreply.github.com>
2023-11-12 22:38:36 +0100

    Update home-manager/charles/sh/functions/tmux.sh

--- home-manager/charles/sh/functions/tmux.sh
+++ home-manager/charles/sh/functions/tmux.sh
@@ -1,70 +1,73 @@
 __switch_session() {
-	if [ -z "$(tmux list-sessions 2>/dev/null)" ]; then
-		trap 'return' INT
-		printf 'new session name : ' && read -r input
+  if [ "$(tmux list-sessions 2>/dev/null)" = "" ]; then
+    trap 'return' INT
+    printf 'new session name : ' && read -r input
 
-		tmux new-session ${input:+-s"$input"}
+    tmux new-session "${input:+-s"$input"}"
 
-		return 1
-	fi
+    return 1
+  fi
 
-	local item_pos
-	local session_id
+  local session_id
+  session_id=$(tmux display-message -p '#{session_id}')
 
-	session_id=$(tmux display-message -p '#{session_id}')
-	item_pos=$(tmux list-sessions -F '#{session_id}' | awk '{if ($1 == "'$session_id'") print NR}')
+  local item_pos
+  item_pos=$(tmux list-sessions -F '#{session_id}' | awk '{if ($1 == "'"$session_id"'") print NR}')
 
-	session=$(
-		tmux ls -F "#{session_name}" 2>/dev/null | fzf \
-			--reverse \
-			--cycle \
-			--height 50% \
-			--delimiter=' ' \
-			--bind='tab:down,btab:up' \
+  local session
+  session=$(
+    tmux ls -F "#{session_name}" 2>/dev/null | fzf \
+      --reverse \
+      --cycle \
+      --height 50% \
+      --delimiter=' ' \
+      --bind='tab:down,btab:up' \
       --bind='enter:execute(echo {1})+abort' \
-			${TMUX:+--bind='focus:execute-silent(tmux switch-client -t {1})'} \
-			${TMUX:+--bind="load:pos($item_pos)"}
-	)
+      "${TMUX:+--bind='focus:execute-silent(tmux switch-client -t {1})'}" \
+      "${TMUX:+--bind="load:pos($item_pos)"}"
+  )
 
-	[ -z "$session" ] && return 1
+  [ "$session" = "" ] && return 1
 
-	if [ -n "$TMUX" ]; then
-		tmux switch-client -t "$session"
-	else
-		tmux attach-session -t "$session"
-	fi
+  if [ "$TMUX" != "" ]; then
+    tmux switch-client -t "$session"
+  else
+    tmux attach-session -t "$session"
+  fi
 }
 
 __switch_window() {
-	local item_pos
-	local window_id
-	local window
 
-	window_id=$(tmux display-message -p '#{window_id}')
-	item_pos=$(tmux list-windows -a -F '#{window_id}' | awk '{if ($1 == "'$window_id'") print NR}')
+  local window_id
+  window_id=$(tmux display-message -p '#{window_id}')
 
-	window=$(
-		tmux list-windows -a -F '#{session_name} #{window_name} #{window_id} #{session_id}' 2>/dev/null | fzf \
-		  --with-nth='1,2' \
-			--reverse \
-			--cycle \
-			--height 50% \
-			--delimiter=' ' \
-			--bind='tab:down,btab:up' \
-			${TMUX:+--bind='focus:execute-silent(tmux switch-client -t {4}; tmux select-window -t {3})'} \
-			${TMUX:+--bind="load:pos($item_pos)"}
-	)
+  local item_pos
+  item_pos=$(tmux list-windows -a -F '#{window_id}' | awk '{if ($1 == "'"$window_id"'") print NR}')
+
+  local window
+  window=$(
+    tmux list-windows -a -F '#{session_name} #{window_name} #{window_id} #{session_id}' 2>/dev/null | fzf \
+      --with-nth='1,2' \
+      --reverse \
+      --cycle \
+      --height 50% \
+      --delimiter=' ' \
+      --bind='tab:down,btab:up' \
+      "${TMUX:+--bind='focus:execute-silent(tmux switch-client -t {4}; tmux select-window -t {3})'}" \
+      "${TMUX:+--bind="load:pos($item_pos)"}"
+  )
 }
 
 __new_session() {
-	trap 'return' INT
-	printf 'new session : ' && read -r input
+  trap 'return' INT
+  printf 'new session : ' && read -r input
 
-	session=$(tmux new-session -d ${input:+-s"$input"} -P -F "#{session_name}" -c "$HOME")
+  local session
+  session=$(tmux new-session -d "${input:+-s"$input"}" -P -F "#{session_name}" -c "$HOME")
 
-	if [ -n "$TMUX" ]; then
-		tmux switch-client -t "$session"
-	else
-		tmux attach-session -t "$session"
-	fi
+  if [ "$TMUX" != "" ]; then
+    tmux switch-client -t "$session"
+  else
+    tmux attach-session -t "$session"
+  fi
 }

34242fbb93d5524c993fc5f14a8a8c88ea2a53e9
Runeword <60324746+Runeword@users.noreply.github.com>
2023-11-12 22:40:14 +0100

    Update home-manager/charles/sh/functions/chezmoi.sh

--- home-manager/charles/sh/functions/chezmoi.sh
+++ home-manager/charles/sh/functions/chezmoi.sh
@@ -1,63 +1,75 @@
 #!/bin/sh
 
 __select_files() {
-	echo "$1" | fzf \
-		--multi --inline-info --reverse --no-separator --border none --cycle --height 100% \
+  echo "$1" | fzf \
+    --multi --inline-info --reverse --no-separator --border none --cycle --height 100% \
     --header-first \
     --header="$2" \
-		--preview 'chezmoi diff --reverse --color=true ~/{}' \
-		--preview-window bottom,80%,noborder
+    --preview 'chezmoi diff --reverse --color=true ~/{}' \
+    --preview-window bottom,80%,noborder
 }
 
 cha() {
-	if [ $# -gt 0 ]; then
-		selected_files=$@
-	else
-		files=$(chezmoi status | awk '{print $2}')
-		[ -z "$files" ] && return 1
+  if [ $# -gt 0 ]; then
+    local selected_files
+    selected_files=$*
+  else
+    local files
+    files=$(chezmoi status | awk '{print $2}')
+    [ "$files" = "" ] && return 1
 
-		selected_files=$(__select_files "$files" "chezmoi add")
-		[ -z "$selected_files" ] && return 1
-	fi
+    selected_files=$(__select_files "$files" "chezmoi add")
+    [ "$selected_files" = "" ] && return 1
+  fi
+
+  # "$(echo "$selected_files" | xargs)" | while IFS= read -r i; do chezmoi add "$HOME/$i"; done
 
   for i in $(echo "$selected_files" | xargs); do
-		chezmoi add "$HOME/$i"
-	done
+    chezmoi add "$HOME/$i"
+  done
 }
 
 chy() {
-	if [ $# -gt 0 ]; then
-		selected_files=$@
-	else
-		files=$(chezmoi status | awk '{print $2}')
-		[ -z "$files" ] && return 1
+  if [ $# -gt 0 ]; then
+    local selected_files
+    selected_files=$*
+  else
+    local files
+    files=$(chezmoi status | awk '{print $2}')
+    [ "$files" = "" ] && return 1
 
-		selected_files=$(__select_files "$files", "chezmoi apply")
-		[ -z "$selected_files" ] && return 1
-	fi
+    selected_files=$(__select_files "$files", "chezmoi apply")
+    [ "$selected_files" = "" ] && return 1
+  fi
 
-	for i in $(echo "$selected_files" | xargs); do
-		chezmoi apply "$HOME/$i"
-	done
+  # "$(echo "$selected_files" | xargs)" | while IFS= read -r i; do chezmoi apply "$HOME/$i"; done
+
+  for i in $(echo "$selected_files" | xargs); do
+    chezmoi apply "$HOME/$i"
+  done
 }
 
 chf() {
-	if [ $# -gt 0 ]; then
-		selected_files=$@
-	else
-		files=$(chezmoi managed)
-		[ -z "$files" ] && return 1
+  if [ $# -gt 0 ]; then
+    local selected_files
+    selected_files=$*
+  else
+    local files
+    files=$(chezmoi managed)
+    [ "$files" = "" ] && return 1
 
-		selected_files=$(echo "$files" | fzf \
-			--multi --inline-info --reverse --no-separator --border none --cycle --height 70% \
+    selected_files=$(echo "$files" | fzf \
+      --multi --inline-info --reverse --no-separator --border none --cycle --height 70% \
       --header-first \
       --header="chezmoi forget" \
-			--preview '[ -f {} ] && bat --style=plain --color=always {}' \
-			--preview-window right,70%,noborder)
-		[ -z "$selected_files" ] && return 1
-	fi
+      --preview '[ -f {} ] && bat --style=plain --color=always {}' \
+      --preview-window right,70%,noborder)
+    [ "$selected_files" = "" ] && return 1
+  fi
+
+  # "$(echo "$selected_files" | xargs)" | while IFS= read -r i; do chezmoi forget "$HOME/$i"; done
 
   for i in $(echo "$selected_files" | xargs); do
-		chezmoi forget "$HOME/$i"
-	done
+    chezmoi forget "$HOME/$i"
+  done
 }

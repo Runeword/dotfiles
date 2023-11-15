d44dd2f917858fa2c05df9ef0136defee4d26654
Runeword <60324746+Runeword@users.noreply.github.com>
2023-11-15 21:15:55 +0100

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -16,24 +16,6 @@ return {
   },
 
   {
-    'gbprod/stay-in-place.nvim',
-    config = function()
-      local stayinplace = require('stay-in-place')
-      stayinplace.setup({
-        set_keymaps = false,
-        preserve_visual_selection = true,
-      })
-      vim.keymap.set('n', '>', stayinplace.shift_right_line,   { noremap = true, })
-      vim.keymap.set('n', '<', stayinplace.shift_left_line,    { noremap = true, })
-      vim.keymap.set('n', '=', stayinplace.filter_line,        { noremap = true, })
-
-      vim.keymap.set('x', '>',  stayinplace.shift_right_visual, { noremap = true, })
-      vim.keymap.set('x', '<',  stayinplace.shift_left_visual,  { noremap = true, })
-      vim.keymap.set('x', '=',  stayinplace.filter_visual,      { noremap = true, })
-    end,
-  },
-
-  {
     'norcalli/nvim-colorizer.lua',
     config = function()
       require('colorizer').setup()

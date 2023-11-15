052d384f6295716d2eccb3bcc2a72a45086ce2d1
Runeword <60324746+Runeword@users.noreply.github.com>
2023-11-15 21:06:53 +0100

    Update .config/nvim/lua/plugins.lua

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -18,7 +18,18 @@ return {
   {
     'gbprod/stay-in-place.nvim',
     config = function()
-      require('stay-in-place').setup()
+      local stayinplace = require('stay-in-place')
+      stayinplace.setup({
+        set_keymaps = false,
+        preserve_visual_selection = true,
+      })
+      vim.keymap.set('n', '>', stayinplace.shift_right_line,   { noremap = true, })
+      vim.keymap.set('n', '<', stayinplace.shift_left_line,    { noremap = true, })
+      vim.keymap.set('n', '=', stayinplace.filter_line,        { noremap = true, })
+
+      vim.keymap.set('x', '>',  stayinplace.shift_right_visual, { noremap = true, })
+      vim.keymap.set('x', '<',  stayinplace.shift_left_visual,  { noremap = true, })
+      vim.keymap.set('x', '=',  stayinplace.filter_visual,      { noremap = true, })
     end,
   },
 

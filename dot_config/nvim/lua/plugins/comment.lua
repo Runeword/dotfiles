91cc7713a131f21eb8934b0a2cc038df42411cde
Runeword <60324746+Runeword@users.noreply.github.com>
2023-11-20 21:34:10 +0100

    Update .config/nvim/lua/plugins/comment.lua

--- dot_config/nvim/lua/plugins/comment.lua
+++ dot_config/nvim/lua/plugins/comment.lua
@@ -4,9 +4,9 @@ return {
   'numToStr/Comment.nvim',
 
   config = function()
-    require('Comment').setup()
+    require('Comment').setup({ ignore = '^$', })
 
-    vim.keymap.set('x', 'gc', '<Plug>(comment_toggle_linewise_visual)gv', { remap = true, })
+    vim.keymap.set('x', 'gc', '<Plug>(comment_toggle_linewise_visual)gv',  { remap = true, })
     vim.keymap.set('x', 'gb', '<Plug>(comment_toggle_blockwise_visual)gv', { remap = true, })
   end,
 }

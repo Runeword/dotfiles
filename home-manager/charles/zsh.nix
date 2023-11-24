4d43a819d63de16742aa9ac4067c1e5f867e259f
Runeword <60324746+Runeword@users.noreply.github.com>
2023-11-24 20:15:16 +0100

    Add home-manager/charles/zsh.nix

---
+++ home-manager/charles/zsh.nix
@@ -0,0 +1,45 @@
+{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
+  programs.zsh.enable = true;
+  programs.zsh.enableAutosuggestions = true;
+  programs.zsh.enableCompletion = false;
+
+  programs.zsh.initExtra = builtins.readFile ./.zshrc;
+  # programs.zsh.initExtraBeforeCompInit = builtins.readFile ./.initExtraBeforeCompInit;
+
+  programs.zsh.plugins = [
+    # {
+    #   name = "zsh-autocomplete";
+    #   src = pkgs.fetchFromGitHub {
+    #     owner = "marlonrichert";
+    #     repo = "zsh-autocomplete";
+    #     rev = "23.07.13";
+    #     sha256 = "sha256-0NW0TI//qFpUA2Hdx6NaYdQIIUpRSd0Y4NhwBbdssCs=";
+    #     # sha256 = lib.fakeSha256;
+    #   };
+    # }
+    # {
+    #   name = "zsh-abbr";
+    #   src = pkgs.fetchFromGitHub {
+    #     owner = "olets";
+    #     repo = "zsh-abbr";
+    #     rev = "v5.1.0";
+    #     sha256 = "sha256-iKL2vn7TmQr78y0Bn02DgNf9DS5jZyh6uK9MzYTFZaA=";
+    #     # sha256 = lib.fakeSha256;
+    #   };
+    # }
+  ];
+
+  # programs.zsh.zplug = {
+  #   enable = false;
+  #   plugins = [
+  #     # {name = zdharma-continuum/fast-syntax-highlighting";}
+  #     {name = "marlonrichert/zsh-autocomplete";}
+  #     { name = "olets/zsh-abbr"; # tags = ["at:82fc2b53d7b1c2df60fdea6f13701cff9c536197"]; }
+  #   ];
+  # };
+}

64c409086f8c20db3fea311b74c392191e7477f1
Runeword <60324746+Runeword@users.noreply.github.com>
2023-11-24 20:16:04 +0100

    Update home-manager/charles/sessionVariables.nix

--- home-manager/charles/sessionVariables.nix
+++ home-manager/charles/sessionVariables.nix
@@ -4,24 +4,24 @@
   lib,
   ...
 }: {
-home.sessionVariables.EDITOR = "nvim";
-home.sessionVariables.VISUAL = "nvim";
-home.sessionVariables.MANPAGER = "nvim +Man!";
-home.sessionVariables.DIRENV_LOG_FORMAT = "$(tput setaf 0)direnv: %s$(tput sgr0)";
-# home.sessionVariables.GRIMBLAST_EDITOR = "swappy";
+  home.sessionVariables.EDITOR = "nvim";
+  home.sessionVariables.VISUAL = "nvim";
+  home.sessionVariables.MANPAGER = "nvim +Man!";
+  home.sessionVariables.DIRENV_LOG_FORMAT = "$(tput setaf 0)direnv: %s$(tput sgr0)";
+  # home.sessionVariables.GRIMBLAST_EDITOR = "swappy";
 
-home.sessionVariables.FORGIT_FZF_DEFAULT_OPTS = "
+  home.sessionVariables.FORGIT_FZF_DEFAULT_OPTS = "
 --preview-window border-none \
 ";
 
-home.sessionVariables._ZO_FZF_OPTS = "
+  home.sessionVariables._ZO_FZF_OPTS = "
 --reverse \
 --height 40% \
 --no-separator \
 --border none \
 ";
 
-home.sessionVariables.FZF_DEFAULT_COMMAND = "
+  home.sessionVariables.FZF_DEFAULT_COMMAND = "
 fd \
 --hidden \
 --follow \
@@ -30,7 +30,7 @@ fd \
 --exclude node_modules \
 ";
 
-home.sessionVariables.FZF_DEFAULT_OPTS = "
+  home.sessionVariables.FZF_DEFAULT_OPTS = "
 --reverse \
 --no-separator \
 --info=inline:'' \
@@ -41,6 +41,6 @@ home.sessionVariables.FZF_DEFAULT_OPTS = "
 --color=marker:#ff75a9,spinner:#ffffff,header:#535e73 \
 --prompt='  ' \
 ";
-# --color=fg+:#ffffff,fg+:regular,bg+:-1,hl+:#6bdbd8,hl+:regular,query:regular \
-# --color=fg+:#ffffff,fg+:regular,bg+:#262626,hl+:#6bdbd8,hl+:regular,query:regular \
+  # --color=fg+:#ffffff,fg+:regular,bg+:-1,hl+:#6bdbd8,hl+:regular,query:regular \
+  # --color=fg+:#ffffff,fg+:regular,bg+:#262626,hl+:#6bdbd8,hl+:regular,query:regular \
 }

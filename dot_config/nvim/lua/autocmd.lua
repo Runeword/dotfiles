59a964f689025ae9ec5f453425cea64d56e3d8c7
Runeword <60324746+Runeword@users.noreply.github.com>
2023-11-30 23:36:30 +0100

    Update .config/nvim/lua/autocmd.lua

--- dot_config/nvim/lua/autocmd.lua
+++ dot_config/nvim/lua/autocmd.lua
@@ -101,10 +101,15 @@ vim.api.nvim_create_autocmd('colorscheme', {
   callback = function()
     -- #0a172e #10141f #1a1a3b #1e2633 #424a57 #7a7c9e #222b66
 
-    vim.api.nvim_set_hl(0, 'diagnosticerror',          { bg = 'NONE', fg = '#ff75a9', bold = false, italic = true, })
-    vim.api.nvim_set_hl(0, 'diagnosticunderlineerror', { bg = 'NONE', undercurl = true, sp = '#ff75a9', })
+    vim.api.nvim_set_hl(0, 'DiagnosticError',          { bg = 'NONE', fg = '#ff75a9', bold = false, italic = true, })
+    vim.api.nvim_set_hl(0, 'DiagnosticUnderlineError', { bg = 'NONE', undercurl = true, sp = '#ff75a9', })
+    vim.api.nvim_set_hl(0, 'diagnosticWarn',          { bg = 'NONE', fg = '#ff9d57', bold = false, italic = true, })
+    vim.api.nvim_set_hl(0, 'diagnosticUnderlineWarn', { bg = 'NONE', undercurl = true, sp = '#ff9d57', })
+    vim.api.nvim_set_hl(0, 'diagnosticInfo',          { bg = 'NONE', fg = '#a6c8ff', bold = false, italic = true, })
+    vim.api.nvim_set_hl(0, 'diagnosticUnderlineInfo', { bg = 'NONE', undercurl = true, sp = '#a6c8ff', })
+    vim.api.nvim_set_hl(0, 'diagnosticHint',          { bg = 'NONE', fg = '#adb5bd', bold = false, italic = true, })
+    vim.api.nvim_set_hl(0, 'diagnosticUnderlineHint', { bg = 'NONE', undercurl = true, sp = '#adb5bd', })
 
-    vim.api.nvim_set_hl(0, 'diagnosticfloatingerror',  { link = 'normal', })
     -- vim.api.nvim_set_hl(0, 'diagnosticfloatingerror',  { link = 'diagnosticvirtualtexterror', })
     -- vim.api.nvim_set_hl(0, 'diagnosticfloatinghint',   { link = 'diagnosticvirtualtexthint', })
     -- vim.api.nvim_set_hl(0, 'diagnosticfloatinginfo',   { link = 'diagnosticvirtualtextinfo', })

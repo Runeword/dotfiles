6694e73cd540ce032d109cbda268fdc4fe4548b3
Runeword <60324746+Runeword@users.noreply.github.com>
2023-12-06 10:29:54 +0100

    Update home-manager/charles/shell/functions/wifi.sh

--- home-manager/charles/shell/functions/wifi.sh
+++ home-manager/charles/shell/functions/wifi.sh
@@ -1,27 +1,28 @@
-__wifi() {
+#!/bin/sh
+
+__nmcli() {
   local ssid
 
-  # nmcli device wifi rescan > /dev/null
-
   ssid=$(
-    nmcli device wifi | fzf \
+    nmcli device wifi 2>/dev/null | fzf \
       --header-lines=1 \
       --preview 'echo {2}' \
-      --preview-window right,45%,noborder \
+      --preview-window right,30%,noborder \
       --no-scrollbar \
       --header-first \
       --header='
-      C-u    up
+      C-ENT  connect     C-r    rescan
+      C-u    up          C-o    wifi on/off
       C-d    down
-      C-del  delete 
-      C-r    reload
+      C-DEL  delete 
       ' \
-      --bind='enter:execute-silent(nmcli --ask device wifi connect {2})' \
-      --bind='ctrl-u:execute-silent(nmcli connection up {2})+reload(nmcli device wifi)' \
+      --bind='enter:execute(echo {2})+abort' \
+      --bind='ctrl-c:execute-silent(nmcli connection up {2})+reload(nmcli device wifi)' \
       --bind='ctrl-d:execute-silent(nmcli connection down {3})+reload(nmcli device wifi)' \
       --bind='ctrl-delete:execute-silent(nmcli connection delete {2})+reload(nmcli device wifi)' \
-      --bind='ctrl-r:reload(nmcli device wifi)'
-  )
+      --bind="ctrl-o:execute-silent(if [ $(nmcli radio wifi) = enabled ]; then nmcli radio wifi off; else nmcli radio wifi on; fi)+reload-sync(nmcli device wifi)" \
+      --bind='ctrl-r:execute-silent(nmcli device wifi rescan)+reload-sync(nmcli device wifi)'
+  ) || return 0
 
-  # nmcli --ask device wifi connect "$ssid"
+  nmcli device wifi connect "$ssid"
 }

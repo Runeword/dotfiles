6f275f6544bd2fb579f63b674cd499d4f0f08b29
Runeword <60324746+Runeword@users.noreply.github.com>
2023-12-16 17:47:10 +0100

    Update .config/nvim/lua/plugins/surroundfunk.lua

--- dot_config/nvim/lua/plugins/surroundfunk.lua
+++ dot_config/nvim/lua/plugins/surroundfunk.lua
@@ -2,17 +2,13 @@ local vim = vim
 
 return {
   'Matt-A-Bennett/vim-surround-funk',
+  enabled = false,
   config = function()
     vim.g.surround_funk_create_mappings = 0
-    vim.cmd([[
-      xmap <silent> an <Plug>(SelectFunctionName)
-      omap <silent> an <Plug>(SelectFunctionName)
-      xmap <silent> aN <Plug>(SelectFunctionNAME)
-      omap <silent> aN <Plug>(SelectFunctionNAME)
-      xmap <silent> in <Plug>(SelectFunctionName)
-      omap <silent> in <Plug>(SelectFunctionName)
-      xmap <silent> iN <Plug>(SelectFunctionNAME)
-      omap <silent> iN <Plug>(SelectFunctionNAME)
-      ]])
+
+    vim.keymap.set({ 'x', 'o', }, 'an', '<Plug>(SelectFunctionName)', { silent = true, })
+    vim.keymap.set({ 'x', 'o', }, 'aN', '<Plug>(SelectFunctionNAME)', { silent = true, })
+    vim.keymap.set({ 'x', 'o', }, 'in', '<Plug>(SelectFunctionName)', { silent = true, })
+    vim.keymap.set({ 'x', 'o', }, 'iN', '<Plug>(SelectFunctionNAME)', { silent = true, })
   end,
 }

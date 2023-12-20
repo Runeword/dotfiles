3b462559249f31d851525397f4ca202a84b88ec9
Runeword <60324746+Runeword@users.noreply.github.com>
2023-12-20 23:55:54 +0100

    Update home-manager/flake.nix

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -7,9 +7,11 @@
   inputs.hyprland-contrib.url = "github:hyprwm/contrib";
   inputs.hyprland-contrib.inputs.nixpkgs.follows = "nixpkgs";
 
-  inputs.neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
-  inputs.neovim-nightly-overlay.inputs.nixpkgs.follows = "nixpkgs";
-  # inputs.neovim-nightly-overlay.inputs.nixpkgs.url = "github:nixos/nixpkgs?rev=fad51abd42ca17a60fc1d4cb9382e2d79ae31836";
+  # inputs.neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+  # inputs.neovim-nightly-overlay.inputs.nixpkgs.follows = "nixpkgs";
+  ## inputs.neovim-nightly-overlay.inputs.nixpkgs.url = "github:nixos/nixpkgs?rev=fad51abd42ca17a60fc1d4cb9382e2d79ae31836";
+
+  inputs.neovim-runeword.url = "github:Runeword/dotfiles?dir=neovim";
 
   # inputs.src-cli.url = "github:sourcegraph/src-cli?dir=contrib";
   # inputs.nixified-ai.url = "github:nixified-ai/flake";
@@ -21,9 +23,9 @@
       pkgs = import inputs.nixpkgs {
         inherit system;
         config.allowUnfree = true;
-        overlays = [
-          inputs.neovim-nightly-overlay.overlay
-        ];
+        # overlays = [
+        #   inputs.neovim-nightly-overlay.overlay
+        # ];
       };
 
       extraSpecialArgs = {inherit inputs;};

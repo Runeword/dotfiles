d96511581a843f0abdcb6026184004b10be293b3
Runeword <60324746+Runeword@users.noreply.github.com>
2023-12-23 23:45:10 +0100

    Update home-manager/charles/shell/scripts/fm_preview.sh

--- home-manager/charles/shell/scripts/executable_fm_preview.sh
+++ home-manager/charles/shell/scripts/executable_fm_preview.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
 if [ -d "$1" ]; then
-  tree -Ca -L 2 "$1" | sed 's/^/  /; 1s/^/\n/'
+  if command -v exa >/dev/null; then
+    exa -l "$1" --octal-permissions --color=always --total-size
+  else
+    ls -l "$1"
+  fi
+  tree -Ca -L 2 "$1" | sed 's/^/ /; 1s/^/\n/'
 else
+  if command -v exa >/dev/null; then
+    exa -l "$1" --octal-permissions --color=always
+  else
+    ls -l "$1"
+  fi
+
   if command -v bat >/dev/null; then
     bat --style=plain --color=always "$1" | sed 's/^/  /; 1s/^/\n/'
   else

0fc73b2d5c8da2e1380f53963c495cbdec23e6ce
Runeword <60324746+Runeword@users.noreply.github.com>
2023-12-25 20:09:49 +0100

    Update home-manager/charles/packages.nix

--- home-manager/charles/packages.nix
+++ home-manager/charles/packages.nix
@@ -35,6 +35,7 @@
 
   home.file."bin/tmux/resurrect".source = "${pkgs.tmuxPlugins.resurrect}/share/tmux-plugins/resurrect";
   home.file."bin/tmux/tmux-fzf".source = "${pkgs.tmuxPlugins.tmux-fzf}/share/tmux-plugins/tmux-fzf";
+  home.file."bin/fzf".source = "${pkgs.fzf}/share/fzf";
 
   home.packages = with pkgs; [
     (nerdfonts.override {fonts = ["Hack" "DroidSansMono" "SourceCodePro"];})

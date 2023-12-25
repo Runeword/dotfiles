1b260900da847d08aba65e31ca59bb1a8d30b1e2
Runeword <60324746+Runeword@users.noreply.github.com>
2023-12-25 22:12:30 +0100

    Update .config/nvim/lua/plugins/surroundfunk.lua

--- dot_config/nvim/lua/plugins/surroundfunk.lua
+++ dot_config/nvim/lua/plugins/surroundfunk.lua
@@ -2,10 +2,14 @@ local vim = vim
 
 return {
   'Matt-A-Bennett/vim-surround-funk',
-  enabled = false,
-  config = function()
-    vim.g.surround_funk_create_mappings = 0
 
+  enabled = true,
+
+  init = function()
+    vim.g.surround_funk_create_mappings = 0
+  end,
+
+  config = function()
     vim.keymap.set({ 'x', 'o', }, 'an', '<Plug>(SelectFunctionName)', { silent = true, })
     vim.keymap.set({ 'x', 'o', }, 'aN', '<Plug>(SelectFunctionNAME)', { silent = true, })
     vim.keymap.set({ 'x', 'o', }, 'in', '<Plug>(SelectFunctionName)', { silent = true, })

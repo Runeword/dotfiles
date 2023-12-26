7dc44ef5f86574e62ec58697df05d4eb389e182e
Runeword <60324746+Runeword@users.noreply.github.com>
2023-12-26 22:45:33 +0100

    Update home-manager/charles/shell/functions/fm.sh

--- home-manager/charles/shell/functions/fm.sh
+++ home-manager/charles/shell/functions/fm.sh
@@ -17,7 +17,7 @@ __open_file() {
         --height 70% \
         --ansi \
         --preview "$HOME/home-manager/$USER/shell/scripts/fm_preview.sh {}" \
-        --preview-window right,55%,border-none,~1 \
+        --preview-window right,55%,border-none,~3 \
         --bind='ctrl-y:execute-silent(wl-copy {})'
   ) || return 0
 

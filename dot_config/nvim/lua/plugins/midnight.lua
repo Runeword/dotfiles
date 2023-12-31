dc939c6d42a76c939998d597a8fd4fdfcc2903f0
Runeword <60324746+Runeword@users.noreply.github.com>
2023-12-31 20:03:41 +0100

    Update .config/nvim/lua/plugins/midnight.lua

--- dot_config/nvim/lua/plugins/midnight.lua
+++ dot_config/nvim/lua/plugins/midnight.lua
@@ -73,7 +73,8 @@ return {
       group = 'highlightedyank',
       pattern = '*',
       callback = function()
-        vim.api.nvim_set_hl(0, 'HighlightedyankRegion', { bg = '#222b66', })
+        vim.api.nvim_set_hl(0, 'HighlightedyankRegion', { link = 'Search', default = true, })
+        -- vim.api.nvim_set_hl(0, 'HighlightedyankRegion', { bg = '#222b66', })
         -- vim.api.nvim_set_hl(0, 'HighlightedyankRegion', { bg = '#00ffa2', fg = 'black', })
         -- vim.api.nvim_set_hl(0, 'HighlightedyankRegion', { bg = '#ff1994', fg = 'black', })
       end,

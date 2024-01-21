4c76d4c330c1b7d10b8f2abd5c4a0cbc1e93ad22
Runeword <60324746+Runeword@users.noreply.github.com>
2024-01-21 17:47:23 +0100

    Add home-manager/charles/overlays.nix

---
+++ home-manager/charles/overlays.nix
@@ -0,0 +1,24 @@
+{ pkgs
+, inputs
+, lib
+, ...
+}: {
+  nixpkgs.overlays = [
+    (self: super: {
+      # yazi = super.yazi.overrideAttrs (old: {
+      yazi = pkgs.rustPlatform.buildRustPackage {
+        name = "yazi";
+        src = super.fetchFromGitHub {
+          owner = "sxyazi";
+          repo = "yazi";
+          rev = "v0.2.1";
+          sha256 = "sha256-XdN2oP5c2lK+bR3i+Hwd4oOlccMQisbzgevHsZ8YbSQ=";
+          # sha256 = pkgs.lib.fakeSha256;
+        };
+        cargoSha256 = "sha256-SkqcMQGNVNvQ5oMrHS4QrQiFU8PfE0woLizCgN10v+o=";
+        # cargoSha256 = "";
+      };
+      # });
+    })
+  ];
+}

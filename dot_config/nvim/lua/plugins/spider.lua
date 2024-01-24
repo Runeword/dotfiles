e9fd7e2db87526b3da7133b1c38463f9bfe43fda
Runeword <60324746+Runeword@users.noreply.github.com>
2024-01-24 23:13:56 +0100

    Update .config/nvim/lua/plugins/spider.lua

--- dot_config/nvim/lua/plugins/spider.lua
+++ dot_config/nvim/lua/plugins/spider.lua
@@ -10,12 +10,12 @@ return {
       "<cmd>lua require('spider').motion('w')<CR>", { desc = 'Spider-w', })
     vim.keymap.set({ 'n', 'o', 'x', }, 'b',
       "<cmd>lua require('spider').motion('b')<CR>", { desc = 'Spider-b', })
-    vim.keymap.set({ 'x', 'o', }, 'e',
+    vim.keymap.set({ 'n', 'x', 'o', }, 'e',
       "<cmd>lua require('spider').motion('e')<CR>", { desc = 'Spider-e', })
-    vim.keymap.set({ 'x', 'o', }, 'ge',
+    vim.keymap.set({ 'n', 'x', 'o', }, 'ge',
       "<cmd>lua require('spider').motion('ge')<CR>", { desc = 'Spider-ge', })
 
-    vim.keymap.set({ 'n', }, 'e',  "<CMD>call search('\\>')<CR>",      { silent = true, noremap = true, })
-    vim.keymap.set({ 'n', }, 'ge', "<CMD>call search('\\>', 'b')<CR>", { silent = true, noremap = true, })
+    -- vim.keymap.set({ 'n', }, 'e',  "<CMD>call search('\\>')<CR>",      { silent = true, noremap = true, })
+    -- vim.keymap.set({ 'n', }, 'ge', "<CMD>call search('\\>', 'b')<CR>", { silent = true, noremap = true, })
   end,
 }

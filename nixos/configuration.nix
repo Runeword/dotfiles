edf045cd03dde20ca0247dad39ae8d5e3ed0a495
Runeword <60324746+Runeword@users.noreply.github.com>
2024-01-25 12:14:52 +0100

    Update nixos/configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -207,10 +207,11 @@
   services.pipewire.enable = true;
   services.pipewire.audio.enable = true;
   services.pipewire.wireplumber.enable = true;
-  # services.pipewire.lowLatency.enable = true;
   # services.pipewire.pulse.enable = true;
   # services.pipewire.alsa.enable = true;
   # services.pipewire.alsa.support32Bit = true;
+
+  # services.pipewire.lowLatency.enable = true;
   # services.pipewire.jack.enable = true;
 
   # user

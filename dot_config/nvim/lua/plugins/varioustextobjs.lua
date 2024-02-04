4042efb82ebb5a76f0c646947c9af5bc88a7b679
Runeword <60324746+Runeword@users.noreply.github.com>
2024-02-04 10:09:14 +0100

    Update .config/nvim/lua/plugins/varioustextobjs.lua

--- dot_config/nvim/lua/plugins/varioustextobjs.lua
+++ dot_config/nvim/lua/plugins/varioustextobjs.lua
@@ -6,9 +6,7 @@ return {
   config = function()
     require('various-textobjs').setup({ useDefaultKeymaps = false, })
 
-    vim.keymap.set({ 'o', 'x', }, 'id',
-      function() require('various-textobjs').diagnostic() end)
-    vim.keymap.set({ 'o', 'x', }, 'ad',
+    vim.keymap.set({ 'o', }, 'd',
       function() require('various-textobjs').diagnostic() end)
     vim.keymap.set({ 'o', 'x', }, 'ak',
       function() require('various-textobjs').key('outer') end)
@@ -22,21 +20,15 @@ return {
       function() require('various-textobjs').subword('outer') end)
     vim.keymap.set({ 'o', 'x', }, 'iw',
       function() require('various-textobjs').subword('inner') end)
-    -- vim.keymap.set({ 'o', 'x', }, '<BS>',
-    --   function() require('various-textobjs').lastChange() end)
+    vim.keymap.set({ 'o', 'x', }, 'ad',
+      function() require('various-textobjs').number('outer') end)
+    vim.keymap.set({ 'o', 'x', }, 'id',
+      function() require('various-textobjs').number('inner') end)
 
-    -- vim.keymap.set({ 'o', }, 'k', 'ik', { remap = true, })
     vim.keymap.set({ 'o', }, 'v', 'iv', { remap = true, })
-
-    vim.keymap.set({ 'o', }, 'w', 'iw', { remap = true, })
-    vim.keymap.set({ 'o', }, 'W', 'iW', { remap = true, })
-
+    -- vim.keymap.set({ 'o', }, 'k', 'ik', { remap = true, })
 
     -- vim.keymap.set({ 'o', 'x', }, 'aw',
     --   function() require('various-textobjs').subword(false) end)
-    -- vim.keymap.set({ 'o', 'x', }, 'ad',
-    --   function() require('various-textobjs').number(false) end)
-    -- vim.keymap.set({ 'o', 'x', }, 'id',
-    --   function() require('various-textobjs').number(true) end)
   end,
 }

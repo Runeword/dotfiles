752790b738cdd8435af2cc643f55e517b898100f
Runeword <60324746+Runeword@users.noreply.github.com>
2024-02-09 19:45:15 +0100

    Update .config/nvim/lua/plugins/dapgo.lua

--- dot_config/nvim/lua/plugins/dapgo.lua
+++ dot_config/nvim/lua/plugins/dapgo.lua
@@ -2,19 +2,20 @@ local vim = vim
 
 return {
   'leoluz/nvim-dap-go',
+  enabled = true,
 
   dependencies = { 'mfussenegger/nvim-dap', },
 
   config = function()
     require('dap-go').setup()
 
-    require('dap').configurations.go = {
-      {
-        type = 'go',
-        request = 'launch',
-        name = 'Launch file',
-        program = '${file}',
-      },
-    }
+    -- require('dap').configurations.go = {
+    --   {
+    --     type = 'go',
+    --     request = 'launch',
+    --     name = 'Launch file',
+    --     program = '${file}',
+    --   },
+    -- }
   end,
 }

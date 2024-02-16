2830c52814b6f378d112e7565ac37c49d815ba19
Runeword <60324746+Runeword@users.noreply.github.com>
2024-02-16 21:58:55 +0100

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -3,6 +3,7 @@ local vim = vim
 require('autocmd')
 require('options')
 require('mappings')
+require('filetype')
 
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 if not vim.loop.fs_stat(lazypath) then

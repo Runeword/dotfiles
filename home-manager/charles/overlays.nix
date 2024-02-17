086fbc8b1d57c85a0b937acc60ca83187b5d1c00
Runeword <60324746+Runeword@users.noreply.github.com>
2024-02-17 23:32:13 +0100

    Update home-manager/charles/overlays.nix

--- home-manager/charles/overlays.nix
+++ home-manager/charles/overlays.nix
@@ -4,22 +4,23 @@
 , ...
 }: {
   nixpkgs.overlays = [
-    (self: super: {
-      # yazi = super.yazi.overrideAttrs (old: {
-      yazi = pkgs.rustPlatform.buildRustPackage {
-        name = "yazi";
-        src = super.fetchFromGitHub {
-          owner = "sxyazi";
-          repo = "yazi";
-          rev = "v0.2.1";
-          sha256 = "sha256-XdN2oP5c2lK+bR3i+Hwd4oOlccMQisbzgevHsZ8YbSQ=";
-          # sha256 = pkgs.lib.fakeSha256;
-        };
-        cargoSha256 = "sha256-SkqcMQGNVNvQ5oMrHS4QrQiFU8PfE0woLizCgN10v+o=";
-        # cargoSha256 = "";
-      };
-      # });
-    })
+    # (self: super: {
+    #   # yazi = super.yazi.overrideAttrs (old: {
+    #   yazi = pkgs.rustPlatform.buildRustPackage {
+    #     name = "yazi";
+    #     src = super.fetchFromGitHub {
+    #       owner = "sxyazi";
+    #       repo = "yazi";
+    #       rev = "v0.2.1";
+    #       sha256 = "sha256-XdN2oP5c2lK+bR3i+Hwd4oOlccMQisbzgevHsZ8YbSQ=";
+    #       # sha256 = pkgs.lib.fakeSha256;
+    #     };
+    #     cargoSha256 = "sha256-SkqcMQGNVNvQ5oMrHS4QrQiFU8PfE0woLizCgN10v+o=";
+    #     # cargoSha256 = "";
+    #   };
+    #   # });
+    # })
+
     # (self: super: {
     #   appimage-run = super.appimage-run.overrideAttrs (old: {
     #     extraPkgs = [ pkgs.libsecret ];

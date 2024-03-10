ca8961fbb928732eedfe6cd3cbd3d1ddcb3bfbeb
Runeword <60324746+Runeword@users.noreply.github.com>
2024-03-10 23:57:45 +0100

    Update .config/nvim/lua/plugins/neotest.lua

--- dot_config/nvim/lua/plugins/neotest.lua
+++ dot_config/nvim/lua/plugins/neotest.lua
@@ -1,7 +1,10 @@
+local vim = vim
+
 return {
   'nvim-neotest/neotest',
   dependencies = {
     'nvim-lua/plenary.nvim',
+    'anuvyklack/hydra.nvim',
     'nvim-treesitter/nvim-treesitter',
     'nvim-neotest/neotest-go',
   },
@@ -11,5 +14,24 @@ return {
         require('neotest-go')({}),
       },
     })
+
+    -- local mappings = require('hydra')({
+    --   mode = { 'o', 'n', 'x', },
+
+    --   config = {
+    --     hint = { type = 'window', },
+    --     color = 'pink',
+    --     foreign_keys = 'run',
+    --     exit = false,
+    --   },
+
+    --   heads = {
+    --     -- { 'o',     require('neotest').summary.toggle(), },
+    --     { 'q',     nil, { exit = true, }, },
+    --     { '<Esc>', nil, { exit = true, }, },
+    --   },
+    -- })
+
+    -- vim.keymap.set({ 'n', 'x', }, '<Leader>t', function() mappings:activate() end)
   end,
 }

d6e833f57e1443e5861cb8e549a998f607a5fae2
Runeword <60324746+Runeword@users.noreply.github.com>
2024-03-10 23:26:15 +0100

    Add .config/nvim/lua/plugins/neotest.lua

---
+++ dot_config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,15 @@
+return {
+  'nvim-neotest/neotest',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-treesitter/nvim-treesitter',
+    'nvim-neotest/neotest-go',
+  },
+  config = function()
+    require('neotest').setup({
+      adapters = {
+        require('neotest-go')({}),
+      },
+    })
+  end,
+}

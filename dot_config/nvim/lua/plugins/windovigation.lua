0ff182245a0613bf9fe0a87ca867a994730411f1
Runeword <60324746+Runeword@users.noreply.github.com>
2024-03-30 18:02:56 +0100

    Update .config/nvim/lua/plugins/windovigation.lua

--- dot_config/nvim/lua/plugins/windovigation.lua
+++ dot_config/nvim/lua/plugins/windovigation.lua
@@ -7,9 +7,9 @@ return {
   config = function()
     require('windovigation').setup()
 
-    vim.keymap.set('n', '<C-P>', function() require('windovigation.actions').move_to_previous_file() end,
+    vim.keymap.set('n', '<C-S-CR>', function() require('windovigation.actions').move_to_previous_file() end,
       { silent = true, noremap = true, })
-    vim.keymap.set('n', '<C-N>', function() require('windovigation.actions').move_to_next_file() end,
+    vim.keymap.set('n', '<C-CR>', function() require('windovigation.actions').move_to_next_file() end,
       { silent = true, noremap = true, })
   end,
 }

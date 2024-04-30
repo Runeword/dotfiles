e5a46e3f80b117d15c54c28ab6ce65e71548ef91
Runeword <60324746+Runeword@users.noreply.github.com>
2024-04-30 21:08:54 +0200

    Update home-manager/charles/sessionVariables.nix

--- home-manager/charles/sessionVariables.nix
+++ home-manager/charles/sessionVariables.nix
@@ -12,7 +12,8 @@
     XDG_CACHE_HOME = "${config.home.homeDirectory}/.cache";
     XDG_CONFIG_DIRS = "/etc/xdg";
     XDG_CONFIG_HOME = "${config.home.homeDirectory}/.config";
-    XDG_DATA_DIRS = "/usr/local/share/:/usr/share/";
+    # XDG_DATA_DIRS = "/usr/local/share/:/usr/share/";
+    XDG_DATA_DIRS = "/usr/share";
     XDG_DATA_HOME = "${config.home.homeDirectory}/.local/share";
     XDG_STATE_HOME = "${config.home.homeDirectory}/.local/state";
 

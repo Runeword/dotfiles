cdf411796f7f2f3d6d90aae00520a7ea67a6d717
Runeword <60324746+Runeword@users.noreply.github.com>
2024-06-13 22:13:46 +0200

    Update .config/yazi/plugins/fzf.yazi/init.lua

--- dot_config/yazi/plugins/fzf.yazi/init.lua
+++ dot_config/yazi/plugins/fzf.yazi/init.lua
@@ -19,14 +19,6 @@ local function entry(_, args)
 	local shell_value = os.getenv("SHELL"):match(".*/(.*)")
 	local cmd_args = ""
 
-	local preview_cmd = [===[line={2} && begin=$( if [[ $line -lt 7 ]]; then echo $((line-1)); else echo 6; fi ) && bat --highlight-line={2} --color=always --line-range $((line-begin)):$((line+10)) {1}]===]
-	if shell_value == "fish" then
-		preview_cmd = [[set line {2} && set begin ( test $line -lt 7  &&  echo (math "$line-1") || echo  6 ) && bat --highlight-line={2} --color=always --line-range (math "$line-$begin"):(math "$line+10") {1}]]
-	elseif shell_value == "nu" then
-		preview_cmd = [[let line = ({2} | into int); let begin = if $line < 7 { $line - 1 } else { 6 }; bat --highlight-line={2} --color=always --line-range $'($line - $begin):($line + 10)' {1}]]
-	end
-
-	if args[1] == "fzf" then
 		-- cmd_args = [[fzf --preview='bat --color=always {1}']]
     cmd_args = [[find -L . \
       \( -name '.git' \
@@ -62,40 +54,6 @@ local function entry(_, args)
         --preview "$HOME/home-manager/$USER/shell/scripts/fm_preview.sh {}" \
         --preview-window right,55%,border-none,~3 \
         --bind='ctrl-y:execute-silent(wl-copy {})']]
-	elseif args[1] == "rg" and shell_value == "fish" then
-		cmd_args = [[
-			RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case " \
-			fzf --ansi --disabled \
-				--bind "start:reload:$RG_PREFIX {q}" \
-				--bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true" \
-				--delimiter : \
-				--preview ']] .. preview_cmd .. [[' \
-				--preview-window 'up,60%' \
-				--nth '3..'
-		]]
-	elseif args[1] == "rg" and (shell_value == "bash" or shell_value == "zsh")  then
-		cmd_args = [[
-			RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case "
-			fzf --ansi --disabled \
-				--bind "start:reload:$RG_PREFIX {q}" \
-				--bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true" \
-				--delimiter : \
-				--preview ']] .. preview_cmd .. [[' \
-				--preview-window 'up,60%' \
-				--nth '3..'
-		]]
-	elseif args[1] == "rg" and shell_value == "nu" then
-		local rg_prefix = "rg --column --line-number --no-heading --color=always --smart-case "
-		cmd_args = [[fzf --ansi --disabled --bind "start:reload:]]
-			.. rg_prefix
-			.. [[{q}" --bind "change:reload:sleep 100ms; try { ]]
-			.. rg_prefix
-			.. [[{q} }" --delimiter : --preview ']]
-			.. preview_cmd
-			.. [[' --preview-window 'up,60%' --nth '3..']]
-	else
-		cmd_args = [[rg --color=always --line-number --no-heading --smart-case '' | fzf --ansi --preview=']] .. preview_cmd .. [[' --delimiter=':' --preview-window='up:60%' --nth='3..']]
-	end
 
 	local child, err =
 		Command(shell_value):args({"-c", cmd_args}):cwd(cwd):stdin(Command.INHERIT):stdout(Command.PIPED):stderr(Command.INHERIT):spawn()

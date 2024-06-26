1760897141f00dd94059a990534bdda8387add06
Runeword <60324746+Runeword@users.noreply.github.com>
2024-06-27 00:45:09 +0200

    Update .config/nvim/lua/plugins/fzflua.lua

--- dot_config/nvim/lua/plugins/fzflua.lua
+++ dot_config/nvim/lua/plugins/fzflua.lua
@@ -1,29 +1,6 @@
 local vim = vim
 
 return {
-  --   {
-  --     'junegunn/fzf',
-  --     config = function()
-  --       vim.cmd [[
-  --     autocmd! FileType fzf
-  --     autocmd FileType fzf tnoremap <buffer> <esc> <c-c>
-  --     autocmd  FileType fzf set laststatus=0 noshowmode noruler
-  --       \| autocmd BufLeave <buffer> set laststatus=2 showmode ruler
-  --     ]]
-  --
-  --       vim.keymap.set('n', '<Tab>',
-  --         function()
-  --           vim.fn['fzf#run'] {
-  --             sink = 'e',
-  --             window = { width = 0.9, height = 0.6, },
-  --             options = { '--preview=bat {}' }
-  --           }
-  --         end,
-  --         { silent = true, })
-  --     end,
-  --   },
-  -- }
-
   'ibhagwan/fzf-lua',
   enabled = true,
 

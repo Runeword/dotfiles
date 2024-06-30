1576a2344af839c86bb4f7e03b9599d1bb4f3dca
Runeword <60324746+Runeword@users.noreply.github.com>
2024-06-30 21:06:33 +0200

    Update .config/nvim/lua/mappings.lua

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -164,6 +164,44 @@ vim.keymap.set({ 'n', 'x', }, '<C-Right>', '<C-w>l')
 --   end)
 
 
+
+-- Function to select the node under the cursor
+local function select_node_under_cursor()
+  -- Get the current buffer and cursor position
+  local bufnr = vim.api.nvim_get_current_buf()
+  local cursor = vim.api.nvim_win_get_cursor(0)
+  local row, col = cursor[1] - 1, cursor[2]
+
+  -- Get the parser and tree for the current buffer
+  local parser = vim.treesitter.get_parser(bufnr)
+  local tree = parser:parse()[1]
+
+  -- Get the node at the cursor position
+  local root = tree:root()
+  local node = root:named_descendant_for_range(row, col, row, col)
+
+  if not node then return end
+
+  -- Get the node's range
+  local start_row, start_col, end_row, end_col = node:range()
+
+  -- Set marks for the start and end of the node
+  vim.api.nvim_buf_set_mark(bufnr, '<', start_row + 1, start_col, {})
+  vim.api.nvim_buf_set_mark(bufnr, '>', end_row + 1, end_col, {})
+
+  -- Visually select the node
+  vim.cmd('normal! gv')
+end
+
+-- Set up the keymaps using Lua
+vim.keymap.set('x', '.', function()
+  select_node_under_cursor()
+end, {noremap = true, silent = true, desc = "Select inner treesitter object"})
+
+vim.keymap.set('o', '.', function()
+  select_node_under_cursor()
+end, {noremap = true, silent = true, desc = "Select inner treesitter object"})
+
 -- ----------------------------------- treesitter text object hook
 
 -- local ts = vim.treesitter

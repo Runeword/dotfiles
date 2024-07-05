6dc89f7e6d98823bb4cfd374efa3c341105872ef
Runeword <60324746+Runeword@users.noreply.github.com>
2024-07-05 22:08:54 +0200

    Update .config/nvim/lua/myplugins/grasp.nvim/lua/grasp/init.lua

--- dot_config/nvim/lua/myplugins/grasp.nvim/lua/grasp/init.lua
+++ dot_config/nvim/lua/myplugins/grasp.nvim/lua/grasp/init.lua
@@ -31,4 +31,67 @@ function M.select_node_under_cursor()
   api.nvim_command('normal! gv')
 end
 
+function M.move_to_next_treesitter_node()
+  local current_buffer_id = api.nvim_get_current_buf()
+  local current_buffer_filetype = api.nvim_get_option_value('filetype', { buf = current_buffer_id, })
+
+  local ts_lang = ts.language.get_lang(current_buffer_filetype)
+  if ts_lang == nil then
+    return
+  end
+
+  local ts_parser = ts.get_parser(current_buffer_id, ts_lang)
+  local ts_tree = ts_parser:parse()[1]
+  local ts_query = ts.query.parse(ts_lang, '(_) @node')
+
+  local cursor_pos = api.nvim_win_get_cursor(0)
+  local cursor_line = cursor_pos[1]
+  local cursor_col = cursor_pos[2]
+
+  for _, ts_node in ts_query:iter_captures(ts_tree:root(), current_buffer_id, cursor_line - 1) do
+    local ts_range = { ts_node:range(), }
+    local row_start = ts_range[1] + 1
+    local col_start = ts_range[2]
+
+    if row_start > cursor_line or (row_start == cursor_line and col_start > cursor_col) then
+      api.nvim_win_set_cursor(0, { row_start, col_start, })
+      break
+    end
+  end
+end
+
+function M.move_to_prev_treesitter_node()
+  local current_buffer_id = api.nvim_get_current_buf()
+  local current_buffer_filetype = api.nvim_get_option_value('filetype', { buf = current_buffer_id, })
+
+  local ts_lang = ts.language.get_lang(current_buffer_filetype)
+  if ts_lang == nil then
+    return
+  end
+
+  local ts_parser = ts.get_parser(current_buffer_id, ts_lang)
+  local ts_tree = ts_parser:parse()[1]
+  local ts_query = ts.query.parse(ts_lang, '(_) @node')
+
+  local cursor_pos = api.nvim_win_get_cursor(0)
+  local cursor_line = cursor_pos[1]
+  local cursor_col = cursor_pos[2]
+
+  local ts_nodes = {}
+  for _, ts_node in ts_query:iter_captures(ts_tree:root(), current_buffer_id, 0, cursor_line) do
+    table.insert(ts_nodes, 1, ts_node)
+  end
+
+  for i = 1, #ts_nodes do
+    local ts_range = { ts_nodes[i]:range(), }
+    local row_start = ts_range[1] + 1
+    local col_start = ts_range[2]
+
+    if row_start < cursor_line or (row_start == cursor_line and col_start < cursor_col) then
+      api.nvim_win_set_cursor(0, { row_start, col_start, })
+      break
+    end
+  end
+end
+
 return M

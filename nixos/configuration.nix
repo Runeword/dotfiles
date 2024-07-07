6d2e20ee33cd6831ffd42f491970baf485073c76
Runeword <60324746+Runeword@users.noreply.github.com>
2024-07-07 22:16:32 +0200

    Update nixos/configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -137,6 +137,7 @@
   # network
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
+  networking.wireless.iwd.enable = true;
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";

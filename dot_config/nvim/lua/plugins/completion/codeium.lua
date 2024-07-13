86146bc40e14c585e0976f2a720e03de108ddd3b
Runeword <60324746+Runeword@users.noreply.github.com>
2024-07-13 22:40:14 +0200

    Add .config/nvim/lua/plugins/completion/codeium.lua

---
+++ dot_config/nvim/lua/plugins/completion/codeium.lua
@@ -0,0 +1,24 @@
+local vim = vim
+
+return {
+  'Exafunction/codeium.vim',
+  enabled = true,
+  event = 'BufEnter',
+
+  init = function()
+    vim.api.nvim_create_augroup('codeium', { clear = true, })
+    vim.api.nvim_create_autocmd('ColorScheme', {
+      group = 'codeium',
+      pattern = '*',
+      callback = function()
+        vim.api.nvim_set_hl(0, 'CodeiumAnnotation', { bg = 'none', fg = '#5a7a99', italic = true, })
+        vim.api.nvim_set_hl(0, 'CodeiumSuggestion', { bg = 'none', fg = '#5a7a99', italic = true, })
+      end,
+    })
+  end,
+
+  config = function()
+    vim.g.codeium_disable_bindings = 1
+    vim.keymap.set('i', '<C-CR>', function() return vim.fn['codeium#Accept']() end, { expr = true, })
+  end,
+}

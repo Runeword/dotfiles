205bbb587ac9884b12f2245541febc3b99864a83
Runeword <60324746+Runeword@users.noreply.github.com>
2024-07-21 23:56:24 +0200

    Add .config/nvim/lua/plugins/init.lua

---
+++ dot_config/nvim/lua/plugins/init.lua
@@ -0,0 +1,63 @@
+local vim = vim
+
+return {
+  { 'nvim-lua/plenary.nvim', },
+  { 'nvim-tree/nvim-web-devicons', },
+  { 'tpope/vim-abolish', },
+  { 'svban/YankAssassin.vim', },
+
+  {
+    'nacro90/numb.nvim',
+    config = function()
+      require('numb').setup()
+    end,
+  },
+
+  { 'windwp/nvim-ts-autotag', },
+  { 'tommcdo/vim-exchange', },
+
+  { 'jghauser/mkdir.nvim', },
+
+  {
+    'ahmedkhalf/project.nvim',
+    config = function()
+      require('project_nvim').setup()
+    end,
+  },
+
+  {
+    'willothy/flatten.nvim',
+    enabled = true,
+    config = true,
+    lazy = false,
+    priority = 1001,
+  },
+
+  {
+    'kana/vim-textobj-line',
+    dependencies = 'kana/vim-textobj-user',
+  },
+
+  -- { 'HiPhish/rainbow-delimiters.nvim', },
+  -- { 'mbbill/undotree', },
+  -- { 'kana/vim-arpeggio', },
+
+  -- {
+  --   'paulhybryant/vim-textobj-path',
+  --   dependencies = 'kana/vim-textobj-user',
+  -- },
+
+  -- { 'smjonas/inc-rename.nvim',
+  --   config = function()
+  --     require('inc_rename').setup()
+  --   end,
+  -- },
+
+  -- { 'mattn/vim-gist',
+  --   dependencies = { 'mattn/webapi-vim' },
+  -- },
+}
+
+-- for key, value in pairs(plugins) do
+--   plugins[key].enabled = false
+-- end

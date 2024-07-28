2003963ff775e9d4e2a4f7e4b7e9b79073a2acd5
Runeword <60324746+Runeword@users.noreply.github.com>
2024-07-28 22:49:48 +0200

    Update .config/nvim/lua/plugins/textobjects/vimtextobjuser.lua

--- dot_config/nvim/lua/plugins/textobjects/vimtextobjuser.lua
+++ dot_config/nvim/lua/plugins/textobjects/vimtextobjuser.lua
@@ -28,8 +28,8 @@ return {
     dependencies = 'kana/vim-textobj-user',
 
     config = function()
-      vim.keymap.set({ 'o', }, 'c', 'ac', { remap = true, })
-      vim.keymap.set({ 'o', }, 'C', 'aC', { remap = true, })
+      -- vim.keymap.set({ 'o', }, 'c', 'ac', { remap = true, })
+      -- vim.keymap.set({ 'o', }, 'C', 'aC', { remap = true, })
     end,
   },
 }

b0199b4c14bf0346cb54aba977b5e342b856c6f0
Runeword <60324746+Runeword@users.noreply.github.com>
2024-08-14 23:50:55 +0200

    Update .config/nvim/lua/plugins/ui/notify.lua

--- dot_config/nvim/lua/plugins/ui/notify.lua
+++ dot_config/nvim/lua/plugins/ui/notify.lua
@@ -1,7 +1,19 @@
+local vim = vim
+
 return {
   'rcarriga/nvim-notify',
 
+  init = function()
+    -- vim.api.nvim_set_hl(0, "NotifyINFOTitle", { fg = "#ffffff" })
+  end,
+
   config = function()
-    require('notify').setup()
+    require('notify').setup({
+      top_down = false,
+      background_colour = '#000000',
+      stages = "static",
+    })
+
+    vim.notify = require('notify')
   end,
 }

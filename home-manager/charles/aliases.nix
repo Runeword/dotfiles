6df209ca31fe7a26001078015fdcdad7a40160d0
Runeword <60324746+Runeword@users.noreply.github.com>
2024-08-20 01:17:54 +0200

    Update home-manager/charles/aliases.nix

--- home-manager/charles/aliases.nix
+++ home-manager/charles/aliases.nix
@@ -18,35 +18,6 @@
     # ll = "exa --long --all --octal-permissions --group-directories-first --total-size --sort=time";
     # bb = "br -c ':toggle_hidden;:toggle_perm;:toggle_dates'";
 
-    # ______________________________________ UTILITY
-
-    play = "asciinema play";
-    "rec" = "asciinema rec $HOME/Downloads/$(date +'%Y-%m-%d_%H-%M-%S').cast";
-    keys = "showkey -a";
-    color = "hyprpicker --autocopy --format=hex";
-    bios = "sudo dmidecode -s bios-version";
-    window = "xprop WM_CLASS";
-    progress = "watch progress -q";
-    aliases = "__run_alias";
-    hardware = "hwinfo --short";
-    system = "neofetch";
-    wallpaper = "__wallpaper";
-    fonts = "fc-list : family style | fzf --reverse --prompt='  ' --info=inline:'' --no-separator --height 70%";
-    path = "echo \"\${PATH//:/\\n}\"";
-    devices = "sudo libinput list-devices";
-    monitors = "hyprctl monitors";
-    clients = "hyprctl clients";
-    keyboard = "pgrep -x evtest > /dev/null && sudo pkill evtest || sudo setsid evtest --grab /dev/input/event1 > /dev/null 2>&1";
-    pk = "sudo pkill";
-    # pg = "pgrep -x";
-    btm = "command btm --tree --left_legend";
-    procs = "command procs --tree";
-    disk = "duf";
-    # disk = "lsblk";
-    # diskinfo = "sudo nvme smart-log /dev/nvme0n1";
-    audit = "lynis audit system";
-    # fcount = ''find . -type d -exec sh -c 'echo -n "$1, "; find "$1" -maxdepth 1 -type f | wc -l' _ {} \; | awk -F, '$2 > 500''';
-
     # ______________________________________ PROGRAM
 
     gparted = "sudo -E gparted";

15b618f40f578c352948b60798c5c8444d48ec7d
Runeword <60324746+Runeword@users.noreply.github.com>
2024-08-24 23:17:37 +0200

    Update .config/shell/variables.sh

--- dot_config/shell/executable_variables.sh
+++ dot_config/shell/executable_variables.sh
@@ -15,6 +15,7 @@ export NOTMUCH_CONFIG="${XDG_CONFIG_HOME}/notmuch/config"
 export GNUPGHOME="${XDG_CONFIG_HOME}/gnupg"
 export PYENV_ROOT="${XDG_DATA_HOME}/pyenv"
 export PASSWORD_STORE_DIR="${XDG_DATA_HOME}/password-store"
+export ATAC_KEY_BINDINGS="${XDG_CONFIG_HOME}/atac/vim_key_bindings.toml"
 # ZDOTDIR = "${XDG_CONFIG_HOME}/zsh";
 # NPM_CONFIG_PREFIX = "${XDG_DATA_HOME}/npm";
 # TLDR_CACHE_DIR = "${XDG_CACHE_HOME}";

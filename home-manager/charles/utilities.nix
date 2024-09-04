156c2881045870f50257e8850c2ba1c380ef3af7
Runeword <60324746+Runeword@users.noreply.github.com>
2024-09-04 22:08:56 +0200

    Update home-manager/charles/utilities.nix

--- home-manager/charles/utilities.nix
+++ home-manager/charles/utilities.nix
@@ -8,36 +8,40 @@
   in
   {
   home.packages = with pkgs; [
-    # fuzzel
-    networkmanagerapplet
-    # # pw-viz
-    # bluetuith
-    # brillo
-    # playerctl
-    # impala # wifi
-    # wl-screenrec
-    # xdragon
-    # showmethekey
-
-    hyprpicker   # Color picker
-    wev          # Event viewer
-    imv          # Image viewer
+    # fuzzel # App launcher
+    # pw-viz # Pipewire graph editor
 
     # Essentials
-    (waybar.overrideAttrs (oldAttrs: { mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ]; }))
-    wl-clipboard # Clipboard
-    hyprlock     # Screen locker
-    fnott        # Notification daemon
-    nwg-displays # Monitor manager
+    wl-clipboard         # Clipboard
+    xdragon              # Drag and drop
+
+    # Terminal Tools
+    hyprpicker           # Color picker
+    wev                  # Event viewer
+    imv                  # Image viewer
+    wl-screenrec         # Screen recorder
+
+    # Interface
+    (waybar.overrideAttrs (oldAttrs: { mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ]; })) # Bar
+    hyprlock             # Screen locker
+    fnott                # Notification daemon
+
+    # Hardware manager/control
+    networkmanagerapplet # Network manager
+    nwg-displays         # Monitor manager
+    impala               # Wifi manager
+    bluetuith            # Bluetooth manager
+    brillo               # Brightness control
+    playerctl            # Media players control
 
     # Wallpaper
-    waypaper     # Wallpaper setter
-    hyprpaper    # Wallpaper backend
+    waypaper             # Wallpaper setter
+    hyprpaper            # Wallpaper backend
 
     # Screenshot
     inputs.hyprland-contrib.packages.${pkgs.system}.grimblast # Screenshot
-    grim         # Grimblast dependency
-    satty        # Screenshot annotation
-    slurp        # Region selector
+    grim                 # Grimblast dependency
+    satty                # Screenshot annotation
+    slurp                # Region selector
   ];
 }

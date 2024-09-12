1becba4adad8c034d30b5e067baf24f17cd2a6e0
Runeword <60324746+Runeword@users.noreply.github.com>
2024-09-12 22:23:40 +0200

    Update nixos/configuration.nix

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -23,6 +23,8 @@
   nix.settings.keep-outputs = true;
   nix.settings.keep-derivations = true;
 
+  nix.settings.trusted-users = [ "root" "charles" ];
+
   nix.settings.substituters = ["https://hyprland.cachix.org"];
   nix.settings.trusted-public-keys = ["hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="];
 

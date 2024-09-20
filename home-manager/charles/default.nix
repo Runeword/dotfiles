7ced608cafa84259b4f71fe9c0067e98f60ec98d
Runeword <60324746+Runeword@users.noreply.github.com>
2024-09-20 21:56:26 +0200

    Update home-manager/charles/default.nix

--- home-manager/charles/default.nix
+++ home-manager/charles/default.nix
@@ -1,9 +1,11 @@
-{ config
-, pkgs
-, lib
-, inputs
-, ...
-}: {
+{
+  config,
+  pkgs,
+  lib,
+  inputs,
+  ...
+}:
+{
   imports = [
     ./overlays.nix
     ./packages.nix
@@ -27,6 +29,16 @@
       package = pkgs.adw-gtk3;
       name = "adw-gtk3";
     };
+  };
+
+  home.sessionVariables = {
+    XDG_DATA_DIRS = "/usr/local/share:/usr/share";
+    XDG_CONFIG_DIRS = "/etc/xdg";
+    XDG_CACHE_HOME = "$HOME/.cache";
+    XDG_CONFIG_HOME = "$HOME/.config";
+    XDG_DATA_HOME = "$HOME/.local/share";
+    XDG_STATE_HOME = "$HOME/.local/state";
+    XDG_BIN_HOME = "$HOME/.local/bin";
   };
 
   programs = {

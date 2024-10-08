ca5216d46c1be275ba22af2ad66b7763db0a6558
Runeword <60324746+Runeword@users.noreply.github.com>
2024-10-08 22:50:28 +0200

    Update home-manager/flake.nix

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -36,17 +36,17 @@
       ];
     };
 
-    # homeConfigurations.jah = inputs.home-manager.lib.homeManagerConfiguration {
-    #   pkgs = import inputs.nixpkgs {
-    #     inherit system;
-    #     config.allowUnfree = true;
-    #   };
+    homeConfigurations.zod = inputs.home-manager.lib.homeManagerConfiguration {
+      pkgs = import inputs.nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
 
-    #   extraSpecialArgs = {inherit inputs;};
+      extraSpecialArgs = {inherit inputs;};
 
-    #   modules = [
-    #     ./jah
-    #   ];
-    # };
+      modules = [
+        ./zod
+      ];
+    };
   };
 }

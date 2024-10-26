c0291205febe810ce783cfb98f9a11f39877a9cb
Runeword <60324746+Runeword@users.noreply.github.com>
2024-10-26 21:55:01 +0200

    Add term/flake.nix

---
+++ term/flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Alacritty with cowsay";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages.default = pkgs.symlinkJoin {
+          name = "alacritty-with-cowsay";
+          paths = [ pkgs.alacritty pkgs.cowsay ];
+          buildInputs = [ pkgs.makeWrapper ];
+          postBuild = ''
+            wrapProgram $out/bin/alacritty \
+              --prefix PATH : ${pkgs.cowsay}/bin
+          '';
+        };
+
+        apps.default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/alacritty";
+        };
+      }
+    );
+}

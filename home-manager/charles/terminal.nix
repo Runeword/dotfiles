f8f73f9532e911fb87c10ee86ae88293ab20bfdf
Runeword <60324746+Runeword@users.noreply.github.com>
2024-10-28 10:42:23 +0100

    Update home-manager/charles/terminal.nix

--- home-manager/charles/terminal.nix
+++ home-manager/charles/terminal.nix
@@ -5,24 +5,24 @@
   config,
   ...
 }:
-let
-  leader = pkgs.stdenv.mkDerivation {
-    pname = "leader";
-    version = "0.3.2";
-    src = pkgs.fetchurl {
-      url = "https://github.com/dhamidi/leader/releases/download/v0.3.2/leader.linux.amd64";
-      sha256 = "sha256-lwOChHRvDvOm371v5xZUXS//6Dgn4CljioMrIBbWgwY=";
-    };
+# let
+  # leader = pkgs.stdenv.mkDerivation {
+  #   pname = "leader";
+  #   version = "0.3.2";
+  #   src = pkgs.fetchurl {
+  #     url = "https://github.com/dhamidi/leader/releases/download/v0.3.2/leader.linux.amd64";
+  #     sha256 = "sha256-lwOChHRvDvOm371v5xZUXS//6Dgn4CljioMrIBbWgwY=";
+  #   };
 
-    dontUnpack = true;
+  #   dontUnpack = true;
 
-    installPhase = ''
-      mkdir -p $out/bin
-      cp $src $out/bin/leader
-      chmod +x $out/bin/leader
-    '';
-  };
-in
+  #   installPhase = ''
+  #     mkdir -p $out/bin
+  #     cp $src $out/bin/leader
+  #     chmod +x $out/bin/leader
+  #   '';
+  # };
+# in
 {
   home.packages = with pkgs; [
     inputs.neovim-runeword.packages.x86_64-linux.default # Text editor

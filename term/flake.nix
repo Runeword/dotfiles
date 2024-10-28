46f5bf7ee87c3daa1ba42134808240726415de16
Runeword <60324746+Runeword@users.noreply.github.com>
2024-10-28 10:41:46 +0100

    Update term/flake.nix

--- term/flake.nix
+++ term/flake.nix
@@ -15,6 +15,23 @@
     flake-utils.lib.eachDefaultSystem (
       system:
       let
+        leader = pkgs.stdenv.mkDerivation {
+          pname = "leader";
+          version = "0.3.2";
+          src = pkgs.fetchurl {
+            url = "https://github.com/dhamidi/leader/releases/download/v0.3.2/leader.linux.amd64";
+            sha256 = "sha256-lwOChHRvDvOm371v5xZUXS//6Dgn4CljioMrIBbWgwY=";
+          };
+
+          dontUnpack = true;
+
+          installPhase = ''
+            mkdir -p $out/bin
+            cp $src $out/bin/leader
+            chmod +x $out/bin/leader
+          '';
+        };
+
         pkgs = nixpkgs.legacyPackages.${system};
 
         additionalPackages = with pkgs; [

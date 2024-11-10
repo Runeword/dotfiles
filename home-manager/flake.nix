997539f3795890a08b315c3adb4695bc19e6b450
Runeword <60324746+Runeword@users.noreply.github.com>
2024-11-10 13:18:30 +0100

    Update home-manager/flake.nix

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -11,7 +11,7 @@
   # inputs.neovim-nightly-overlay.inputs.nixpkgs.follows = "nixpkgs";
   ## inputs.neovim-nightly-overlay.inputs.nixpkgs.url = "github:nixos/nixpkgs?rev=fad51abd42ca17a60fc1d4cb9382e2d79ae31836";
 
-  inputs.neovim-runeword.url = "github:Runeword/dotfiles?dir=neovim";
+  inputs.runeword-neovim.url = "github:Runeword/dotfiles?dir=neovim";
   inputs.runeword-terminal.url = "github:Runeword/dotfiles?dir=term";
   inputs.ags.url = "github:Aylur/ags";
 

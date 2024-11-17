41fdaabf53cf4280a5d78d8421bae98a32f62cb3
Runeword <60324746+Runeword@users.noreply.github.com>
2024-11-17 22:46:44 +0100

    Update home-manager/charles/desktop.nix

--- home-manager/charles/desktop.nix
+++ home-manager/charles/desktop.nix
@@ -1,46 +1,66 @@
-{ pkgs
-, inputs
-, lib
-, config
-, ...
-  }:
-  let
-  in
-  {
+{
+  pkgs,
+  inputs,
+  lib,
+  config,
+  ...
+}:
+let
+in
+# waybarwithfont = pkgs.waybar.overrideAttrs (oldAttrs: {
+#   nativeBuildInputs = oldAttrs.nativeBuildInputs ++ [ pkgs.nerdfonts.override {
+#   fonts = [ "DroidSansMono" ];
+# }; ];
+#   postInstall =
+#     (oldAttrs.postInstall or "")
+#     + ''
+#       mkdir -p $out/share/fonts
+#       ln -s ${customFont}/share/fonts/* $out/share/fonts/
+#     '';
+# });
+{
   home.packages = with pkgs; [
     # fuzzel # App launcher
 
     # Viewers
-    imv                  # Image viewer
-    wev                  # Event viewer
+    imv # Image viewer
+    wev # Event viewer
 
     # Interface
-    (waybar.overrideAttrs (oldAttrs: { mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ]; })) # Bar
+    (waybar.overrideAttrs (oldAttrs: {
+      mesonFlags = oldAttrs.mesonFlags ++ [ "-Dexperimental=true" ];
+    }))
+
+    (pkgs.nerdfonts.override {
+      fonts = [
+        "DroidSansMono"
+      ];
+    })
     hyprpanel
-    hyprlock             # Screen locker
-    fnott                # Notification daemon
+    hyprlock # Screen locker
+    fnott # Notification daemon
     # deadd-notification-center
 
     # Hardware managers
     networkmanagerapplet # Network manager
-    nwg-displays         # Monitor manager
-    impala               # Wifi manager
-    bluetuith            # Bluetooth manager
+    nwg-displays # Monitor manager
+    impala # Wifi manager
+    bluetuith # Bluetooth manager
     # pw-viz             # Pipewire graph editor
 
     # Controllers
-    brillo               # Brightness control
-    playerctl            # Media players control
+    brillo # Brightness control
+    playerctl # Media players control
 
     # Wallpaper
-    waypaper             # Wallpaper setter
-    hyprpaper            # Wallpaper backend
+    waypaper # Wallpaper setter
+    hyprpaper # Wallpaper backend
 
     # Screen capture
     # wl-screenrec         # Screen recorder
     inputs.hyprland-contrib.packages.${pkgs.system}.grimblast # Screenshot
-    grim                 # Grimblast dependency
-    satty                # Screenshot annotation
-    slurp                # Region selector
+    grim # Grimblast dependency
+    satty # Screenshot annotation
+    slurp # Region selector
   ];
 }

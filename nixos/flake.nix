7f6d4280ab1a17f12edb4fc96a45f1be078634e7
Runeword <60324746+Runeword@users.noreply.github.com>
2024-11-18 14:55:12 +0100

    Update nixos/flake.nix

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,29 +1,33 @@
 {
-  # inputs.nixpkgs.url = "github:nixos/nixpkgs";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   # inputs.nixpkgs.url = "github:nixos/nixpkgs?rev=b12803b6d90e2e583429bb79b859ca53c348b39a";
+  # inputs.nixpkgs.url = "github:nixos/nixpkgs";
 
   inputs.nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
-  # inputs.hyprland.url = "github:hyprwm/Hyprland";
   inputs.hyprland.url = "git+https://github.com/hyprwm/Hyprland?submodules=1";
   # inputs.hyprland.inputs.nixpkgs.follows = "nixpgks";
   # inputs.hyprland.inputs.nixpkgs.follows = "nixpkgs-unstable";
   # inputs.hyprland.inputs.nixpkgs.url = "github:nixos/nixpkgs?rev=b12803b6d90e2e583429bb79b859ca53c348b39a";
 
-  outputs = {self, ...} @ inputs: let
-    system = "x86_64-linux";
-  in {
-    nixosConfigurations.charles = inputs.nixpkgs.lib.nixosSystem {
-      inherit system;
+  outputs =
+    { self, ... }@inputs:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      nixosConfigurations.charles = inputs.nixpkgs.lib.nixosSystem {
+        inherit system;
 
-      specialArgs = {inherit inputs;};
+        specialArgs = {
+          inherit inputs;
+        };
 
-      modules = [
-        ./configuration.nix
-        ./hardware-configuration.nix
-        inputs.nixos-hardware.nixosModules.framework-12th-gen-intel
-      ];
+        modules = [
+          ./configuration.nix
+          ./hardware-configuration.nix
+          inputs.nixos-hardware.nixosModules.framework-12th-gen-intel
+        ];
+      };
     };
-  };
 }

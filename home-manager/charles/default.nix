94a69e59739d201c52ad5fd8035f589b5728d66d
Runeword <60324746+Runeword@users.noreply.github.com>
2024-11-19 19:37:36 +0100

    Update home-manager/charles/default.nix

--- home-manager/charles/default.nix
+++ home-manager/charles/default.nix
@@ -44,15 +44,15 @@
   #   };
   # };
 
-  home.sessionVariables = {
-    XDG_DATA_DIRS = "/usr/local/share:/usr/share";
-    XDG_CONFIG_DIRS = "/etc/xdg";
-    XDG_CACHE_HOME = "$HOME/.cache";
-    XDG_CONFIG_HOME = "$HOME/.config";
-    XDG_DATA_HOME = "$HOME/.local/share";
-    XDG_STATE_HOME = "$HOME/.local/state";
-    XDG_BIN_HOME = "$HOME/.local/bin";
-  };
+  # home.sessionVariables = {
+  #   XDG_DATA_DIRS = "/usr/local/share:/usr/share";
+  #   XDG_CONFIG_DIRS = "/etc/xdg";
+  #   XDG_CACHE_HOME = "$HOME/.cache";
+  #   XDG_CONFIG_HOME = "$HOME/.config";
+  #   XDG_DATA_HOME = "$HOME/.local/share";
+  #   XDG_STATE_HOME = "$HOME/.local/state";
+  #   XDG_BIN_HOME = "$HOME/.local/bin";
+  # };
 
   xdg.mime.enable = true;
 

5a916b161bbd5dc7d507b87f8a106a29540798f5
Runeword <60324746+Runeword@users.noreply.github.com>
2024-11-21 22:25:10 +0100

    Add .config/shell/functions/git.sh Remove .config/shell/functions/git.sh

---
+++ dot_config/shell/functions/executable_git.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+__git_clone() {
+  local repo_url="${1:-$(wl-paste)}" # Use clipboard content if no URL is provided
+  local base_dir="${HOME}/Documents/dev"
+  mkdir -p "$base_dir" # Create the base directory if it doesn't exist
+  git clone "$repo_url" "$base_dir/$(basename "$repo_url" .git)";
+  cd "$base_dir/$(basename "$repo_url" .git)" || return # Change into the cloned directory
+}

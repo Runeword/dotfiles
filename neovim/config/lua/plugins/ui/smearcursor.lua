e2d331e439cebddd7471966f1995a4b75618ae45
Runeword <60324746+Runeword@users.noreply.github.com>
2024-11-25 22:45:37 +0100

    Update neovim/config/lua/plugins/ui/smearcursor.lua

--- neovim/config/lua/plugins/ui/smearcursor.lua
+++ neovim/config/lua/plugins/ui/smearcursor.lua
@@ -7,8 +7,8 @@ return {
     require('smear_cursor').setup({
       cursor_color = '#d3cdc3',
       normal_bg = '#282828',
-      smear_between_buffers = true,
-      smear_between_neighbor_lines = true,
+      smear_between_buffers = false,
+      smear_between_neighbor_lines = false,
       use_floating_windows = false,
       legacy_computing_symbols_support = false,
       hide_target_hack = true,

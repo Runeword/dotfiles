47d87808d0f48b9fb2286512764f513736942bc8
Runeword <60324746+Runeword@users.noreply.github.com>
2024-11-25 22:45:31 +0100

    Update .config/shell/functions/git.sh

--- dot_config/shell/functions/executable_git.sh
+++ dot_config/shell/functions/executable_git.sh
@@ -2,7 +2,7 @@
 
 __git_clone() {
   local repo_url="${1:-$(wl-paste)}" # Use clipboard content if no URL is provided
-  local base_dir="${HOME}/Documents/dev"
+  local base_dir="${HOME}/{$1}"
   mkdir -p "$base_dir" # Create the base directory if it doesn't exist
   git clone "$repo_url" "$base_dir/$(basename "$repo_url" .git)";
   cd "$base_dir/$(basename "$repo_url" .git)" || return # Change into the cloned directory

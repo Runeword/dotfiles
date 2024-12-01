f45726c984e9cb2dd99962cf62d3b28824d2c44a
Runeword <60324746+Runeword@users.noreply.github.com>
2024-12-01 20:28:36 +0100

    Update neovim/config/lua/plugins/completion/gp.lua

--- neovim/config/lua/plugins/completion/gp.lua
+++ neovim/config/lua/plugins/completion/gp.lua
@@ -1,19 +1,21 @@
+local vim = vim
 return {
   'robitx/gp.nvim',
 
   config = function()
     require('gp').setup({
-      openai_api_key = { 'pass', 'show', 'OPENAI_API_KEY', },
+      -- openai_api_key = { 'pass', 'show', 'OPENAI_API_KEY', },
 
       providers = {
         openai = {},
+
         googleai = {
           disabled = false,
-          endpoint =
-          'https://generativelanguage.googleapis.com/v1beta/models/{{model}}:streamGenerateContent?key={{secret}}',
           secret = { 'pass', 'show', 'GEMINI_API_KEY', },
         },
       },
     })
+
+    vim.keymap.set('n', '<Leader>c', '<cmd>:GpChatNew vsplit<CR>')
   end,
 }

de822709a26dec60bd3c48bc893b6990e86a0a71
Runeword <60324746+Runeword@users.noreply.github.com>
2024-12-02 20:08:44 +0100

    Update neovim/config/lua/plugins/search/fzf.lua

--- neovim/config/lua/plugins/search/fzf.lua
+++ neovim/config/lua/plugins/search/fzf.lua
@@ -2,22 +2,22 @@ local vim = vim
 
 return {
   'junegunn/fzf',
-  -- config = function()
-  --   vim.cmd [[
-  --     autocmd! FileType fzf
-  --     autocmd FileType fzf tnoremap <buffer> <esc> <c-c>
-  --     autocmd  FileType fzf set laststatus=0 noshowmode noruler
-  --       \| autocmd BufLeave <buffer> set laststatus=2 showmode ruler
-  --     ]]
+  config = function()
+    vim.cmd [[
+      autocmd! FileType fzf
+      autocmd FileType fzf tnoremap <buffer> <esc> <c-c>
+      autocmd  FileType fzf set laststatus=0 noshowmode noruler
+        \| autocmd BufLeave <buffer> set laststatus=2 showmode ruler
+      ]]
 
-  --   vim.keymap.set('n', '<Tab>',
-  --     function()
-  --       vim.fn['fzf#run'] {
-  --         sink = 'e',
-  --         window = { width = 0.9, height = 0.6, },
-  --         options = { '--preview=bat {}', },
-  --       }
-  --     end,
-  --     { silent = true, })
-  -- end,
+    vim.keymap.set('n', '<Tab>',
+      function()
+        vim.fn['fzf#run'] {
+          sink = 'e',
+          window = { width = 0.9, height = 0.6, },
+          options = { '--reverse', '--prompt=  ', '--preview=bat --style=plain --color=always {1}', '--no-scrollbar', '--preview-window=right,55%,border-none', '--no-separator', '--inline-info' },
+        }
+      end,
+      { silent = true, })
+  end,
 }

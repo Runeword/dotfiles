7719b45e6d5759a83e9bd07e8f0a4226a97a4c4a
Runeword <60324746+Runeword@users.noreply.github.com>
2024-12-13 21:38:16 +0100

    Add neovim/config/lua/plugins/completion/neocodeium.lua

---
+++ neovim/config/lua/plugins/completion/neocodeium.lua
@@ -0,0 +1,15 @@
+local vim = vim
+return {
+  'monkoose/neocodeium',
+  event = 'VeryLazy',
+  enabled = true,
+
+  config = function()
+    local neocodeium = require('neocodeium')
+    neocodeium.setup()
+
+    vim.keymap.set('i', '<C-CR>', neocodeium.accept)
+    vim.keymap.set('i', '<C-w>', neocodeium.accept_word)
+    vim.keymap.set('i', '<C-e>', neocodeium.accept_line)
+  end,
+}

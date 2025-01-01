9d0f627e616b48f74ae827817db8f0a964445008
Runeword <60324746+Runeword@users.noreply.github.com>
2025-01-01 22:15:27 +0100

    Update neovim/config/lua/plugins/minimal/conform.lua

--- neovim/config/lua/plugins/minimal/conform.lua
+++ neovim/config/lua/plugins/minimal/conform.lua
@@ -2,22 +2,26 @@ local vim = vim
 
 return {
   'stevearc/conform.nvim',
+
+  enabled = true,
+
   config = function()
     require('conform').setup({
       formatters_by_ft = {
         sh = { 'shfmt', 'shellharden', },
         zsh = { 'shfmt', 'shellharden', }, -- 'beautysh'
         python = { 'isort', 'black', },
-        -- null_ls.builtins.formatting.shfmt,
-        -- null_ls.builtins.diagnostics.eslint,
-        -- null_ls.builtins.completion.spell,
+				javascript = { 'prettier' },
+				typescript = { 'prettier' },
       },
+
       formatters = {
         shfmt = {
           prepend_args = { '-i', '2', '-ci', },
         },
       },
     })
+
     vim.keymap.set({ 'n', 'x', }, '<Leader>f',
       function() require('conform').format({ async = true, lsp_fallback = true, }) end)
   end,

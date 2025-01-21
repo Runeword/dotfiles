78947688b88ff0972b3182f032470c11d7b3ff29
Runeword <60324746+Runeword@users.noreply.github.com>
2025-01-21 19:10:12 +0100

    Update neovim/config/lua/plugins/move/grasp.lua

--- neovim/config/lua/plugins/move/grasp.lua
+++ neovim/config/lua/plugins/move/grasp.lua
@@ -5,9 +5,7 @@ return {
 
   config = function()
     vim.keymap.set({ 'x', 'o', }, '<Tab>',   require('grasp').select_treesitter_node_under_cursor)
-    vim.keymap.set('n',           '<Tab>',   require('grasp').move_to_next_treesitter_node)
-    vim.keymap.set('n',           '<S-Tab>', require('grasp').move_to_prev_treesitter_node)
-    -- vim.keymap.set('n',           '<Down>',  require('grasp').move_to_next_treesitter_node)
-    -- vim.keymap.set('n',           '<Up>',    require('grasp').move_to_prev_treesitter_node)
+    -- vim.keymap.set('n',           '<Tab>',   require('grasp').move_to_next_treesitter_node)
+    -- vim.keymap.set('n',           '<S-Tab>', require('grasp').move_to_prev_treesitter_node)
   end,
 }

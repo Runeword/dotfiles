c52e457ec562885e4e684b41c2198547ae892273
Runeword <60324746+Runeword@users.noreply.github.com>
2025-03-12 21:16:42 +0100

    Update .config/surfingkeys.js

--- dot_config/surfingkeys.js
+++ dot_config/surfingkeys.js
@@ -1,4 +1,4 @@
-settings.blocklistPattern = /.*getpocket\.com.*|.*mail\.google\.com.*|.*docs\.google\.com.*/i;
+settings.blocklistPattern = /.*getpocket\.com.*|.*academy\.hackthebox\.com.*|.*mail\.google\.com.*|.*docs\.google\.com.*/i;
 // chrome.storage.local.set({"noPdfViewer": 1})
 
 // Remap keys

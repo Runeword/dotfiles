64e2cd56a32d746fd8270d3a5ad17691d029659d
Runeword <60324746+Runeword@users.noreply.github.com>
2025-03-18 21:01:02 +0100

    Update neovim/config/lua/plugins/core/lspconfig.lua

--- neovim/config/lua/plugins/core/lspconfig.lua
+++ neovim/config/lua/plugins/core/lspconfig.lua
@@ -13,16 +13,16 @@ return {
         client.server_capabilities.documentFormattingProvider = dfp
         client.server_capabilities.semanticTokensProvider = nil
 
-   vim.keymap.set('n', 'gh', '<cmd>lua vim.lsp.buf.hover()<cr>', { buffer = buffer, })
-   vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', { buffer = buffer, })
-   vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', { buffer = buffer, })
-   vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', { buffer = buffer, })
-   vim.keymap.set('n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<cr>', { buffer = buffer, })
-   vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', { buffer = buffer, })
-   vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', { buffer = buffer, })
-   -- vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', { buffer = buffer, })
-   -- vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', { buffer = buffer, })
-   -- vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', { buffer = buffer, })
+        vim.keymap.set('n', 'gh', '<cmd>lua vim.lsp.buf.hover()<cr>', { buffer = buffer, })
+        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', { buffer = buffer, })
+        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', { buffer = buffer, })
+        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', { buffer = buffer, })
+        vim.keymap.set('n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<cr>', { buffer = buffer, })
+        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', { buffer = buffer, })
+        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', { buffer = buffer, })
+        -- vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', { buffer = buffer, })
+        -- vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', { buffer = buffer, })
+        -- vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', { buffer = buffer, })
 
         -- vim.keymap.set('n', 'gd', function()
         --   if not vim.lsp.buf.definition() then vim.lsp.buf.declaration() end
@@ -43,7 +43,7 @@ return {
         -- vim.keymap.set('n', '<PageDown>', vim.diagnostic.goto_next, { buffer = buffer, })
 
         -- vim.keymap.set('n', '<Leader>x', vim.diagnostic.setqflist,
-          -- { noremap = true, silent = true, })
+        -- { noremap = true, silent = true, })
 
         -- vim.keymap.set('n', '<Leader>l', diagnostic.setloclist, { noremap = true, silent = true })
         -- lsp.buf.formatting_seq_sync(nil, 6000, { 'ts_ls', 'html', 'cssls', 'vuels', 'eslint' })
@@ -68,29 +68,18 @@ return {
     lspconfig['lua_ls'].setup(set_config())
     lspconfig.yamlls.setup(set_config())
     lspconfig.ccls.setup(set_config())
-		lspconfig.eslint.setup(set_config())
+    lspconfig.eslint.setup(set_config())
     lspconfig.jsonls.setup(set_config())
     lspconfig.gopls.setup(set_config())
     lspconfig['rust_analyzer'].setup(set_config())
     lspconfig.marksman.setup(set_config())
     lspconfig.terraformls.setup(set_config())
     lspconfig.taplo.setup(set_config())
-    -- lspconfig['terraform_lsp'].setup(set_config())
     lspconfig.bashls.setup(set_config({ filetypes = { 'sh', 'zsh', }, }))
     lspconfig.cssls.setup(set_config())
-    -- lspconfig.vuels.setup(set_config({ on_attach = on_attach_server(false), }))
-		lspconfig.volar.setup(set_config({ on_attach = on_attach_server(false), }))
+    lspconfig.volar.setup(set_config({ on_attach = on_attach_server(false), }))
     lspconfig.pyright.setup(set_config())
-
-    -- lspconfig.typos_lsp.setup(set_config(
-    --   {
-    --     root_dir = function() return '' end,
-    --     cmd_env = { RUST_LOG = 'Hint', },
-    --     init_options = {
-    --       diagnosticSeverity = 'hint',
-    --     },
-    --   }
-    -- ))
+    lspconfig.harper_ls.setup(set_config())
 
     lspconfig['ts_ls'].setup(set_config(
       {
@@ -167,17 +156,17 @@ return {
         vim.api.nvim_set_hl(0, 'DiagnosticError', { bg = 'NONE', fg = '#cf4e84', bold = false, italic = true, })
         vim.api.nvim_set_hl(0, 'DiagnosticVirtualTextError',
           { bg = 'NONE', fg = '#cf4e84', bold = false, italic = true, })
-        vim.api.nvim_set_hl(0, 'DiagnosticUnderlineError',  { bg = 'NONE', undercurl = true, sp = '#cf4e84', })
-        vim.api.nvim_set_hl(0, 'diagnosticWarn',            { bg = 'NONE', fg = '#ff9d57', bold = false, italic = true, })
+        vim.api.nvim_set_hl(0, 'DiagnosticUnderlineError', { bg = 'NONE', undercurl = true, sp = '#cf4e84', })
+        vim.api.nvim_set_hl(0, 'diagnosticWarn', { bg = 'NONE', fg = '#ff9d57', bold = false, italic = true, })
         vim.api.nvim_set_hl(0, 'diagnosticVirtualTextWarn', { bg = 'NONE', fg = '#ff9d57', bold = false, italic = true, })
-        vim.api.nvim_set_hl(0, 'diagnosticUnderlineWarn',   { bg = 'NONE', undercurl = true, sp = '#ff9d57', })
-        vim.api.nvim_set_hl(0, 'diagnosticInfo',            { bg = 'NONE', fg = '#a6c8ff', bold = false, italic = true, })
+        vim.api.nvim_set_hl(0, 'diagnosticUnderlineWarn', { bg = 'NONE', undercurl = true, sp = '#ff9d57', })
+        vim.api.nvim_set_hl(0, 'diagnosticInfo', { bg = 'NONE', fg = '#a6c8ff', bold = false, italic = true, })
         vim.api.nvim_set_hl(0, 'diagnosticVirtualTextInfo', { bg = 'NONE', fg = '#a6c8ff', bold = false, italic = true, })
-        vim.api.nvim_set_hl(0, 'diagnosticUnderlineInfo',   { bg = 'NONE', undercurl = true, sp = '#a6c8ff', })
-        vim.api.nvim_set_hl(0, 'diagnosticHint',            { bg = 'NONE', fg = '#adb5bd', bold = false, italic = true, })
+        vim.api.nvim_set_hl(0, 'diagnosticUnderlineInfo', { bg = 'NONE', undercurl = true, sp = '#a6c8ff', })
+        vim.api.nvim_set_hl(0, 'diagnosticHint', { bg = 'NONE', fg = '#adb5bd', bold = false, italic = true, })
         vim.api.nvim_set_hl(0, 'diagnosticVirtualTextHint', { bg = 'NONE', fg = '#adb5bd', bold = false, italic = true, })
-        vim.api.nvim_set_hl(0, 'diagnosticUnderlineHint',   { bg = 'NONE', undercurl = true, sp = '#adb5bd', })
-        vim.api.nvim_set_hl(0, 'diagnosticUnnecessary',     { bg = 'NONE', undercurl = true, sp = '#adb5bd', })
+        vim.api.nvim_set_hl(0, 'diagnosticUnderlineHint', { bg = 'NONE', undercurl = true, sp = '#adb5bd', })
+        vim.api.nvim_set_hl(0, 'diagnosticUnnecessary', { bg = 'NONE', undercurl = true, sp = '#adb5bd', })
 
         -- vim.api.nvim_set_hl(0, 'diagnosticfloatingerror',  { link = 'diagnosticvirtualtexterror', })
         -- vim.api.nvim_set_hl(0, 'diagnosticfloatinghint',   { link = 'diagnosticvirtualtexthint', })

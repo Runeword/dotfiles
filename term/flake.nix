642e1953f7f153021edc21822d6d6a5ed8424086
Runeword <60324746+Runeword@users.noreply.github.com>
2025-03-31 00:46:55 +0200

    Update term/flake.nix

--- term/flake.nix
+++ term/flake.nix
@@ -22,147 +22,28 @@
           };
         };
 
-        customPackages.leader = pkgs.stdenv.mkDerivation {
-          pname = "leader";
-          version = "0.3.2";
-          src = pkgs.fetchurl {
-            url = "https://github.com/dhamidi/leader/releases/download/v0.3.2/leader.linux.amd64";
-            sha256 = "sha256-lwOChHRvDvOm371v5xZUXS//6Dgn4CljioMrIBbWgwY=";
-          };
+        mkOutOfStoreSymlink = import ./lib/mkOutOfStoreSymlink.nix { inherit pkgs; };
 
-          dontUnpack = true;
+        customPackages.leader = import ./packages/leader.nix { inherit pkgs; };
 
-          installPhase = ''
-            mkdir -p $out/bin
-            cp $src $out/bin/leader
-            chmod +x $out/bin/leader
-          '';
+        customTmux = import ./packages/tmux.nix {
+          inherit pkgs;
+          inherit mkOutOfStoreSymlink;
         };
 
-        mkOutOfStoreSymlink =
-          path:
-          let
-            fileName = builtins.baseNameOf path;
-          in
-          "${
-            pkgs.runCommandLocal "out-of-store-symlink-${fileName}"
-              {
-                allowSubstitutes = false;
-                preferLocalBuild = true;
-              }
-              ''
-                mkdir -p $out
-                ln -s ${toString /home/charles/term}${pkgs.lib.removePrefix (toString ./.) (toString path)} $out/${fileName}
-              ''
-          }/${fileName}";
-
-        customTmux = pkgs.symlinkJoin {
-          name = "tmux-with-config";
-          paths = [ pkgs.tmux ];
-          buildInputs = [ pkgs.makeWrapper ];
-          postBuild = ''
-            mkdir -p $out/.config/tmux/plugins
-
-            ln -sf ${mkOutOfStoreSymlink (toString ./tmux/tmux.conf)} $out/.config/tmux/tmux.conf
-
-            ln -s ${pkgs.tmuxPlugins.resurrect}/share/tmux-plugins/resurrect $out/.config/tmux/plugins/resurrect
-            ln -s ${pkgs.tmuxPlugins.tmux-fzf}/share/tmux-plugins/tmux-fzf $out/.config/tmux/plugins/tmux-fzf
-
-            wrapProgram $out/bin/tmux \
-              --set XDG_CONFIG_HOME "$out/.config"
-          '';
+        packagesModule = import ./modules/packages.nix {
+          inherit pkgs;
+          inherit customTmux;
         };
+        extraPackages = packagesModule.extraPackages ++ [ customPackages.leader ];
+        extraFonts = packagesModule.extraFonts;
 
-        extraPackages = with pkgs; [
-          customPackages.leader # leader key
-          cowsay # cowsay
-          yazi # file manager
-          navi # cheat sheet
-          customTmux
-          starship # prompt
-          wl-clipboard # copy/paste
-          xdragon # drag and drop
-          ueberzugpp # images support for terminal
-          aider-chat # ai
-          qrcp # mobile QR files transfer
-          ngrok
-          awscli2
-
-          #_______________________________ Coreutils
-          bat
-          zoxide
-          gomi
-          ripgrep
-          fd
-          fzf
-          tree
-          wget
-          jq
-          sshs
-
-          #_______________________________ Monitoring
-          htop
-          btop
-          procs
-          gping
-          hyperfine # benchmarking
-
-          #_______________________________ Archivers
-          ouch
-          xarchiver
-          unzip
-
-          #_______________________________ Versioning
-          lazygit
-          git-graph
-          git # versioning
-          zsh-forgit # fuzzy git
-
-          #_______________________________ Containers
-          lazydocker
-          docker-compose
-          distrobox
-          terraform
-
-          #_______________________________ Files
-          miller # cvs toolbox
-          glow # markdown
-
-          #_______________________________ Info
-          hwinfo # Hardware info
-          onefetch # Git info
-          neofetch # System info
-
-          # zip
-          # p7zip
-          # gh
-          # lazygit
-          # gitui
-          # inputs.src-cli.packages.x86_64-linux.default
-        ];
-
-        extraFonts = [
-          pkgs.nerd-fonts.sauce-code-pro
-          pkgs.nerd-fonts.monaspace
-          pkgs.nerd-fonts.caskaydia-mono
-        ];
-
-        customAlacritty =
-          pkgs.runCommand "alacritty"
-            {
-              nativeBuildInputs = [ pkgs.makeWrapper ];
-            }
-            ''
-              mkdir -p $out/bin
-              mkdir -p $out/.config/alacritty
-
-              ln -sf ${mkOutOfStoreSymlink (toString ./alacritty/alacritty.toml)} $out/.config/alacritty/alacritty.toml
-
-              makeWrapper ${pkgs.alacritty}/bin/alacritty $out/bin/alacritty \
-                --prefix PATH : ${pkgs.lib.makeBinPath extraPackages} \
-                --set FONTCONFIG_FILE ${pkgs.makeFontsConf { fontDirectories = extraFonts; }} \
-                --set XDG_CONFIG_HOME "$out/.config"
-            '';
+        customAlacritty = import ./packages/alacritty.nix {
+          inherit pkgs;
+          inherit mkOutOfStoreSymlink;
+          inherit extraPackages;
+          inherit extraFonts;
+        };
 
       in
       {

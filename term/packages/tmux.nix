b97b3ad45f48cc404ba17ecb4aed8bc34bb05159
Runeword <60324746+Runeword@users.noreply.github.com>
2025-03-31 00:40:59 +0200

    Add term/packages/tmux.nix

---
+++ term/packages/tmux.nix
@@ -0,0 +1,19 @@
+# Custom Tmux with configuration
+{ pkgs, mkOutOfStoreSymlink }:
+
+pkgs.symlinkJoin {
+  name = "tmux-with-config";
+  paths = [ pkgs.tmux ];
+  buildInputs = [ pkgs.makeWrapper ];
+  postBuild = ''
+    mkdir -p $out/.config/tmux/plugins
+
+    ln -sf ${mkOutOfStoreSymlink "config/tmux/tmux.conf"} $out/.config/tmux/tmux.conf
+
+    ln -s ${pkgs.tmuxPlugins.resurrect}/share/tmux-plugins/resurrect $out/.config/tmux/plugins/resurrect
+    ln -s ${pkgs.tmuxPlugins.tmux-fzf}/share/tmux-plugins/tmux-fzf $out/.config/tmux/plugins/tmux-fzf
+
+    wrapProgram $out/bin/tmux \
+      --set XDG_CONFIG_HOME "$out/.config"
+  '';
+}

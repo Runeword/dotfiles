dd74528943c199df00ddb7950801d12031759b56
Runeword <60324746+Runeword@users.noreply.github.com>
2025-04-03 21:21:04 +0200

    Update term/packages/zsh.nix

--- term/packages/zsh.nix
+++ term/packages/zsh.nix
@@ -6,8 +6,10 @@ pkgs.symlinkJoin {
   buildInputs = [ pkgs.makeWrapper ];
   postBuild = ''
     mkdir -p $out/etc
+    # mkdir -p /home/charles/term/config/zsh/plugins/zsh-autosuggestions
 
     ln -sf ${mkOutOfStoreSymlink "config/zsh/zshrc"} $out/etc/zshrc
+    # ln -s ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions /home/charles/term/config/zsh/plugins/zsh-autosuggestions
 
     wrapProgram $out/bin/zsh \
       --set ZDOTDIR $out/etc \

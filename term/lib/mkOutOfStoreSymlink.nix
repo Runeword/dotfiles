83f112020d820a5cc29c7146969a91eccef8bf5e
Runeword <60324746+Runeword@users.noreply.github.com>
2025-04-05 00:03:33 +0200

    Update term/lib/mkOutOfStoreSymlink.nix

--- term/lib/mkOutOfStoreSymlink.nix
+++ term/lib/mkOutOfStoreSymlink.nix
@@ -9,6 +9,15 @@ let
   pathStr = toString path;
   name = builtins.baseNameOf pathStr;
   fullPath = "${toString workspacePath}/${pathStr}";
+  # Check if this is a directory by trying to list it
+  isDir = builtins.pathExists fullPath && builtins.readDir fullPath != {};
 in
-pkgs.runCommandLocal name {} ''ln -s ${pkgs.lib.escapeShellArg fullPath} $out''
-
+if isDir then
+  # For directories, create a derivation that will symlink the directory
+  pkgs.runCommandLocal name {} ''
+    mkdir -p $out
+    ln -s ${pkgs.lib.escapeShellArg fullPath}/* $out/
+  ''
+else
+  # For files, use the original implementation
+  pkgs.runCommandLocal name {} ''ln -s ${pkgs.lib.escapeShellArg fullPath} $out''

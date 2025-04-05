25571d2a4ca7b2386fd2ee8ad34bdd12e5f3ea02
Runeword <60324746+Runeword@users.noreply.github.com>
2025-04-05 15:16:50 +0200

    Update term/packages/tmux.nix

--- term/packages/tmux.nix
+++ term/packages/tmux.nix
@@ -1,20 +1,20 @@
-# Custom Tmux with configuration
 { pkgs, mkOutOfStoreSymlink }:
 
 pkgs.symlinkJoin {
   name = "tmux-with-config";
   paths = [ pkgs.tmux ];
-  buildInputs = [
-    pkgs.makeWrapper
-  ];
+  buildInputs = [ pkgs.makeWrapper ];
   postBuild = ''
     mkdir -p $out/.config
+
     ln -sf ${mkOutOfStoreSymlink "config/tmux"} $out/.config/tmux
 
-    # ln -sf ${pkgs.tmuxPlugins.resurrect}/share/tmux-plugins/resurrect $out/.config/tmux/plugins/resurrect
-    # ln -sf ${pkgs.tmuxPlugins.tmux-fzf}/share/tmux-plugins/tmux-fzf $out/.config/tmux/plugins/tmux-fzf
+    mkdir -p $out/share/tmux-plugins
+
+    ln -sf ${pkgs.tmuxPlugins.resurrect}/share/tmux-plugins/resurrect $out/share/tmux-plugins/resurrect
+    ln -sf ${pkgs.tmuxPlugins.tmux-fzf}/share/tmux-plugins/tmux-fzf $out/share/tmux-plugins/tmux-fzf
 
     wrapProgram $out/bin/tmux \
-      --set XDG_CONFIG_HOME "$out/.config"
+    --set XDG_CONFIG_HOME "$out/.config"
   '';
 }

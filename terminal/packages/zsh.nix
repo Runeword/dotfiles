7c5c459b1faa6c398cd59b24c71d064592c477ef
Runeword <60324746+Runeword@users.noreply.github.com>
2025-04-06 22:47:12 +0200

    Add terminal/packages/zsh.nix

---
+++ terminal/packages/zsh.nix
@@ -0,0 +1,19 @@
+{ pkgs, mkOutOfStoreSymlink }:
+
+pkgs.symlinkJoin {
+  name = "zsh-with-config";
+  paths = [ pkgs.zsh ];
+  buildInputs = [ pkgs.makeWrapper ];
+  postBuild = ''
+    mkdir -p $out/etc
+    # mkdir -p /home/charles/term/config/zsh/plugins/zsh-autosuggestions
+
+    ln -sf ${mkOutOfStoreSymlink "config/zsh/zshrc"} $out/etc/zshrc
+
+    # ln -s ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions /home/charles/term/config/zsh/plugins/zsh-autosuggestions
+
+    wrapProgram $out/bin/zsh \
+      --set ZDOTDIR $out/etc \
+      # --add-flags "-i"
+  '';
+}

a24f7a52858a060c474d25c55d8699a27269a238
Runeword <60324746+Runeword@users.noreply.github.com>
2025-04-09 20:22:31 +0200

    Update home-manager/charles/packages.nix

--- home-manager/charles/packages.nix
+++ home-manager/charles/packages.nix
@@ -55,8 +55,8 @@ in
 {
   # home.file.".config/nvim".source =
   #   config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/neovim/config";
-  home.file.".config/zsh/plugins/zsh-autosuggestions".source =
-    "${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions";
+  # home.file.".config/zsh/plugins/zsh-autosuggestions".source =
+  #   "${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions";
 
   # home.file.".local/share/bash-completion/bash_completion".source = "${pkgs.bash-completion}/share/bash-completion/bash_completion";
 
@@ -68,8 +68,10 @@ in
     inputs.runeword-terminal.packages.x86_64-linux.default
     python311
     gcc
+    obsidian
 
     git
+    gitleaks
     # zsh
 
     pgmodeler

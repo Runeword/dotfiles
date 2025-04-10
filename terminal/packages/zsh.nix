022c3201d8b68389bf5137e06b1deab90f8d7ca1
Runeword <60324746+Runeword@users.noreply.github.com>
2025-04-10 20:05:10 +0200

    Update terminal/packages/zsh.nix

--- terminal/packages/zsh.nix
+++ terminal/packages/zsh.nix
@@ -5,15 +5,7 @@ pkgs.symlinkJoin {
   paths = [ pkgs.zsh ];
   buildInputs = [ pkgs.makeWrapper ];
   postBuild = ''
-    # Create wrapper scripts for zsh-autosuggestions
     mkdir -p $out/bin
-
-    # Autosuggestions script
-    cat > $out/bin/zsh-autosuggestions << EOF
-    #!/bin/sh
-    exec ${pkgs.zsh-autosuggestions}/share/zsh-autosuggestions/zsh-autosuggestions.zsh "\$@"
-    EOF
-    chmod +x $out/bin/zsh-autosuggestions
 
     ln -sf ${mkOutOfStoreSymlink "config/zsh/zshrc"} $out/.zshrc
 

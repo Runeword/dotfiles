df2dc95861db777670e4a8d9e234684e75d7da3d
Runeword <60324746+Runeword@users.noreply.github.com>
2025-04-11 23:45:07 +0200

    Update .config/shell/functions/git.sh

--- dot_config/shell/functions/executable_git.sh
+++ dot_config/shell/functions/executable_git.sh
@@ -37,39 +37,3 @@ __git_open_staged() {
     | sed "s|^|$repo_root/|" \
     | xargs -r nvim
 }
-
-__git_aliases() {
-  local selected_command
-  selected_command=$(< ~/.config/shell/functions/git-aliases \
-    column \
-    --table \
-    --separator $'\t' \
-    --output-separator $'\u00A0' \
-    | fzf -i \
-    --with-nth=1,2,3 \
-    --print-query \
-    --query "^" \
-    --exact \
-    --nth=1 \
-    --no-info \
-    --no-separator \
-    --delimiter=$'\u00A0' \
-    --cycle \
-    --no-preview \
-    --reverse \
-    --no-sort \
-    --prompt='  ' \
-    --bind 'one:accept,zero:accept,tab:accept' \
-    --height 70% \
-  )
-
-  if [ $? -eq 0 ]; then
-    LBUFFER+=$(echo "$selected_command" | awk -F $'\u00A0' '{ if (NR==2) { sub(/[[:space:]]+$/, "", $2); print $2 " " } }')
-  elif [ "$selected_command" ]; then
-    LBUFFER+=g$(echo "$selected_command" | sed -n '1p' | sed 's/[^[:alpha:]]//g')
-  fi
-
-  zle autosuggest-fetch
-
-  return 1
-}

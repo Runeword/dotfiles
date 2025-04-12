9b30a4ccf58063e1dc77031bf87443049343bdb5
Runeword <60324746+Runeword@users.noreply.github.com>
2025-04-12 12:22:49 +0200

    Update terminal/flake.nix

--- terminal/flake.nix
+++ terminal/flake.nix
@@ -30,23 +30,23 @@
           in
           pkgs.runCommandLocal name { } ''ln -s ${pkgs.lib.escapeShellArg fullPath} $out'';
 
-        # Build packages in dependency order
-        zsh = import ./packages/zsh.nix { inherit pkgs mkOutOfStoreSymlink; };
+        # # Build packages in dependency order
+        # zsh = import ./packages/zsh.nix { inherit pkgs mkOutOfStoreSymlink; };
 
-        tmux = import ./packages/tmux.nix {
-          inherit mkOutOfStoreSymlink;
-          pkgs = pkgs // {
-            zsh = zsh;
-          };
-        };
+        # tmux = import ./packages/tmux.nix {
+        #   inherit mkOutOfStoreSymlink;
+        #   pkgs = pkgs // {
+        #     zsh = zsh;
+        #   };
+        # };
 
-        leader = import ./packages/leader.nix { inherit pkgs; };
+        # leader = import ./packages/leader.nix { inherit pkgs; };
 
         # Package list for Alacritty
         packageList = (import ./packages/extraPackages.nix { inherit pkgs; }) ++ [
-          zsh
-          tmux
-          leader
+          # zsh
+          # tmux
+          # leader
         ];
 
         # Build Alacritty with the complete package list
@@ -62,9 +62,9 @@
           default = alacritty;
           inherit
             alacritty
-            tmux
-            leader
-            zsh
+            # tmux
+            # leader
+            # zsh
             ;
         };
 
@@ -73,14 +73,14 @@
             type = "app";
             program = "${alacritty}/bin/alacritty";
           };
-          tmux = {
-            type = "app";
-            program = "${tmux}/bin/tmux";
-          };
-          zsh = {
-            type = "app";
-            program = "${zsh}/bin/zsh";
-          };
+          # tmux = {
+          #   type = "app";
+          #   program = "${tmux}/bin/tmux";
+          # };
+          # zsh = {
+          #   type = "app";
+          #   program = "${zsh}/bin/zsh";
+          # };
         };
       }
     );

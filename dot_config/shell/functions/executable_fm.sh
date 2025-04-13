d9802c4dad764aa5d15624c149c4145cf861087c
Runeword <60324746+Runeword@users.noreply.github.com>
2025-04-13 20:48:54 +0200

    Update .config/shell/functions/fm.sh

--- dot_config/shell/functions/executable_fm.sh
+++ dot_config/shell/functions/executable_fm.sh
@@ -26,8 +26,9 @@ __open_file() {
         --header=''\''exact !not [!]^prefix [!]suffix$' \
         --preview "$HOME/.config/shell/scripts/fm_preview.sh {}" \
         --preview-window right,55%,border-none,~2 \
-        --bind='ctrl-y:execute-silent(wl-copy {})' \
+        --bind='ctrl-c:execute-silent(wl-copy {})' \
         --bind='ctrl-a:select-all' \
+        --bind='ctrl-o:execute(nohup setsid cursor {} > /dev/null 2>&1 &)' \
   ) || return 1
   # fzf-tmux \
   #   -p \

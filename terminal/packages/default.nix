97f681093fafdb5805a0a7a54a1fd12720e6b342
Runeword <60324746+Runeword@users.noreply.github.com>
2025-04-18 00:32:54 +0200

    Add terminal/packages/default.nix

---
+++ terminal/packages/default.nix
@@ -0,0 +1,34 @@
+{ pkgs, utils, system }:
+
+let
+  common = (import ./packages.nix { inherit pkgs; }) ++ [
+  ];
+
+  linux = pkgs.lib.optionals (system == "x86_64-linux" || system == "aarch64-linux") (
+    import ./packages-linux.nix { inherit pkgs; }
+  );
+
+  zsh = import ./zsh.nix {
+    inherit pkgs;
+    mkOutOfStoreSymlink = utils.mkOutOfStoreSymlink;
+  };
+
+  tmux = import ./tmux.nix {
+    inherit pkgs;
+    mkOutOfStoreSymlink = utils.mkOutOfStoreSymlink;
+  };
+
+  bash = import ./bash.nix {
+    inherit pkgs;
+    mkOutOfStoreSymlink = utils.mkOutOfStoreSymlink;
+  };
+
+  custom = [
+    zsh
+    tmux
+    bash
+  ];
+in
+{
+  inherit common linux custom;
+} 

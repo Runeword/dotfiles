229fb5818c1dcd324d5b87de4a7f45bad606f723
Runeword <60324746+Runeword@users.noreply.github.com>
2025-04-24 12:01:27 +0200

    Update terminal/flake.nix

--- terminal/flake.nix
+++ terminal/flake.nix
@@ -32,6 +32,32 @@
 
         extraPackages = packages.common ++ packages.linux ++ packages.custom;
 
+        findFlakeDir = pkgs.writeScriptBin "find-flake-dir" ''
+          #!/bin/sh
+          set -e
+
+          find_up() {
+            current_dir="$PWD"
+            flake_file="flake.nix"
+
+            while true; do
+              if [ -f "$current_dir/$flake_file" ]; then
+                echo "$current_dir"
+                exit 0
+              fi
+
+              if [ "$current_dir" = "/" ]; then
+                echo "ERROR: Unable to locate flake.nix in any parent directory" >&2
+                exit 1
+              fi
+
+              current_dir="$(dirname "$current_dir")"
+            done
+          }
+
+          find_up
+        '';
+
         alacritty =
           pkgs.runCommand "alacritty"
             {
@@ -47,7 +73,8 @@
               makeWrapper ${pkgs.alacritty}/bin/alacritty $out/bin/alacritty \
               --prefix PATH : ${pkgs.lib.makeBinPath extraPackages} \
               --set FONTCONFIG_FILE ${pkgs.makeFontsConf { fontDirectories = extraFonts; }} \
-              --set XDG_CONFIG_HOME "$out/.config"
+              --set XDG_CONFIG_HOME "$out/.config" \
+              --run 'export FLAKE_DIR="$(${findFlakeDir}/bin/find-flake-dir)"'
             '';
       in
       {

0c7ea2f68a19feb10a9f2e870687a6fd483e15ca
Runeword <60324746+Runeword@users.noreply.github.com>
2025-04-24 19:26:07 +0200

    Update terminal/lib/utils.nix

--- terminal/lib/utils.nix
+++ terminal/lib/utils.nix
@@ -1,11 +1,16 @@
 { pkgs }:
 
 {
-  mkOutOfStoreSymlink = path:
+  mkOutOfStoreSymlink =
+    path:
     let
       pathStr = toString path;
       name = builtins.baseNameOf pathStr;
       fullPath = "${builtins.toString /home/charles/terminal}/${pathStr}";
     in
     pkgs.runCommandLocal name { } ''ln -s ${pkgs.lib.escapeShellArg fullPath} $out'';
-} 
+
+  findFlakeDir = pkgs.writeScriptBin "find-flake-dir" ''
+    ${builtins.readFile ./find-flake-dir.sh}
+  '';
+}

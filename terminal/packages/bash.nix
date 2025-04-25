c98606f2226a3e771ee19479274e90fd846f9c52
Runeword <60324746+Runeword@users.noreply.github.com>
2025-04-25 13:02:46 +0200

    Update terminal/packages/bash.nix

--- terminal/packages/bash.nix
+++ terminal/packages/bash.nix
@@ -6,12 +6,13 @@ pkgs.symlinkJoin {
   buildInputs = [ pkgs.makeWrapper ];
   postBuild = ''
     mkdir -p $out/bin
+    mkdir -p $out/.config/bash
 
-    ln -sf ${mkOutOfStoreSymlink "config/bash/bashrc"} $out/.bashrc
-    ln -sf ${mkOutOfStoreSymlink "config/shell"} $out/shell
+    ln -sf ${mkOutOfStoreSymlink "config/bash/bashrc"} $out/.config/bash/.bashrc
+    ln -sf ${mkOutOfStoreSymlink "config/shell"} $out/.config/shell
 
     wrapProgram $out/bin/bash \
-      --add-flags "--rcfile $out/.bashrc" \
-      --set BASH_CONFIG "$out"
+      --add-flags "--rcfile $out/.config/bash/.bashrc" \
+      --set OUT "$out"
   '';
 }

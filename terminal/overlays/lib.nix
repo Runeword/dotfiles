00aad3bba80302ff826ea47a4ad5e63e9c4ffc66
Runeword <60324746+Runeword@users.noreply.github.com>
2025-04-25 16:59:23 +0200

    Add terminal/overlays/lib.nix

---
+++ terminal/overlays/lib.nix
@@ -0,0 +1,13 @@
+{ pkgs, flakePath }:
+
+final: prev: {
+  lib = prev.lib // {
+    mkOutOfStoreSymlink = path:
+      let
+        pathStr = toString path;
+        name = builtins.baseNameOf pathStr;
+        fullPath = "${flakePath}/${pathStr}";
+      in
+      pkgs.runCommandLocal name { } ''ln -s ${pkgs.lib.escapeShellArg fullPath} $out'';
+  };
+} 

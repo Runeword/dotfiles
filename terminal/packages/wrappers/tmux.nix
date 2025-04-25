e384dbe8c6b8b74978055a4004ffb16adebaac53
Runeword <60324746+Runeword@users.noreply.github.com>
2025-04-25 21:51:35 +0200

    Add terminal/packages/wrappers/tmux.nix

---
+++ terminal/packages/wrappers/tmux.nix
@@ -0,0 +1,22 @@
+{ pkgs }:
+
+pkgs.symlinkJoin {
+  name = "tmux-with-config";
+  paths = [ pkgs.tmux ];
+  buildInputs = [ pkgs.makeWrapper ];
+  postBuild = ''
+    mkdir -p $out/.config/tmux/plugins
+    mkdir -p $out/.config/shell/functions
+
+    ln -sf ${pkgs.lib.mkOutOfStoreSymlink "config/tmux/tmux.conf"} $out/.config/tmux/tmux.conf
+
+    ln -sf ${pkgs.lib.mkOutOfStoreSymlink "config/shell/functions/tmux.sh"} $out/.config/shell/functions/tmux.sh
+
+    ln -sf ${pkgs.tmuxPlugins.resurrect}/share/tmux-plugins/resurrect $out/.config/tmux/plugins/resurrect
+
+    wrapProgram $out/bin/tmux \
+      --set TMUX_SHELL ${pkgs.zsh}/bin/zsh \
+      --set TMUX_OUT "$out" \
+      --add-flags "-f $out/.config/tmux/tmux.conf"
+  '';
+}

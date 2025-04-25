3061236d75b8c585e21b1aa250974960b9bdcde8
Runeword <60324746+Runeword@users.noreply.github.com>
2025-04-25 11:22:03 +0200

    Update terminal/packages/zsh.nix

--- terminal/packages/zsh.nix
+++ terminal/packages/zsh.nix
@@ -9,9 +9,10 @@ pkgs.symlinkJoin {
   buildInputs = [ pkgs.makeWrapper ];
   postBuild = ''
     mkdir -p $out/bin
+    mkdir -p $out/.config
 
     ln -sf ${mkOutOfStoreSymlink "config/zsh/zshrc"} $out/.zshrc
-    ln -sf ${mkOutOfStoreSymlink "config/shell"} $out/shell
+    ln -sf ${mkOutOfStoreSymlink "config/shell"} $out/.config/shell
 
     wrapProgram $out/bin/zsh \
       --set ZDOTDIR "$out"

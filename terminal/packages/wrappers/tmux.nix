8505e0bacfce6b49f9bdc4e29a354b70b7b5f470
Runeword <60324746+Runeword@users.noreply.github.com>
2025-04-27 10:53:18 +0200

    Update terminal/packages/wrappers/tmux.nix

--- terminal/packages/wrappers/tmux.nix
+++ terminal/packages/wrappers/tmux.nix
@@ -13,10 +13,13 @@ pkgs.symlinkJoin {
     mkdir -p $out/.config/shell/functions
 
     ln -sf ${pkgs.lib.mkOutOfStoreSymlink "config/tmux/tmux.conf"} $out/.config/tmux/tmux.conf
-
     ln -sf ${pkgs.lib.mkOutOfStoreSymlink "config/shell/functions/tmux.sh"} $out/.config/shell/functions/tmux.sh
-
     ln -sf ${pkgs.tmuxPlugins.resurrect}/share/tmux-plugins/resurrect $out/.config/tmux/plugins/resurrect
+
+    # cp ${pkgs.lib.cleanSource ./../../config/tmux/tmux.conf} $out/.config/tmux/tmux.conf
+    # cp ${pkgs.lib.cleanSource ./../../config/shell/functions/tmux.sh} $out/.config/shell/functions/tmux.sh
+    # cp -r ${pkgs.tmuxPlugins.resurrect}/share/tmux-plugins/resurrect $out/.config/tmux/plugins/resurrect
+
 
     wrapProgram $out/bin/tmux \
       --set TMUX_SHELL ${zsh}/bin/zsh \

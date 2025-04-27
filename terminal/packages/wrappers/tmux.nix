91a46ef03bbfad793a0f563f3beb4586f895b44e
Runeword <60324746+Runeword@users.noreply.github.com>
2025-04-27 15:21:02 +0200

    Update terminal/packages/wrappers/tmux.nix

--- terminal/packages/wrappers/tmux.nix
+++ terminal/packages/wrappers/tmux.nix
@@ -13,14 +13,14 @@ pkgs.symlinkJoin {
     ${pkgs.lib.mkLink (pkgs.lib.mkOutOfStoreSymlink "config/shell/functions/tmux.sh") ".config/shell/functions/tmux.sh"}
     ${pkgs.lib.mkLink "${pkgs.tmuxPlugins.resurrect}/share/tmux-plugins/resurrect" ".config/tmux/plugins/resurrect"}
 
-    # cp ${pkgs.lib.cleanSource ./../../config/tmux/tmux.conf} $out/.config/tmux/tmux.conf
-    # cp ${pkgs.lib.cleanSource ./../../config/shell/functions/tmux.sh} $out/.config/shell/functions/tmux.sh
-    # cp -r ${pkgs.tmuxPlugins.resurrect}/share/tmux-plugins/resurrect $out/.config/tmux/plugins/resurrect
+    # ${pkgs.lib.mkCopy (pkgs.lib.cleanSource ./../../config/tmux/tmux.conf) ".config/tmux/tmux.conf"}
+    # ${pkgs.lib.mkCopy (pkgs.lib.cleanSource ./../../config/shell/functions/tmux.sh) ".config/shell/functions/tmux.sh"}
+    # ${pkgs.lib.mkCopy "${pkgs.tmuxPlugins.resurrect}/share/tmux-plugins/resurrect" ".config/tmux/plugins/resurrect"}
 
     mkdir -p $out/bin
     wrapProgram $out/bin/tmux \
-    --set TMUX_SHELL ${zsh}/bin/zsh \
-    --set TMUX_OUT "$out" \
-    --add-flags "-f $out/.config/tmux/tmux.conf"
+      --set TMUX_SHELL ${zsh}/bin/zsh \
+      --set TMUX_OUT "$out" \
+      --add-flags "-f $out/.config/tmux/tmux.conf"
   '';
 }

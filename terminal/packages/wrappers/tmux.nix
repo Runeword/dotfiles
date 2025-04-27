36805b051bb5a10ddcfee4017fb855f0ed523fb9
Runeword <60324746+Runeword@users.noreply.github.com>
2025-04-27 14:52:37 +0200

    Update terminal/packages/wrappers/tmux.nix

--- terminal/packages/wrappers/tmux.nix
+++ terminal/packages/wrappers/tmux.nix
@@ -9,17 +9,13 @@ pkgs.symlinkJoin {
   paths = [ pkgs.tmux ];
   buildInputs = [ pkgs.makeWrapper ];
   postBuild = ''
-    mkdir -p $out/.config/tmux/plugins
-    mkdir -p $out/.config/shell/functions
-
-    ln -sf ${pkgs.lib.mkOutOfStoreSymlink "config/tmux/tmux.conf"} $out/.config/tmux/tmux.conf
-    ln -sf ${pkgs.lib.mkOutOfStoreSymlink "config/shell/functions/tmux.sh"} $out/.config/shell/functions/tmux.sh
-    ln -sf ${pkgs.tmuxPlugins.resurrect}/share/tmux-plugins/resurrect $out/.config/tmux/plugins/resurrect
+    ${pkgs.lib.mkLink (pkgs.lib.mkOutOfStoreSymlink "config/tmux/tmux.conf") "/.config/tmux/tmux.conf"}
+    ${pkgs.lib.mkLink (pkgs.lib.mkOutOfStoreSymlink "config/shell/functions/tmux.sh") "/.config/shell/functions/tmux.sh"}
+    ${pkgs.lib.mkLink "${pkgs.tmuxPlugins.resurrect}/share/tmux-plugins/resurrect" "/.config/tmux/plugins/resurrect"}
 
     # cp ${pkgs.lib.cleanSource ./../../config/tmux/tmux.conf} $out/.config/tmux/tmux.conf
     # cp ${pkgs.lib.cleanSource ./../../config/shell/functions/tmux.sh} $out/.config/shell/functions/tmux.sh
     # cp -r ${pkgs.tmuxPlugins.resurrect}/share/tmux-plugins/resurrect $out/.config/tmux/plugins/resurrect
-
 
     wrapProgram $out/bin/tmux \
       --set TMUX_SHELL ${zsh}/bin/zsh \

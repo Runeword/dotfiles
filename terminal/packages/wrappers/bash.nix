67ef6e649ddf8f7481b8aaf98bd0f66e9188fdf0
Runeword <60324746+Runeword@users.noreply.github.com>
2025-04-27 21:42:34 +0200

    Update terminal/packages/wrappers/bash.nix

--- terminal/packages/wrappers/bash.nix
+++ terminal/packages/wrappers/bash.nix
@@ -5,8 +5,8 @@ pkgs.symlinkJoin {
   paths = [ pkgs.bash ];
   buildInputs = [ pkgs.makeWrapper ];
   postBuild = ''
-    ${pkgs.lib.mkLink "${pkgs.lib.flakePath}/config/bash/bashrc" ".config/bash/.bashrc"}
-    ${pkgs.lib.mkLink "${pkgs.lib.flakePath}/config/shell" ".config/shell"}
+    ${pkgs.lib.mkFile "./../../" "config/bash/bashrc" ".config/bash/.bashrc"}
+    ${pkgs.lib.mkFile "./../../" "config/shell" ".config/shell"}
 
     mkdir -p $out/bin
     wrapProgram $out/bin/bash \

1cf632e71b26d9861ad6b05e4365fb99e8d79819
Runeword <60324746+Runeword@users.noreply.github.com>
2025-04-28 12:03:05 +0200

    Update terminal/packages/wrappers/tmux.nix

--- terminal/packages/wrappers/tmux.nix
+++ terminal/packages/wrappers/tmux.nix
@@ -10,15 +10,15 @@ pkgs.symlinkJoin {
   buildInputs = [ pkgs.makeWrapper ];
   postBuild = ''
 
-  ${pkgs.lib.mkCopy (pkgs.lib.cleanSource ./../../config/tmux/tmux.conf) ".config/tmux/tmux.conf"}
-  ${pkgs.lib.mkCopy (pkgs.lib.cleanSource ./../../config/shell/functions/tmux.sh) ".config/shell/functions/tmux.sh"}
-  ${pkgs.lib.mkCopy "${pkgs.tmuxPlugins.resurrect}/share/tmux-plugins/resurrect" ".config/tmux/plugins/resurrect"}
+    ${pkgs.lib.mkCopy ./../../config/tmux/tmux.conf ".config/tmux/tmux.conf"}
+    ${pkgs.lib.mkCopy ./../../config/shell/functions/tmux.sh ".config/shell/functions/tmux.sh"}
+    ${pkgs.lib.mkCopy "${pkgs.tmuxPlugins.resurrect}/share/tmux-plugins/resurrect" ".config/tmux/plugins/resurrect"}
 
     mkdir -p $out/bin
     wrapProgram $out/bin/tmux \
-      --set TMUX_SHELL ${zsh}/bin/zsh \
-      --set TMUX_OUT "$out" \
-      --add-flags "-f $out/.config/tmux/tmux.conf"
+    --set TMUX_SHELL ${zsh}/bin/zsh \
+    --set TMUX_OUT "$out" \
+    --add-flags "-f $out/.config/tmux/tmux.conf"
   '';
 }
 

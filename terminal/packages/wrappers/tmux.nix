035fec7e719fc4de65964cd47afcb6fa53757fb8
Runeword <60324746+Runeword@users.noreply.github.com>
2025-04-28 16:35:19 +0200

    Update terminal/packages/wrappers/tmux.nix

--- terminal/packages/wrappers/tmux.nix
+++ terminal/packages/wrappers/tmux.nix
@@ -7,14 +7,12 @@ in
 pkgs.symlinkJoin {
   name = "tmux-with-config";
   paths = [ pkgs.tmux ];
-  buildInputs = [ pkgs.makeWrapper ];
+  nativeBuildInputs = [ pkgs.makeWrapper ];
   postBuild = ''
+    ${pkgs.lib.mkLink "config/tmux/tmux.conf" ".config/tmux/tmux.conf"}
+    ${pkgs.lib.mkLink "config/shell/functions/tmux.sh" ".config/shell/functions/tmux.sh"}
+    ${pkgs.lib.mkLink "${pkgs.tmuxPlugins.resurrect}/share/tmux-plugins/resurrect" ".config/tmux/plugins/resurrect"}
 
-    ${pkgs.lib.mkCopy ./../../config/tmux/tmux.conf ".config/tmux/tmux.conf"}
-    ${pkgs.lib.mkCopy ./../../config/shell/functions/tmux.sh ".config/shell/functions/tmux.sh"}
-    ${pkgs.lib.mkCopy "${pkgs.tmuxPlugins.resurrect}/share/tmux-plugins/resurrect" ".config/tmux/plugins/resurrect"}
-
-    mkdir -p $out/bin
     wrapProgram $out/bin/tmux \
     --set TMUX_SHELL ${zsh}/bin/zsh \
     --set TMUX_OUT "$out" \
@@ -22,9 +20,9 @@ pkgs.symlinkJoin {
   '';
 }
 
-# ${pkgs.lib.mkLink "config/tmux/tmux.conf" ".config/tmux/tmux.conf"}
-# ${pkgs.lib.mkLink "config/shell/functions/tmux.sh" ".config/shell/functions/tmux.sh"}
-# ${pkgs.lib.mkLink "${pkgs.tmuxPlugins.resurrect}/share/tmux-plugins/resurrect" ".config/tmux/plugins/resurrect"}
+# ${pkgs.lib.mkCopy ./../../config/tmux/tmux.conf ".config/tmux/tmux.conf"}
+# ${pkgs.lib.mkCopy ./../../config/shell/functions/tmux.sh ".config/shell/functions/tmux.sh"}
+# ${pkgs.lib.mkCopy "${pkgs.tmuxPlugins.resurrect}/share/tmux-plugins/resurrect" ".config/tmux/plugins/resurrect"}
 
 # ${pkgs.lib.mkCopy (pkgs.lib.cleanSource ./../../config/tmux/tmux.conf) ".config/tmux/tmux.conf"}
 # ${pkgs.lib.mkCopy (pkgs.lib.cleanSource ./../../config/shell/functions/tmux.sh) ".config/shell/functions/tmux.sh"}

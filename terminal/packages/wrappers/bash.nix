045bb991c7f665ef7b0c8225703c8d6baa43f2ab
Runeword <60324746+Runeword@users.noreply.github.com>
2025-04-28 16:35:16 +0200

    Update terminal/packages/wrappers/bash.nix

--- terminal/packages/wrappers/bash.nix
+++ terminal/packages/wrappers/bash.nix
@@ -3,12 +3,11 @@
 pkgs.symlinkJoin {
   name = "bash-with-config";
   paths = [ pkgs.bash ];
-  buildInputs = [ pkgs.makeWrapper ];
+  nativeBuildInputs = [ pkgs.makeWrapper ];
   postBuild = ''
     ${pkgs.lib.mkLink "config/bash/bashrc" ".config/bash/.bashrc"}
     ${pkgs.lib.mkLink "config/shell" ".config/shell"}
 
-    mkdir -p $out/bin
     wrapProgram $out/bin/bash \
       --add-flags "--rcfile $out/.config/bash/.bashrc" \
       --set OUT "$out"

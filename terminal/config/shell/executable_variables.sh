c7229430b0a9b2638ff2f07f7d0e7049582fd996
Runeword <60324746+Runeword@users.noreply.github.com>
2025-05-09 23:08:49 +0200

    Update terminal/config/shell/variables.sh

--- terminal/config/shell/executable_variables.sh
+++ terminal/config/shell/executable_variables.sh
@@ -4,7 +4,6 @@ export EDITOR="nvim"
 export VISUAL="nvim"
 export MANPAGER="nvim +Man!"
 export BROWSER="google-chrome-stable"
-export DIRENV_LOG_FORMAT=
 
 # DIRENV_LOG_FORMAT = "$(tput setaf 0)direnv: %s$(tput sgr0)";
 # DIRENV_LOG_FORMAT = ''echo -e "\e[90mdirenv: %s\e[0m"'';
@@ -36,11 +35,12 @@ export FORGIT_FZF_DEFAULT_OPTS="
 
 # --color=marker:#57b58f,spinner:#ffffff,header:#535e73 \
 export FZF_DEFAULT_OPTS="
---color=fg:#d0d0d0,bg:-1,hl:#ffffff \
+--color=fg:#d0d0d0,bg:-1,hl:#ffffff,gutter:-1 \
 --color=fg+:#ffffff,fg+:regular,bg+:#142920,hl+:#67d6a9,hl+:regular,query:italic \
 --color=info:#d0d0d0,prompt:#ffffff,pointer:#67d6a9,border:#2f394a \
 --color=marker:#458f71,spinner:#ffffff,header:#535e73 \
 "
+
 export FZF_CTRL_R_OPTS="
 --reverse \
 --prompt='  ' \

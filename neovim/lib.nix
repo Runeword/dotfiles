96ad35bbd96aad84c21632bf6dc3825e09bbaf8f
Runeword <60324746+Runeword@users.noreply.github.com>
2025-05-23 16:54:07 +0200

    Remove neovim/lib.nix

--- neovim/lib.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, basePath }:
-
-path:
-let
-  pathStr = toString path;
-  drvName = builtins.baseNameOf pathStr;
-  fullPath = "${basePath}/${pathStr}";
-in
-pkgs.runCommandLocal drvName { } ''ln -s ${pkgs.lib.escapeShellArg fullPath} $out''

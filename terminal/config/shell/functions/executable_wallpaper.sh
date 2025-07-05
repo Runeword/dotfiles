443a1150b517800c6b1ca557089cfec32c28f3e5
Runeword <60324746+Runeword@users.noreply.github.com>
2025-07-05 20:00:42 +0200

    Remove terminal/config/alacritty/alacritty.toml Remove terminal/config/bash/bashrc Remove terminal/config/bash/.shellcheckrc Remove terminal/config/shell/.shellcheckrc Remove terminal/config/shell/aliases.sh Remove terminal/config/shell/variables.sh Remove terminal/config/shell/xdg.sh Remove terminal/config/shell/functions/bluetooth.sh Remove terminal/config/shell/functions/aliases.sh Remove terminal/config/shell/functions/bitwarden.sh Remove terminal/config/shell/functions/cd.sh Remove terminal/config/shell/functions/chezmoi.sh Remove terminal/config/shell/functions/devices.sh Remove terminal/config/shell/functions/distrobox.sh Remove terminal/config/shell/functions/fm.sh Remove terminal/config/shell/functions/git.sh Remove terminal/config/shell/functions/leader.zsh.sh Remove terminal/config/shell/functions/nix.sh Remove terminal/config/shell/functions/pass.sh Remove terminal/config/shell/functions/processes.sh Remove terminal/config/shell/functions/tmux.sh Remove terminal/config/shell/functions/wallpaper.sh Remove terminal/config/shell/functions/wifi.sh Remove terminal/config/shell/functions/yazi.sh Remove terminal/config/shell/functions/leader-aliases Remove terminal/config/shell/scripts/fm_preview.sh Remove terminal/config/shell/scripts/ssh-agent.sh Remove terminal/config/tmux/tmux.conf Remove terminal/config/zsh/.shellcheckrc Remove terminal/config/zsh/zshrc Remove terminal/extraConfig/bat/config Remove terminal/extraConfig/bat/themes/monochrome.tmTheme Remove terminal/flake.lock Remove terminal/flake.nix Remove terminal/overlays/lib.nix Remove terminal/packages/commons.nix Remove terminal/packages/darwin.nix Remove terminal/packages/linux.nix Remove terminal/packages/wrappers/alacritty.nix Remove terminal/packages/wrappers/bash.nix Remove terminal/packages/wrappers/extra.nix Remove terminal/packages/wrappers/tmux.nix Remove terminal/packages/wrappers/zsh.nix

--- terminal/config/shell/functions/executable_wallpaper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-__wallpaper() {
-	hyprctl hyprpaper preload "$1" &&
-		hyprctl hyprpaper wallpaper eDP-1,"$1" &&
-		hyprctl hyprpaper unload all
-}
--- terminal/packages/darwin.nix
+++
@@ -1,6 +0,0 @@
-{ pkgs, system }:
-
-[
-  # Add macOS-specific packages here
-  pkgs.fontconfig # Required for font configuration on macOS
-]
